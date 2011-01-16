9c66a2d54c1051bfd9daab26b6b1494c0a4a25bf
GreYFoXGTi <GreYFoXGTi@GMaiL.CoM>
2011-01-17 00:13:21 +0200

    Endless Hook Tile for ingameplay by Floff aka LemonFace

--- src/game/server/entities/character.h
+++ src/game/server/entities/character.h
@@ -122,6 +122,8 @@ public:
 	//DDRace
 	int m_FreezeTime;
 	int m_FreezeTick;
+
+	bool m_EndlessHook;
 	
 	int m_Doored;
 
--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -285,6 +285,7 @@ void CPlayer::LoadCharacter()
 	Character->m_LastWeapon = m_PauseInfo.m_LastWeapon;
 	Character->m_HammerType = m_PauseInfo.m_HammerType;
 	Character->m_Super = m_PauseInfo.m_Super;
+	Character->m_EndlessHook = m_PauseInfo.m_EndlessHook;
 	CGameControllerDDRace* Controller = (CGameControllerDDRace*)GameServer()->m_pController;
 	Controller->m_Teams.m_Core.Team(GetCID(), m_PauseInfo.m_Team);
 	m_PauseInfo.m_Respawn = false;
@@ -315,6 +316,7 @@ void CPlayer::SaveCharacter()
 	m_PauseInfo.m_LastWeapon = Character->m_LastWeapon;
 	m_PauseInfo.m_HammerType = Character->m_HammerType;
 	m_PauseInfo.m_Super = Character->m_Super;
+	m_PauseInfo.m_EndlessHook = Character->m_EndlessHook;
 	CGameControllerDDRace* Controller = (CGameControllerDDRace*)GameServer()->m_pController;
 	m_PauseInfo.m_Team = Controller->m_Teams.m_Core.Team(GetCID());
 	m_PauseInfo.m_PauseTime = Server()->Tick();
