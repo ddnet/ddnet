937d81c85b6889fd9fec83eaeb3553384909a7d3
GreYFoXGTi <GreYFoXGTi@GMaiL.CoM>
2011-02-21 00:42:12 +0200

    Upped the version again to 1.052a just for bug tracking

--- src/game/version.h
+++ src/game/version.h
@@ -3,6 +3,6 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 
-#define GAME_VERSION "0.5 trunk, 1.051a"
+#define GAME_VERSION "0.5 trunk, 1.052a"
 #define GAME_NETVERSION "0.5 b67d1f1a1eea234e"
 #endif
