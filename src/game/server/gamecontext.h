1172a79693c47644c2208f98decf24ee43a16367
GreYFoX <GreYFoXGTi@GMaiL.CoM>
2011-03-22 21:15:15 +0200

    fixed compilation

--- src/game/server/gamecontext.h
+++ src/game/server/gamecontext.h
@@ -231,8 +231,9 @@ private:
 	static void ConTop5(IConsole::IResult *pResult, void *pUserData, int ClientID);
 	#if defined(CONF_SQL)
 	static void ConTimes(IConsole::IResult *pResult, void *pUserData, int ClientID);
-	static void ConUTF8(IConsole::IResult *pResult, void *pUserData, int ClientID);	
 	#endif
+
+	static void ConUTF8(IConsole::IResult *pResult, void *pUserData, int ClientID);	
 	static void ConRank(IConsole::IResult *pResult, void *pUserData, int ClientID);
 	static void ConBroadTime(IConsole::IResult *pResult, void *pUserData, int ClientID);
 	static void ConJoinTeam(IConsole::IResult *pResult, void *pUserData, int ClientID);
