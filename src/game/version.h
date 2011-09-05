eda127392ab766cac7a1091ba57885584d798f4a
GreYFoX <GreYFoXGTi@GMaiL.CoM>
2011-09-05 15:51:58 +0200

    0.6 trunk, 1.096a

--- src/game/version.h
+++ src/game/version.h
@@ -3,6 +3,6 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define GAME_VERSION "0.6 trunk, 1.095a"
+#define GAME_VERSION "0.6 trunk, 1.096a"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 #endif
