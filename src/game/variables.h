ae53eb201eba68b76c28386cff27b54f1592d908
def <dennis@felsin9.de>
2013-07-19 01:26:14 +0200

    Set sv_teleport_hook and sv_teleport_weapons off by default

--- src/game/variables.h
+++ src/game/variables.h
@@ -82,8 +82,8 @@ MACRO_CONFIG_INT(SvVoteSpectateRejoindelay, sv_vote_spectate_rejoindelay, 3, 0, 
 MACRO_CONFIG_INT(SvVoteKick, sv_vote_kick, 1, 0, 1, CFGFLAG_SERVER, "Allow voting to kick players")
 MACRO_CONFIG_INT(SvVoteKickMin, sv_vote_kick_min, 0, 0, MAX_CLIENTS, CFGFLAG_SERVER, "Minimum number of players required to start a kick vote")
 MACRO_CONFIG_INT(SvVoteKickBantime, sv_vote_kick_bantime, 5, 0, 1440, CFGFLAG_SERVER, "The time to ban a player if kicked by vote. 0 makes it just use kick")
-MACRO_CONFIG_INT(SvTeleportWeapons, sv_teleport_weapons, 1, 0, 1, CFGFLAG_SERVER, "Teleporting of all weapons");
-MACRO_CONFIG_INT(SvTeleportHook, sv_teleport_hook, 1, 0, 1, CFGFLAG_SERVER, "Hook through teleporter");
+MACRO_CONFIG_INT(SvTeleportWeapons, sv_teleport_weapons, 0, 0, 1, CFGFLAG_SERVER, "Teleporting of all weapons");
+MACRO_CONFIG_INT(SvTeleportHook, sv_teleport_hook, 0, 0, 1, CFGFLAG_SERVER, "Hook through teleporter");
 
 // debug
 #ifdef CONF_DEBUG // this one can crash the server if not used correctly
