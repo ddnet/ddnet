ba5b98dd1db408743252aed8fd3854e0e3cd9f41
def <dennis@felsin9.de>
2013-07-30 23:26:30 +0200

    Restrict rcon output (east)

--- src/engine/server.h
+++ src/engine/server.h
@@ -32,6 +32,7 @@ public:
 	virtual bool ClientIngame(int ClientID) = 0;
 	virtual int GetClientInfo(int ClientID, CClientInfo *pInfo) = 0;
 	virtual void GetClientAddr(int ClientID, char *pAddrStr, int Size) = 0;
+	virtual void RestrictRconOutput(int ClientID) = 0;
 
 	virtual int SendMsg(CMsgPacker *pMsg, int Flags, int ClientID) = 0;
 
--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -802,9 +802,10 @@ void CGameContext::OnMessage(int MsgID, CUnpacker *pUnpacker, int ClientID)
 				*pMessage = ' ';
 			pMessage++;
 		}
-		if(pMsg->m_pMessage[0]=='/')
+		if(pMsg->m_pMessage[0]=='/') // TODO: Add spam protection
 		{
 			m_ChatResponseTargetID = ClientID;
+			Server()->RestrictRconOutput(ClientID);
 			Console()->SetFlagMask(CFGFLAG_CHAT);
 
 			if (pPlayer->m_Authed)
@@ -821,6 +822,7 @@ void CGameContext::OnMessage(int MsgID, CUnpacker *pUnpacker, int ClientID)
 			Console()->SetAccessLevel(IConsole::ACCESS_LEVEL_ADMIN);
 			Console()->SetFlagMask(CFGFLAG_SERVER);
 			m_ChatResponseTargetID = -1;
+			Server()->RestrictRconOutput(-1);
 		}
 		else
 			SendChat(ClientID, Team, pMsg->m_pMessage, ClientID);
