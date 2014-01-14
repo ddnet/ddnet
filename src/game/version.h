f0a242f0e9c490af223a2043f97fe94b999082da
def <dennis@felsin9.de>
2014-01-15 00:35:26 +0100

    Version 2.12

--- src/game/version.h
+++ src/game/version.h
@@ -3,7 +3,7 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define GAME_VERSION "0.6.2, 2.11"
+#define GAME_VERSION "0.6.2, 2.12"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
-static const char GAME_RELEASE_VERSION[8] = "2.11";
+static const char GAME_RELEASE_VERSION[8] = "2.12";
 #endif
