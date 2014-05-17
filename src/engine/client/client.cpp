bede3039767e21a0e23a084ad7fb9de1c552edeb
def <dennis@felsin9.de>
2014-05-17 21:37:55 +0200

    Fix: Dummy connect

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -1749,7 +1749,13 @@ void CClient::ProcessServerPacketDummy(CNetChunk *pPacket)
 
 	if(Sys)
 	{
-		if(Msg == NETMSG_SNAP || Msg == NETMSG_SNAPSINGLE || Msg == NETMSG_SNAPEMPTY)
+		if(Msg == NETMSG_CON_READY)
+		{
+			m_DummyConnected = true;
+			m_LastDummyConnectTime = GameTick();
+			g_Config.m_ClDummy = 1;
+		}
+		else if(Msg == NETMSG_SNAP || Msg == NETMSG_SNAPSINGLE || Msg == NETMSG_SNAPEMPTY)
 		{
 			int NumParts = 1;
 			int Part = 0;
@@ -1923,10 +1929,6 @@ void CClient::ProcessServerPacketDummy(CNetChunk *pPacket)
 						m_aSnapshots[!g_Config.m_ClDummy][SNAP_CURRENT] = m_SnapshotStorage[!g_Config.m_ClDummy].m_pLast;
 						m_LocalStartTime = time_get();
 						SetState(IClient::STATE_ONLINE);
-
-						m_DummyConnected = true;
-						m_LastDummyConnectTime = GameTick;
-						g_Config.m_ClDummy = 1;
 
 						//DemoRecorder_HandleAutoStart();
 					}
