6267c84ad89328bacca72b5742a8d6664dca28a6
def <dennis@felsin9.de>
2014-06-05 15:19:54 +0200

    Version 4.1

--- src/game/version.h
+++ src/game/version.h
@@ -3,8 +3,8 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define GAME_VERSION "0.6.2, 4.0"
+#define GAME_VERSION "0.6.2, 4.1"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
-static const char GAME_RELEASE_VERSION[8] = "4.0";
-#define CLIENT_VERSIONNR 400
+static const char GAME_RELEASE_VERSION[8] = "4.1";
+#define CLIENT_VERSIONNR 401
 #endif
