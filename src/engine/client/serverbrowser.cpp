13f0a455e104ef11741f9c317394ad836c73416a
def <dennis@felsin9.de>
2014-06-22 02:41:46 +0200

    Unneded include

--- src/engine/client/serverbrowser.cpp
+++ src/engine/client/serverbrowser.cpp
@@ -1,6 +1,5 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
-#include <malloc.h>
 #include <algorithm> // sort  TODO: remove this
 
 #include <base/math.h>
