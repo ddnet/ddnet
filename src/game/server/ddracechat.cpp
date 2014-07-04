88b187bc7d4fbe45c6395cc4d40e137ccf7eca58
def <dennis@felsin9.de>
2014-07-04 09:22:21 +0200

    More readable lock messages

--- src/game/server/ddracechat.cpp
+++ src/game/server/ddracechat.cpp
@@ -655,7 +655,7 @@ void CGameContext::ConLockTeam(IConsole::IResult *pResult, void *pUserData)
 		{
 			((CGameControllerDDRace*) pSelf->m_pController)->m_Teams.SetTeamLock(Team, false);
 
-			str_format(aBuf, sizeof(aBuf), "Your team is now unlocked (by %s).", pSelf->Server()->ClientName(pResult->m_ClientID));
+			str_format(aBuf, sizeof(aBuf), "'%s' unlocked your team.", pSelf->Server()->ClientName(pResult->m_ClientID));
 
 			for (int i = 0; i < MAX_CLIENTS; i++)
 				if (((CGameControllerDDRace*) pSelf->m_pController)->m_Teams.m_Core.Team(i) == Team)
@@ -665,7 +665,7 @@ void CGameContext::ConLockTeam(IConsole::IResult *pResult, void *pUserData)
 		{
 			((CGameControllerDDRace*) pSelf->m_pController)->m_Teams.SetTeamLock(Team, true);
 
-			str_format(aBuf, sizeof(aBuf), "Your team is now locked (by %s). After the race started killing will kill everyone in your team.", pSelf->Server()->ClientName(pResult->m_ClientID));
+			str_format(aBuf, sizeof(aBuf), "'%s' locked your team. After the race started killing will kill everyone in your team.", pSelf->Server()->ClientName(pResult->m_ClientID));
 
 			for (int i = 0; i < MAX_CLIENTS; i++)
 				if (((CGameControllerDDRace*) pSelf->m_pController)->m_Teams.m_Core.Team(i) == Team)
