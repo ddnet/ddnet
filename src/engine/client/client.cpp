c54cf2e7ba0fae376d06d5c0fab446adbf1d769c
def <dennis@felsin9.de>
2014-09-26 02:41:25 +0200

    Cleanup

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -1,7 +1,6 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #include <new>
-#include <iostream>
 
 #include <time.h>
 #include <stdlib.h> // qsort
--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -1,6 +1,5 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
-#include <iostream>
 #include <engine/editor.h>
 #include <engine/engine.h>
 #include <engine/friends.h>
