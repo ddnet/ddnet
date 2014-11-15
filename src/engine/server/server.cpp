56fbd92be9a2eaa0c588b8bfdcd6dab0a97be274
def <dennis@felsin9.de>
2014-11-15 18:58:57 +0100

    number check

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -1425,7 +1425,7 @@ void CServer::PumpNetwork()
 			int Last = 0;
 	
 			// drop faulty map data requests
-			if(Chunk < 0 || Offset > m_CurrentMapSize)
+			if(Chunk < 0 || Offset < 0 || Offset > m_CurrentMapSize)
 				continue;
 			if(Offset+ChunkSize >= m_CurrentMapSize)
 			{
