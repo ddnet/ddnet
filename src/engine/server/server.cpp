197ed5dca3a60caf62f768db65d5716744750040
def <dennis@felsin9.de>
2014-11-22 13:17:55 +0100

    Use 0.6.3 server crash fix since the other seems not to work with GCC 4.9.2 with optimizations on

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -987,8 +987,8 @@ void CServer::ProcessClientPacket(CNetChunk *pPacket)
 				return; // no map w/o password, sorry guys
 
 			int Chunk = Unpacker.GetInt();
-			int ChunkSize = 1024-128;
-			int Offset = Chunk * ChunkSize;
+			unsigned int ChunkSize = 1024-128;
+			unsigned int Offset = Chunk * ChunkSize;
 			int Last = 0;
 
 			lastask[ClientID] = Chunk;
@@ -999,7 +999,7 @@ void CServer::ProcessClientPacket(CNetChunk *pPacket)
 			}
 
 			// drop faulty map data requests
-			if(Chunk < 0 || Offset < 0 || Offset > m_CurrentMapSize)
+			if(Chunk < 0 || Offset > m_CurrentMapSize)
 				return;
 
 			if(Offset+ChunkSize >= m_CurrentMapSize)
@@ -1420,12 +1420,12 @@ void CServer::PumpNetwork()
 				continue;
 	
 			int Chunk = lastsent[i]++;
-			int ChunkSize = 1024-128;
-			int Offset = Chunk * ChunkSize;
+			unsigned int ChunkSize = 1024-128;
+			unsigned int Offset = Chunk * ChunkSize;
 			int Last = 0;
 	
 			// drop faulty map data requests
-			if(Chunk < 0 || Offset < 0 || Offset > m_CurrentMapSize)
+			if(Chunk < 0 || Offset > m_CurrentMapSize)
 				continue;
 			if(Offset+ChunkSize >= m_CurrentMapSize)
 			{
