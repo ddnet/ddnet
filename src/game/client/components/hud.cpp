0947de4b3f1c80cb746cda2a425228f94ad711dc
def <dennis@felsin9.de>
2014-12-02 13:55:19 +0100

    Centisecs -> Decisecs

--- src/game/client/components/hud.cpp
+++ src/game/client/components/hud.cpp
@@ -62,19 +62,19 @@ void CHud::RenderGameTimer()
 		bool IsGameTypeRace = str_find_nocase(Info.m_aGameType, "race") || str_find_nocase(Info.m_aGameType, "fastcap");
 		bool IsGameTypeDDRace = str_find_nocase(Info.m_aGameType, "ddrace") || str_find_nocase(Info.m_aGameType, "mkrace");
 
-		if(Time <= 0 && g_Config.m_ClShowCentisecs)
+		if(Time <= 0 && g_Config.m_ClShowDecisecs)
 			str_format(Buf, sizeof(Buf), "00:00.0");
 		else if(Time <= 0)
 			str_format(Buf, sizeof(Buf), "00:00");
 		else if(IsGameTypeRace && !IsGameTypeDDRace && m_ServerRecord >= 0)
 			str_format(Buf, sizeof(Buf), "%02d:%02d", (int)(m_ServerRecord*100)/60, ((int)(m_ServerRecord*100)%60));
-		else if(g_Config.m_ClShowCentisecs)
+		else if(g_Config.m_ClShowDecisecs)
 			str_format(Buf, sizeof(Buf), "%02d:%02d.%d", Time/60, Time%60, m_DDRaceTick/10);
 		else
 			str_format(Buf, sizeof(Buf), "%02d:%02d", Time/60, Time%60);
 		float FontSize = 10.0f;
 		float w;
-		if(g_Config.m_ClShowCentisecs)
+		if(g_Config.m_ClShowDecisecs)
 			w = TextRender()->TextWidth(0, 12,"00:00.0",-1);
 		else
 			w = TextRender()->TextWidth(0, 12,"00:00",-1);
