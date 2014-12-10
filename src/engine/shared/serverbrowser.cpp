dfa804fb58264d6031dda231a14ffbb3cdc553ea
def <dennis@felsin9.de>
2014-12-10 11:21:13 +0100

    Add missing file

---
+++ src/engine/shared/serverbrowser.cpp
@@ -0,0 +1,27 @@
+#include <engine/serverbrowser.h>
+#include <base/system.h>
+
+bool IsRace(const CServerInfo *pInfo)
+{
+	return str_find_nocase(pInfo->m_aGameType, "race")
+	    || str_find_nocase(pInfo->m_aGameType, "fastcap");
+}
+
+bool IsDDRace(const CServerInfo *pInfo)
+{
+	return str_find_nocase(pInfo->m_aGameType, "ddrace")
+	    || str_find_nocase(pInfo->m_aGameType, "mkrace");
+}
+
+bool IsDDNet(const CServerInfo *pInfo)
+{
+	return str_find_nocase(pInfo->m_aGameType, "ddracenet")
+	    || str_find_nocase(pInfo->m_aGameType, "ddnet");
+}
+
+bool Is64Player(const CServerInfo *pInfo)
+{
+	return str_find(pInfo->m_aGameType, "64")
+	    || str_find(pInfo->m_aName, "64")
+	    || IsDDNet(pInfo);
+}
