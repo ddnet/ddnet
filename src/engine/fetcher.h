142c386cd619629256c2cf15f1e7a9f2c8ff2f5e
Learath Lea <learath2@gmail.com>
2015-02-08 11:52:47 +0200

    Remove obsolete function prototype.

--- src/engine/fetcher.h
+++ src/engine/fetcher.h
@@ -53,7 +53,6 @@ class IFetcher : public IInterface
 public:
 	virtual bool Init() = 0;
 	virtual void QueueAdd(CFetchTask *pTask,const char *pUrl, const char *pDest, int StorageType = 2, void *pUser = 0, COMPFUNC pfnCompCb = 0, PROGFUNC pfnProgCb = 0) = 0;
-	virtual long HTTPResponse(const char *pUrl) = 0;
 };
 
 #endif
