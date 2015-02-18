8f570596d94c56ecf10046e5befdc1b256d6c9f9
def <dennis@felsin9.de>
2015-02-18 14:34:49 +0100

    Release hook when player changes team

--- src/game/gamecore.cpp
+++ src/game/gamecore.cpp
@@ -349,7 +349,7 @@ void CCharacterCore::Tick(bool UseInput, bool IsClient)
 		if(m_HookedPlayer != -1)
 		{
 			CCharacterCore *pCharCore = m_pWorld->m_apCharacters[m_HookedPlayer];
-			if(pCharCore)
+			if(pCharCore && m_Id >= 0 && pCharCore->m_Id >= 0 && m_pTeams->SameTeam(m_Id, pCharCore->m_Id))
 				m_HookPos = pCharCore->m_Pos;
 			else
 			{
