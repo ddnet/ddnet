4625a4de5890039b89dec65635f6e0d778c83403
def <dennis@felsin9.de>
2015-04-18 19:56:07 +0200

    Version 7.5

--- src/game/version.h
+++ src/game/version.h
@@ -3,8 +3,8 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define GAME_VERSION "0.6.3, 7.4"
+#define GAME_VERSION "0.6.3, 7.5"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
-static const char GAME_RELEASE_VERSION[8] = "7.4";
-#define CLIENT_VERSIONNR 704
+static const char GAME_RELEASE_VERSION[8] = "7.5";
+#define CLIENT_VERSIONNR 705
 #endif
