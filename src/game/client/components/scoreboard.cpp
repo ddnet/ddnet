1c7750151dd8619aeab72e5cadaac94e90bdc51d
def <dennis@felsin9.de>
2015-06-25 15:39:22 +0200

    Show full time in scoreboard even when minutes are > 100

--- src/game/client/components/scoreboard.cpp
+++ src/game/client/components/scoreboard.cpp
@@ -419,8 +419,7 @@ void CScoreboard::RenderScoreboard(float x, float y, float w, int Team, const ch
 		else
 			str_format(aBuf, sizeof(aBuf), "%d", clamp(pInfo->m_Score, -999, 999));
 		tw = TextRender()->TextWidth(0, FontSize, aBuf, -1);
-		TextRender()->SetCursor(&Cursor, ScoreOffset+ScoreLength-tw, y+Spacing, FontSize, TEXTFLAG_RENDER|TEXTFLAG_STOP_AT_END);
-		Cursor.m_LineWidth = ScoreLength;
+		TextRender()->SetCursor(&Cursor, ScoreOffset+ScoreLength-tw, y+Spacing, FontSize, TEXTFLAG_RENDER);
 		TextRender()->TextEx(&Cursor, aBuf, -1);
 
 		// flag
