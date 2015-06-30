dec9985c6a3c984528d0772596636eb5dedea0fe
timgame <tim@bschu.de>
2015-06-30 18:57:25 +0200

    added language scripts

---
+++ data/languages/scripts/find_unchanged.py
@@ -0,0 +1,12 @@
+import twlang
+import sys
+
+if len(sys.argv) < 2:
+    print("usage: python find_unchanged.py <file>")
+    sys.exit()
+infile = sys.argv[1]
+
+trans = twlang.translations(infile)
+for tran, (_, expr, _) in trans.iteritems():
+    if tran == expr:
+        print(tran)
