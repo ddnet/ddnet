8f5812decdaf9405899f1eb6d2b753af7f197b29
def <dennis@felsin9.de>
2015-07-26 15:23:09 +0200

    Version 8.0

--- src/game/version.h
+++ src/game/version.h
@@ -3,8 +3,8 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #include "generated/nethash.cpp"
-#define GAME_VERSION "0.6.3, 7.9.1"
+#define GAME_VERSION "0.6.3, 8.0"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
-static const char GAME_RELEASE_VERSION[8] = "7.9.1";
-#define CLIENT_VERSIONNR 709
+static const char GAME_RELEASE_VERSION[8] = "8.0";
+#define CLIENT_VERSIONNR 800
 #endif
