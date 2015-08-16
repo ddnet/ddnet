51dda4e72b8ed5a94948a7eb835545fcec11454f
def <dennis@felsin9.de>
2015-08-16 19:11:39 +0200

    Fix aliasing warning

--- src/engine/shared/network_server.cpp
+++ src/engine/shared/network_server.cpp
@@ -120,7 +120,7 @@ SECURITY_TOKEN CNetServer::GetToken(const NETADDR &Addr)
 	md5_append(&md5, (unsigned char*)&Addr, sizeof(Addr));
 
 	md5_finish(&md5, digest);
-	SecurityToken = *(SECURITY_TOKEN*)digest;
+	SecurityToken = ToSecurityToken(digest);
 
 	if (SecurityToken == NET_SECURITY_TOKEN_UNKNOWN ||
 		SecurityToken == NET_SECURITY_TOKEN_UNSUPPORTED)
