ef94797c021a3e767c886488d4c2a00eab9d9ff5
Tim Schumacher <tim@timakro.de>
2016-02-09 17:02:14 +0100

    forbid placing hookthrough shortcut in front layer

--- src/game/editor/layer_tiles.cpp
+++ src/game/editor/layer_tiles.cpp
@@ -1296,7 +1296,7 @@ CTile CLayerFront::GetTile(int x, int y, bool force)
 
 void CLayerFront::SetTile(int x, int y, CTile tile, bool force)
 {
-	if(force || GetTile(x, y, true).m_Index != TILE_THROUGH_CUT)
+	if(force || (GetTile(x, y, true).m_Index != TILE_THROUGH_CUT && tile.m_Index != TILE_THROUGH_CUT))
 		m_pTiles[y*m_Width+x] = tile;
 }
 
