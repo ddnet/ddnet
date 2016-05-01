aeea3777a31cf042865c962d3a3a706ff9bba1b5
Tim Schumacher <tim@timakro.de>
2016-05-01 18:08:07 +0200

    show button to mapper when placing unused tiles

--- src/game/editor/layer_tiles.cpp
+++ src/game/editor/layer_tiles.cpp
@@ -1318,6 +1318,11 @@ void CLayerFront::SetTile(int x, int y, CTile tile)
 	} else {
 		CTile air = {TILE_AIR};
 		CLayerTiles::SetTile(x, y, air);
+		if(!m_pEditor->m_PreventUnusedTilesWasWarned) {
+			m_pEditor->m_PopupEventType = m_pEditor->POPEVENT_PREVENTUNUSEDTILES;
+			m_pEditor->m_PopupEventActivated = true;
+			m_pEditor->m_PreventUnusedTilesWasWarned = true;
+		}
 	}
 }
 
