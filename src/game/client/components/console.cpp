1d98ff031ccbad89ebec9e6de3ee3dabe0df8f04
def <dennis@felsin9.de>
2016-05-22 02:12:52 +0200

    Immediately accept input while console is still opening (fixes #484)

--- src/game/client/components/console.cpp
+++ src/game/client/components/console.cpp
@@ -609,7 +609,7 @@ void CGameConsole::OnMessage(int MsgType, void *pRawMsg)
 
 bool CGameConsole::OnInput(IInput::CEvent Event)
 {
-	if(m_ConsoleState != CONSOLE_OPEN)
+	if(m_ConsoleState != CONSOLE_OPEN && m_ConsoleState != CONSOLE_OPENING)
 		return false;
 	if((Event.m_Key >= KEY_F1 && Event.m_Key <= KEY_F12) || (Event.m_Key >= KEY_F13 && Event.m_Key <= KEY_F24))
 		return false;
