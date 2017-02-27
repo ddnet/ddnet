b7539e53e15f9fa93b2fc3e3a7b4aecebfd85dd0
Learath2 <learath2@gmail.com>
2017-02-28 01:23:10 +0300

    Actually use the color chosen

--- src/game/client/components/chat.cpp
+++ src/game/client/components/chat.cpp
@@ -772,8 +772,10 @@ void CChat::OnRender()
 			TextRender()->TextColor(0.7f, 0.7f, 1.0f, Blend); // blue
 		else if(m_aLines[r].m_NameColor == TEAM_SPECTATORS)
 			TextRender()->TextColor(0.75f, 0.5f, 0.75f, Blend); // spectator
-		else if(m_aLines[r].m_NameColor == 2)
-			TextRender()->TextColor(1.0f, 0.1f, 0.1f, Blend); // red
+		else if(m_aLines[r].m_NameColor == 2){
+			vec3 rgb = HslToRgb(vec3(g_Config.m_ClMessageFriendHue / 255.0f, g_Config.m_ClMessageFriendSat / 255.0f, g_Config.m_ClMessageFriendLht / 255.0f));
+			TextRender()->TextColor(rgb.r, rgb.g, rgb.b, Blend);
+		}
 		else if(m_aLines[r].m_ClientID >= 0 && g_Config.m_ClChatTeamColors && m_pClient->m_Teams.Team(m_aLines[r].m_ClientID))
 		{
 			vec3 rgb = HslToRgb(vec3(m_pClient->m_Teams.Team(m_aLines[r].m_ClientID) / 64.0f, 1.0f, 0.75f));
