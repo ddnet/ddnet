84726547aa31b095c0342affaf9f406235b9a5a3
Ryozuki <clud6565@gmail.com>
2017-03-12 14:49:18 +0100

    Fix warning also for server.cpp, and fixed client.cpp

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -1,8 +1,9 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
-#include <new>
 
 #define _WIN32_WINNT 0x0501
+
+#include <new>
 
 #include <time.h>
 #include <stdlib.h> // qsort
