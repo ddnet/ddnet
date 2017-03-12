bfd6c5ab98f1df59306ceff28efbb9b10344c1cb
Ryozuki <clud6565@gmail.com>
2017-03-12 12:12:39 +0100

    Fix some warnings

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -55,7 +55,9 @@
 #include <engine/client/serverbrowser.h>
 
 #if defined(CONF_FAMILY_WINDOWS)
-	#define _WIN32_WINNT 0x0501
+	#ifndef _WIN32_WINNT
+		#define _WIN32_WINNT 0x0501
+	#endif
 	#define WIN32_LEAN_AND_MEAN
 	#include <windows.h>
 #endif
--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -37,7 +37,9 @@
 #include "server.h"
 
 #if defined(CONF_FAMILY_WINDOWS)
-	#define _WIN32_WINNT 0x0501
+	#ifndef _WIN32_WINNT
+		#define _WIN32_WINNT 0x0501
+	#endif
 	#define WIN32_LEAN_AND_MEAN
 	#include <windows.h>
 #endif
