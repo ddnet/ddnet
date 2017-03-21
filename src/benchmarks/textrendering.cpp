b7e555b9c34800611aa08e98445c5842d3b94550
necropotame <necropotame@gmail.com>
2017-03-21 08:47:36 +0100

    Changes after heinrich5991's review of #685 - Fix code style

--- src/benchmarks/textrendering.cpp
+++ src/benchmarks/textrendering.cpp
@@ -18,7 +18,7 @@ int main(int argc, const char **argv) // ignore_convention
 #endif
 	dbg_logger_stdout();
 	
-	IKernel* pKernel = IKernel::Create();
+	IKernel *pKernel = IKernel::Create();
 
 	// init SDL
 	{
@@ -32,7 +32,7 @@ int main(int argc, const char **argv) // ignore_convention
 	}
 
 	// init graphics
-	IEngineGraphics* pGraphics = CreateEngineGraphicsThreaded();
+	IEngineGraphics *pGraphics = CreateEngineGraphicsThreaded();
 	pKernel->RegisterInterface(static_cast<IEngineGraphics*>(pGraphics));
 	pKernel->RegisterInterface(static_cast<IGraphics*>(pGraphics));
 	if(pGraphics->Init() != 0)
@@ -42,12 +42,12 @@ int main(int argc, const char **argv) // ignore_convention
 	}
 	
 	// init textrender
-	IEngineTextRender* pTextRender = CreateEngineTextRender();
+	IEngineTextRender *pTextRender = CreateEngineTextRender();
 	pKernel->RegisterInterface(static_cast<IEngineTextRender*>(pTextRender));
 	pKernel->RegisterInterface(static_cast<ITextRender*>(pTextRender));
 	pTextRender->Init();
 	
-	static CFont *pDefaultFont = pTextRender->LoadFont("data/fonts/DejaVuSansCJKName.ttf");
+	CFont *pDefaultFont = pTextRender->LoadFont("data/fonts/DejaVuSansCJKName.ttf");
 	pTextRender->SetDefaultFont(pDefaultFont);
 	
 	pGraphics->BlendNormal();
@@ -55,7 +55,7 @@ int main(int argc, const char **argv) // ignore_convention
 	dbg_msg("benchmark", "ready!");
 	
 	// text from https://fr.wikibooks.org/wiki/Translinguisme/Par_expression/Bonjour
-	static const char aaText[][1024] =
+	static const char aaText[][32] =
 	{
 		"azertyuiop",
 		"ըթժիլխծկհձ",
@@ -81,7 +81,7 @@ int main(int argc, const char **argv) // ignore_convention
 		"כלםמןנסעףפ",
 		"AZERTYUIOP",
 	};
-	static const int NbText = sizeof(aaText)/sizeof(aaText[0]);
+	static const int NbText = sizeof(aaText) / sizeof(aaText[0]);
 	static const int Duration = 20; // in seconds
 	
 	float Fps = 0.0f;
@@ -92,28 +92,28 @@ int main(int argc, const char **argv) // ignore_convention
 		while(1)
 		{
 			int64 CurrentTime = time_get();
-			double RenderTime = 0.1*(CurrentTime - StartTime)/(double)time_freq();
+			double RenderTime = 0.1 * (CurrentTime - StartTime) / (double) time_freq();
 
 			pGraphics->Clear(0.5f, 0.5f, 0.5f);
 			
 			float Height = 300.0f;
-			float Width = Height*pGraphics->ScreenAspect();
+			float Width = Height * pGraphics->ScreenAspect();
 			pGraphics->MapScreen(0.0f, 0.0f, Width, Height);
 			
-			for(int j=0; j<3; j++)
+			for(int j = 0; j < 3; j++)
 			{
-				for(int i=0; i<NbText; i++)
+				for(int i=0; i < NbText; i++)
 				{
-					float TimeWrap = fmod(RenderTime + i/(double)(NbText), 1.0f);
+					float TimeWrap = fmod(RenderTime + i / (double) (NbText), 1.0f);
 					
-					float PositionX = j*Width/3.0f;
-					float PositionY = TimeWrap*Height*0.8f;
-					float Size = 8+TimeWrap*40.0;
+					float PositionX = j * Width / 3.0f;
+					float PositionY = TimeWrap * Height * 0.8f;
+					float Size = 8 + TimeWrap * 40.0;
 					
 					CTextCursor Cursor;
 					pTextRender->SetCursor(&Cursor, PositionX, PositionY, Size, TEXTFLAG_RENDER|TEXTFLAG_STOP_AT_END);
 					Cursor.m_LineWidth = Width;
-					pTextRender->TextEx(&Cursor, aaText[i%NbText], -1);
+					pTextRender->TextEx(&Cursor, aaText[i % NbText], -1);
 				}
 			}
 			
@@ -121,16 +121,16 @@ int main(int argc, const char **argv) // ignore_convention
 			
 			FrameCounter++;
 			int64 TimeDiff = CurrentTime - StartTime;
-			if(TimeDiff > time_freq()*Duration)
+			if(TimeDiff > time_freq() * Duration)
 			{
-				double Time = TimeDiff/(double)time_freq();
+				double Time = TimeDiff / (double) time_freq();
 				Fps = (double)FrameCounter / Time;
 				break;
 			}
 		}
 	}
 
-	dbg_msg("Benchmark", "Result: %f", Fps);
+	dbg_msg("Benchmark", "Result: %f fps", Fps);
 
 	delete pTextRender;
 	delete pGraphics;
