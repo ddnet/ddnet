9f93450e7b5fc1ce964ffda6b0cf7313c3a24e96
HMH <henry@freedesk.net>
2017-03-23 22:28:06 +0100

    Fix style in some places which the previous fix missed

--- src/game/server/score/sql_score.cpp
+++ src/game/server/score/sql_score.cpp
@@ -403,7 +403,7 @@ bool CSqlScore::MapInfoThread(CSqlServer* pSqlServer, const CSqlData *pGameData,
 			char pReleasedString[60] = "\0";
 			if(stamp != 0)
 			{
-				sqlstr::agoTimeToString(ago, pAgoString);
+				sqlstr::AgoTimeToString(ago, pAgoString);
 				str_format(pReleasedString, sizeof(pReleasedString), ", released %s ago", pAgoString);
 			}
 
@@ -476,7 +476,7 @@ bool CSqlScore::SaveScoreThread(CSqlServer* pSqlServer, const CSqlData *pGameDat
 			dbg_msg("sql", "ERROR: Could not save Score, writing insert to a file now...");
 
 			char aTimestamp [20];
-			sqlstr::getTimeStamp(aTimestamp, sizeof(aTimestamp));
+			sqlstr::GetTimeStamp(aTimestamp, sizeof(aTimestamp));
 
 			char aBuf[768];
 				str_format(aBuf, sizeof(aBuf), "INSERT IGNORE INTO %%s_race(Map, Name, Timestamp, Time, Server, cp1, cp2, cp3, cp4, cp5, cp6, cp7, cp8, cp9, cp10, cp11, cp12, cp13, cp14, cp15, cp16, cp17, cp18, cp19, cp20, cp21, cp22, cp23, cp24, cp25) VALUES ('%s', '%s', '%s', '%.2f', '%s', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f', '%.2f');", pData->m_Map.ClrStr(), pData->m_Name.ClrStr(), aTimestamp, pData->m_Time, g_Config.m_SvSqlServerName, pData->m_aCpCurrent[0], pData->m_aCpCurrent[1], pData->m_aCpCurrent[2], pData->m_aCpCurrent[3], pData->m_aCpCurrent[4], pData->m_aCpCurrent[5], pData->m_aCpCurrent[6], pData->m_aCpCurrent[7], pData->m_aCpCurrent[8], pData->m_aCpCurrent[9], pData->m_aCpCurrent[10], pData->m_aCpCurrent[11], pData->m_aCpCurrent[12], pData->m_aCpCurrent[13], pData->m_aCpCurrent[14], pData->m_aCpCurrent[15], pData->m_aCpCurrent[16], pData->m_aCpCurrent[17], pData->m_aCpCurrent[18], pData->m_aCpCurrent[19], pData->m_aCpCurrent[20], pData->m_aCpCurrent[21], pData->m_aCpCurrent[22], pData->m_aCpCurrent[23], pData->m_aCpCurrent[24]);
@@ -577,7 +577,7 @@ bool CSqlScore::SaveTeamScoreThread(CSqlServer* pSqlServer, const CSqlData *pGam
 			io_write_newline(File);
 
 			char aTimestamp [20];
-			sqlstr::getTimeStamp(aTimestamp, sizeof(aTimestamp));
+			sqlstr::GetTimeStamp(aTimestamp, sizeof(aTimestamp));
 
 			char aBuf[2300];
 			for(unsigned int i = 0; i < pData->m_Size; i++)
@@ -1076,7 +1076,7 @@ bool CSqlScore::ShowTimesThread(CSqlServer* pSqlServer, const CSqlData *pGameDat
 			pStamp = (int)pSqlServer->GetResults()->getInt("Stamp");
 			pTime = (float)pSqlServer->GetResults()->getDouble("Time");
 
-			sqlstr::agoTimeToString(pSince,pAgoString);
+			sqlstr::AgoTimeToString(pSince,pAgoString);
 
 			if(pData->m_Search) // last 5 times of a player
 			{
