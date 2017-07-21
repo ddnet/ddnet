f352daf00dae26da977e1a0f6b13d259c9b2f275
def <dennis@felsin9.de>
2017-07-21 19:23:15 +0200

    Allow moving mouse while spectating and typing (fixes #798)

--- src/game/client/components/controls.cpp
+++ src/game/client/components/controls.cpp
@@ -486,8 +486,7 @@ void CControls::OnRender()
 
 bool CControls::OnMouseMove(float x, float y)
 {
-	if((m_pClient->m_Snap.m_pGameInfoObj && m_pClient->m_Snap.m_pGameInfoObj->m_GameStateFlags&GAMESTATEFLAG_PAUSED) ||
-		(m_pClient->m_Snap.m_SpecInfo.m_Active && m_pClient->m_pChat->IsActive()))
+	if((m_pClient->m_Snap.m_pGameInfoObj && m_pClient->m_Snap.m_pGameInfoObj->m_GameStateFlags&GAMESTATEFLAG_PAUSED))
 		return false;
 
 #if defined(__ANDROID__) // No relative mouse on Android
