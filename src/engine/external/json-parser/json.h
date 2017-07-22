2fb58359802bac123d51ae9cae754caee1e93c9d
def <dennis@felsin9.de>
2017-07-22 11:07:17 +0200

    Update json-parser to 1.1.0, zlib to 1.2.11

--- src/engine/external/json-parser/json.h
+++ src/engine/external/json-parser/json.h
@@ -1,7 +1,7 @@
 
-/* vim: set et ts=3 sw=3 ft=c:
+/* vim: set et ts=3 sw=3 sts=3 ft=c:
  *
- * Copyright (C) 2012 James McLaughlin et al.  All rights reserved.
+ * Copyright (C) 2012, 2013, 2014 James McLaughlin et al.  All rights reserved.
  * https://github.com/udp/json-parser
  *
  * Redistribution and use in source and binary forms, with or without
@@ -31,15 +31,20 @@
 #ifndef _JSON_H
 #define _JSON_H
 
-#ifdef _MSC_VER
-   #define inline __inline
-#endif
-
-#include <string.h>
-
 #ifndef json_char
    #define json_char char
 #endif
+
+#ifndef json_int_t
+   #ifndef _MSC_VER
+      #include <inttypes.h>
+      #define json_int_t int64_t
+   #else
+      #define json_int_t __int64
+   #endif
+#endif
+
+#include <stdlib.h>
 
 #ifdef __cplusplus
 
@@ -55,9 +60,19 @@ typedef struct
    unsigned long max_memory;
    int settings;
 
+   /* Custom allocator support (leave null to use malloc/free)
+    */
+
+   void * (* mem_alloc) (size_t, int zero, void * user_data);
+   void (* mem_free) (void *, void * user_data);
+
+   void * user_data;  /* will be passed to mem_alloc and mem_free */
+
+   size_t value_extra;  /* how much extra space to allocate for values? */
+
 } json_settings;
 
-#define json_relaxed_commas 1
+#define json_enable_comments  0x01
 
 typedef enum
 {
@@ -83,7 +98,7 @@ typedef struct _json_value
    union
    {
       int boolean;
-      long integer;
+      json_int_t integer;
       double dbl;
 
       struct
@@ -100,9 +115,20 @@ typedef struct _json_value
          struct
          {
             json_char * name;
+            unsigned int name_length;
+
             struct _json_value * value;
 
          } * values;
+
+         #if defined(__cplusplus) && __cplusplus >= 201103L
+         decltype(values) begin () const
+         {  return values;
+         }
+         decltype(values) end () const
+         {  return values + length;
+         }
+         #endif
 
       } object;
 
@@ -110,6 +136,15 @@ typedef struct _json_value
       {
          unsigned int length;
          struct _json_value ** values;
+
+         #if defined(__cplusplus) && __cplusplus >= 201103L
+         decltype(values) begin () const
+         {  return values;
+         }
+         decltype(values) end () const
+         {  return values + length;
+         }
+         #endif
 
       } array;
 
@@ -121,6 +156,14 @@ typedef struct _json_value
       void * object_mem;
 
    } _reserved;
+
+   #ifdef JSON_TRACK_SOURCE
+
+      /* Location of the value in the source JSON
+       */
+      unsigned int line, col;
+
+   #endif
 
 
    /* Some C++ operator sugar */
@@ -145,7 +188,7 @@ typedef struct _json_value
          }
 
          inline const struct _json_value &operator [] (const char * index) const
-         {
+         { 
             if (type != json_object)
                return json_value_none;
 
@@ -157,7 +200,7 @@ typedef struct _json_value
          }
 
          inline operator const char * () const
-         {
+         {  
             switch (type)
             {
                case json_string:
@@ -168,30 +211,69 @@ typedef struct _json_value
             };
          }
 
-         inline operator long () const
-         {  return u.integer;
+         inline operator json_int_t () const
+         {  
+            switch (type)
+            {
+               case json_integer:
+                  return u.integer;
+
+               case json_double:
+                  return (json_int_t) u.dbl;
+
+               default:
+                  return 0;
+            };
          }
 
          inline operator bool () const
-         {  return u.boolean != 0;
+         {  
+            if (type != json_boolean)
+               return false;
+
+            return u.boolean != 0;
+         }
+
+         inline operator double () const
+         {  
+            switch (type)
+            {
+               case json_integer:
+                  return (double) u.integer;
+
+               case json_double:
+                  return u.dbl;
+
+               default:
+                  return 0;
+            };
          }
 
    #endif
 
 } json_value;
 
-json_value * json_parse
-   (const json_char * json);
+json_value * json_parse (const json_char * json,
+                         size_t length);
 
-json_value * json_parse_ex
-   (json_settings * settings, const json_char * json, char * error);
+#define json_error_max 128
+json_value * json_parse_ex (json_settings * settings,
+                            const json_char * json,
+                            size_t length,
+                            char * error);
 
 void json_value_free (json_value *);
 
+
+/* Not usually necessary, unless you used a custom mem_alloc and now want to
+ * use a custom mem_free.
+ */
+void json_value_free_ex (json_settings * settings,
+                         json_value *);
+
+/* DDNet additions */
 const struct _json_value *json_object_get (const json_value * object, const char * index);
-
 const struct _json_value *json_array_get (const json_value * array, int index);
-
 int json_array_length (const json_value * array);
 const char * json_string_get (const json_value * string);
 int json_int_get (const json_value * integer);
@@ -202,4 +284,5 @@ int json_boolean_get(const json_value * boolean);
 #endif
 
 #endif
+
 
