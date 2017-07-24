147005ea428df22b435461906b080c48aaefbdbe
def <dennis@felsin9.de>
2017-07-24 18:33:11 +0200

    ask before overwriting demo file when slicing (fixes #806)

--- src/game/client/components/menus_demo.cpp
+++ src/game/client/components/menus_demo.cpp
@@ -122,19 +122,29 @@ void CMenus::RenderDemoPlayer(CUIRect MainView)
 		static int s_ButtonOk = 0;
 		if(DoButton_Menu(&s_ButtonOk, Localize("Ok"), 0, &Ok) || m_EnterPressed)
 		{
-			if (str_comp(m_lDemos[m_DemolistSelectedIndex].m_aFilename, m_aCurrentDemoFile) == 0)
+			if(str_comp(m_lDemos[m_DemolistSelectedIndex].m_aFilename, m_aCurrentDemoFile) == 0)
 				str_copy(m_aDemoPlayerPopupHint, Localize("Please use a different name"), sizeof(m_aDemoPlayerPopupHint));
 			else
 			{
-				m_DemoPlayerState = DEMOPLAYER_NONE;
-
 				int len = str_length(m_aCurrentDemoFile);
 				if(len < 5 || str_comp_nocase(&m_aCurrentDemoFile[len-5], ".demo"))
 					str_append(m_aCurrentDemoFile, ".demo", sizeof(m_aCurrentDemoFile));
 
 				char aPath[512];
 				str_format(aPath, sizeof(aPath), "%s/%s", m_aCurrentDemoFolder, m_aCurrentDemoFile);
-				Client()->DemoSlice(aPath, CMenus::DemoFilterChat, &s_RemoveChat);
+
+				IOHANDLE DemoFile = Storage()->OpenFile(aPath, IOFLAG_READ, IStorage::TYPE_SAVE);
+				const char* pStr = Localize("File already exists, do you want to overwrite it?");
+				if(DemoFile && str_comp_num(m_aDemoPlayerPopupHint, pStr, sizeof(m_aDemoPlayerPopupHint)) != 0)
+				{
+					io_close(DemoFile);
+					str_copy(m_aDemoPlayerPopupHint, pStr, sizeof(m_aDemoPlayerPopupHint));
+				}
+				else
+				{
+					m_DemoPlayerState = DEMOPLAYER_NONE;
+					Client()->DemoSlice(aPath, CMenus::DemoFilterChat, &s_RemoveChat);
+				}
 			}
 		}
 
@@ -160,7 +170,10 @@ void CMenus::RenderDemoPlayer(CUIRect MainView)
 		TextBox.VSplitRight(60.0f, &TextBox, 0);
 		UI()->DoLabel(&Label, Localize("New name:"), 18.0f, -1);
 		static float Offset = 0.0f;
-		DoEditBox(&Offset, &TextBox, m_aCurrentDemoFile, sizeof(m_aCurrentDemoFile), 12.0f, &Offset);
+		if(DoEditBox(&Offset, &TextBox, m_aCurrentDemoFile, sizeof(m_aCurrentDemoFile), 12.0f, &Offset))
+		{
+			m_aDemoPlayerPopupHint[0] = '\0';
+		}
 	}
 
 	// handle mousewheel independent of active menu
