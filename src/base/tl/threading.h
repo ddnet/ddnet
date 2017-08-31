1062d5359ac7d128124918d925c6e968b0830818
Learath2 <learath2@gmail.com>
2017-08-31 12:30:42 +0200

    Rename semaphore_* to sphore_*
    Previous naming conflicted with Mach kernel definitions.

--- src/base/tl/threading.h
+++ src/base/tl/threading.h
@@ -70,10 +70,10 @@
 	{
 		SEMAPHORE sem;
 	public:
-		semaphore() { semaphore_init(&sem); }
-		~semaphore() { semaphore_destroy(&sem); }
-		void wait() { semaphore_wait(&sem); }
-		void signal() { semaphore_signal(&sem); }
+		semaphore() { sphore_init(&sem); }
+		~semaphore() { sphore_destroy(&sem); }
+		void wait() { sphore_wait(&sem); }
+		void signal() { sphore_signal(&sem); }
 	};
 #endif
 
