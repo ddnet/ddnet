14e1e929add3d0982ed92abbd7a3c956ff72ccf9
Jupeyy <jupjopjap@gmail.com>
2017-11-27 02:56:22 +0100

    direct initialize f

--- src/engine/client/opengl_sl.cpp
+++ src/engine/client/opengl_sl.cpp
@@ -7,9 +7,9 @@
 
 bool CGLSL::LoadShader(IStorage* pStorage, const char* pFile, int Type)
 {
-	if (m_IsLoaded) return true;
-	IOHANDLE f;
-	f = pStorage->OpenFile(pFile, IOFLAG_READ, IStorage::TYPE_ALL);
+	if (m_IsLoaded)
+		return true;
+	IOHANDLE f = pStorage->OpenFile(pFile, IOFLAG_READ, IStorage::TYPE_ALL);
 	
 	std::vector<std::string> Lines;
 	if (f)
