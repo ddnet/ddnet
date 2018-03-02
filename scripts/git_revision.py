b285d777f3d26b073a40a7548a1988408c1eb34a
heinrich5991 <heinrich5991@gmail.com>
2018-03-02 03:22:44 +0100

    Python 2 support for `scripts/git_revision.py` on error /o\

--- scripts/git_revision.py
+++ scripts/git_revision.py
@@ -1,7 +1,16 @@
+import errno
 import subprocess
+try:
+	FileNotFoundError
+except NameError:
+	FileNotFoundError = OSError
 try:
 	git_hash = subprocess.check_output(["git", "rev-parse", "--short=16", "HEAD"], stderr=None).decode().strip()
 	definition = '"{}"'.format(git_hash)
-except (FileNotFoundError, subprocess.CalledProcessError):
+except FileNotFoundError as e:
+	if e.errno != errno.ENOENT:
+		raise
+	definition = "0"
+except subprocess.CalledProcessError:
 	definition = "0";
 print("const char *GIT_SHORTREV_HASH = {};".format(definition))
