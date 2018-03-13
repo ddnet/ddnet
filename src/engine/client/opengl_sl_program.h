75eddfec7aa89780634a8e441c9e064d2d5e5eb4
Jupeyy <jupjopjap@gmail.com>
2018-03-13 21:47:45 +0100

    add new shader programs

--- src/engine/client/opengl_sl_program.h
+++ src/engine/client/opengl_sl_program.h
@@ -40,26 +40,92 @@ protected:
 };
 
 class CGLSLTWProgram : public CGLSLProgram {
-public:	
+public:
+	CGLSLTWProgram() : m_LastTextureSampler(-1), m_LastIsTextured(-1) {}
+
 	int m_LocPos;
 	int m_LocIsTextured;
 	int m_LocTextureSampler;
+
+	int m_LastTextureSampler;
+	int m_LastIsTextured;
 };
 
-class CGLSLQuadProgram : public CGLSLTWProgram {
+class CGLSLTextProgram : public CGLSLTWProgram {
 public:
-	
+	CGLSLTextProgram() : CGLSLTWProgram()
+	{
+		m_LastColor[0] = m_LastColor[1] = m_LastColor[2] = m_LastColor[3] = -1.f;
+		m_LastOutlineColor[0] = m_LastOutlineColor[1] = m_LastOutlineColor[2] = m_LastOutlineColor[3] = -1.f;
+		m_LastTextSampler = m_LastTextOutlineSampler = -1;
+		m_LastTextureSize = -1;
+	}
+
+	int m_LocColor;
+	int m_LocOutlineColor;
+	int m_LocTextSampler;
+	int m_LocTextOutlineSampler;
+	int m_LocTextureSize;
+
+	float m_LastColor[4];
+	float m_LastOutlineColor[4];
+	int m_LastTextSampler;
+	int m_LastTextOutlineSampler;
+	int m_LastTextureSize;
 };
 
 class CGLSLPrimitiveProgram : public CGLSLTWProgram {
 public:
-	
+};
+
+class CGLSLSpriteProgram : public CGLSLTWProgram {
+public:
+	CGLSLSpriteProgram() : CGLSLTWProgram()
+	{
+		m_LastRotation = 0.f;
+		m_LastCenter[0] = m_LastCenter[1] = 0.f;
+		m_LastVertciesColor[0] = m_LastVertciesColor[1] = m_LastVertciesColor[2] = m_LastVertciesColor[3] = -1.f;
+	}
+
+	int m_LocRotation;
+	int m_LocCenter;
+	int m_LocVertciesColor;
+
+	float m_LastRotation;
+	float m_LastCenter[2];
+	float m_LastVertciesColor[4];
+};
+
+class CGLSLSpriteMultipleProgram : public CGLSLTWProgram {
+public:
+	CGLSLSpriteMultipleProgram() : CGLSLTWProgram()
+	{
+		m_LastCenter[0] = m_LastCenter[1] = 0.f;
+		m_LastVertciesColor[0] = m_LastVertciesColor[1] = m_LastVertciesColor[2] = m_LastVertciesColor[3] = -1.f;
+	}
+
+	int m_LocRSP;
+	int m_LocCenter;
+	int m_LocVertciesColor;
+
+	float m_LastCenter[2];
+	float m_LastVertciesColor[4];
+};
+
+class CGLSLQuadProgram : public CGLSLTWProgram {
+public:
+	int m_LocColor;
+	int m_LocOffset;
+	int m_LocRotation;
 };
 
 class CGLSLTileProgram : public CGLSLTWProgram {
 public:
 	int m_LocColor;
 	int m_LocLOD;
+	int m_LocTexelOffset;
+
+	int m_LastLOD;
 };
 
 class CGLSLBorderTileProgram : public CGLSLTileProgram {
