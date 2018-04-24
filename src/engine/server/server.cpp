3f7b18322c407767e428175098bd009831a51320
def <dennis@felsin9.de>
2018-04-24 23:07:51 +0200

    Set up DB by default

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -2504,7 +2504,7 @@ void CServer::ConAddSqlServer(IConsole::IResult *pResult, void *pUserData)
 		return;
 	}
 
-	bool SetUpDb = pResult->NumArguments() == 8 ? pResult->GetInteger(7) : false;
+	bool SetUpDb = pResult->NumArguments() == 8 ? pResult->GetInteger(7) : true;
 
 	CSqlServer** apSqlServers = ReadOnly ? pSelf->m_apSqlReadServers : pSelf->m_apSqlWriteServers;
 
