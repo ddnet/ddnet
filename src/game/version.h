05c41efcea13e4c881f88d86ad99b542d0765ccf
Dennis Felsing <dennis@felsin9.de>
2018-05-07 08:29:27 +0200

    Version 11.1.5

--- src/game/version.h
+++ src/game/version.h
@@ -2,9 +2,9 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-#define GAME_VERSION "0.6.4, 11.1.4"
+#define GAME_VERSION "0.6.4, 11.1.5"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
-#define GAME_RELEASE_VERSION "11.1.4"
-#define CLIENT_VERSIONNR 11014
+#define GAME_RELEASE_VERSION "11.1.5"
+#define CLIENT_VERSIONNR 11015
 extern const char *GIT_SHORTREV_HASH;
 #endif
