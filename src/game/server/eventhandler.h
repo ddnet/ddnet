d539331c644aadbadba0f41db0589af24a8ed722
Ryozuki <edgar@ryobyte.com>
2018-05-29 09:01:35 +0200

    use includes first in msc in eventhandler.h

--- src/game/server/eventhandler.h
+++ src/game/server/eventhandler.h
@@ -4,10 +4,14 @@
 #define GAME_SERVER_EVENTHANDLER_H
 
 #ifdef _MSC_VER
-typedef __int32 int32_t;
-typedef unsigned __int32 uint32_t;
-typedef __int64 int64_t;
-typedef unsigned __int64 uint64_t;
+	#if _MSC_VER >= 1600
+		#include <cstdint>
+	#else
+		typedef __int32 int32_t;
+		typedef unsigned __int32 uint32_t;
+		typedef __int64 int64_t;
+		typedef unsigned __int64 uint64_t;
+	#endif
 #else
 #include <stdint.h>
 #endif
