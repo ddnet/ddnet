69667d4537985e908d7c307a33dc34700f251bb8
Dennis Felsing <dennis@felsin9.de>
2018-05-29 09:15:00 +0200

    Also use cstdint on non-MSVC instead of stdint.h

--- src/game/server/eventhandler.h
+++ src/game/server/eventhandler.h
@@ -3,17 +3,13 @@
 #ifndef GAME_SERVER_EVENTHANDLER_H
 #define GAME_SERVER_EVENTHANDLER_H
 
-#ifdef _MSC_VER
-	#if _MSC_VER >= 1600
-		#include <cstdint>
-	#else
-		typedef __int32 int32_t;
-		typedef unsigned __int32 uint32_t;
-		typedef __int64 int64_t;
-		typedef unsigned __int64 uint64_t;
-	#endif
+#if !defined(_MSC_VER) || _MSC_VER >= 1600
+	#include <cstdint>
 #else
-#include <stdint.h>
+	typedef __int32 int32_t;
+	typedef unsigned __int32 uint32_t;
+	typedef __int64 int64_t;
+	typedef unsigned __int64 uint64_t;
 #endif
 //
 class CEventHandler
