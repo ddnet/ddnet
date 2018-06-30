af2d86765c0bb52444e2e90f50138b573adc97e5
def <dennis@felsin9.de>
2018-06-30 09:49:42 +0200

    Version 11.2.1

--- src/game/version.h
+++ src/game/version.h
@@ -2,9 +2,9 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-#define GAME_VERSION "0.6.4, 11.2"
+#define GAME_VERSION "0.6.4, 11.2.1"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
-#define GAME_RELEASE_VERSION "11.2"
-#define CLIENT_VERSIONNR 11020
+#define GAME_RELEASE_VERSION "11.2.1"
+#define CLIENT_VERSIONNR 11021
 extern const char *GIT_SHORTREV_HASH;
 #endif
