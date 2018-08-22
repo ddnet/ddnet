d60b0db63253f534fc49bd499f5850d7cd1dff55
Dennis Felsing <dennis@felsin9.de>
2018-08-22 08:33:21 +0200

    Revert "Add mapbug "stoppers-passthrough@ddnet.tw""
    This reverts commit 25e86851d328a0b58c1e8a21cc92812d5598ea55.

--- src/game/gamecore.cpp
+++ src/game/gamecore.cpp
@@ -497,7 +497,7 @@ void CCharacterCore::Tick(bool UseInput, bool IsClient)
 		m_Vel = normalize(m_Vel) * 6000;
 }
 
-void CCharacterCore::Move(bool BugStoppersPassthrough)
+void CCharacterCore::Move()
 {
 	float RampValue = VelocityRamp(length(m_Vel)*50, m_pWorld->m_Tuning[g_Config.m_ClDummy].m_VelrampStart, m_pWorld->m_Tuning[g_Config.m_ClDummy].m_VelrampRange, m_pWorld->m_Tuning[g_Config.m_ClDummy].m_VelrampCurvature);
 
@@ -506,7 +506,7 @@ void CCharacterCore::Move(bool BugStoppersPassthrough)
 	vec2 NewPos = m_Pos;
 
 	vec2 OldVel = m_Vel;
-	m_pCollision->MoveBox(IsSwitchActiveCb, this, &NewPos, &m_Vel, vec2(28.0f, 28.0f), 0.0f, !BugStoppersPassthrough);
+	m_pCollision->MoveBox(IsSwitchActiveCb, this, &NewPos, &m_Vel, vec2(28.0f, 28.0f), 0.0f, true);
 
 	m_Colliding = 0;
 	if(m_Vel.x < 0.001 && m_Vel.x > -0.001)
