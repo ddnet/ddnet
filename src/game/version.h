54cbda199942fb0cb90edf64c04ad2b33418799d
ChillerDragon <chillerdragon@gmail.com>
2018-12-08 20:58:52 +0100

    version 001

--- src/game/version.h
+++ src/game/version.h
@@ -5,6 +5,6 @@
 #define GAME_VERSION "0.6.4, 12.6.1"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 #define GAME_RELEASE_VERSION "12.6.1"
-#define CLIENT_VERSIONNR 12061
+#define CLIENT_VERSIONNR 99001
 extern const char *GIT_SHORTREV_HASH;
 #endif
