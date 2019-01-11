725ec200ac0c94c22ae383b91e30208e1356fd05
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2019-01-11 23:28:39 +0100

    merge blockworlds support into IsDDNet

--- src/engine/serverbrowser.h
+++ src/engine/serverbrowser.h
@@ -70,7 +70,6 @@ bool IsRace(const CServerInfo *pInfo);
 bool IsFastCap(const CServerInfo *pInfo);
 bool IsDDRace(const CServerInfo *pInfo);
 bool IsDDNet(const CServerInfo *pInfo);
-bool IsBlockWorlds(const CServerInfo *pInfo);
 
 bool Is64Player(const CServerInfo *pInfo);
 bool IsPlus(const CServerInfo *pInfo);
