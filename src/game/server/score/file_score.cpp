279d6e60cac49bcba5b38410f640ffab939691d8
def <dennis@felsin9.de>
2019-04-10 22:05:44 +0200

    No duplicate () on rank

--- src/game/server/score/file_score.cpp
+++ src/game/server/score/file_score.cpp
@@ -268,9 +268,6 @@ void CFileScore::ShowRank(int ClientID, const char* pName, bool Search)
 	if (pScore && Pos > -1)
 	{
 		float Time = pScore->m_Score;
-		char aClientName[128];
-		str_format(aClientName, sizeof(aClientName), " (%s)",
-				Server()->ClientName(ClientID));
 		if (g_Config.m_SvHideScore)
 			str_format(aBuf, sizeof(aBuf),
 					"Your time: %d minute(s) %5.2f second(s)", (int)Time / 60,
@@ -279,7 +276,7 @@ void CFileScore::ShowRank(int ClientID, const char* pName, bool Search)
 			str_format(aBuf, sizeof(aBuf),
 					"%d. %s Time: %d minute(s) %5.2f second(s), requested by (%s)", Pos,
 					pScore->m_aName, (int)Time / 60,
-					Time - ((int)Time / 60 * 60), aClientName);
+					Time - ((int)Time / 60 * 60), Server()->ClientName(ClientID));
 		if (!Search)
 			GameServer()->SendChat(-1, CGameContext::CHAT_ALL, aBuf, ClientID);
 		else
