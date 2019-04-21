6fc832d0ae4df9291cf009e9426c0ff7bcd12287
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2019-04-22 01:17:54 +0200

    fix time not showing when joining a server using filebase
    Needs to get set after LoadScore

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -1031,11 +1031,13 @@ void CGameContext::OnClientEnter(int ClientID)
 	m_apPlayers[ClientID]->Respawn();
 	// init the player
 	Score()->PlayerData(ClientID)->Reset();
-	m_apPlayers[ClientID]->m_Score = Score()->PlayerData(ClientID)->m_BestTime ? Score()->PlayerData(ClientID)->m_BestTime : -9999;
 
 	// Can't set score here as LoadScore() is threaded, run it in
 	// LoadScoreThreaded() instead
 	Score()->LoadScore(ClientID);
+
+        m_apPlayers[ClientID]->m_Score = Score()->PlayerData(ClientID)->m_BestTime ? Score()->PlayerData(ClientID)->m_BestTime : -9999;
+
 	Score()->CheckBirthday(ClientID);
 
 	{
