10e06f919ebee4d872f8a98c5c5355144a6b4348
d3fault <d3fault@d3fault.net>
2019-06-07 02:44:19 -0700

    respawn is now instant, but rate limited to 3 sec unless player clicks mouse1

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -901,7 +901,8 @@ void CCharacter::Die(int Killer, int Weapon)
 	// a nice sound
 	GameServer()->CreateSound(m_Pos, SOUND_PLAYER_DIE, Teams()->TeamMask(Team(), -1, m_pPlayer->GetCID()));
 
-	// this is for auto respawn after 3 secs
+	// this is to rate limit respawning to 3 secs
+	m_pPlayer->m_PreviousDieTick = m_pPlayer->m_DieTick;
 	m_pPlayer->m_DieTick = Server()->Tick();
 
 	m_Alive = false;
