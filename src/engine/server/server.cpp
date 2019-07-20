57197d990c7cf1b6ced67a31e57ce68a53e97a94
Emir Marincic <learath2@gmail.com>
2019-07-20 21:25:15 +0300

    Better represent the state of the server
    Co-Authored-By: heinrich5991 <heinrich5991@gmx.de>

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -1585,7 +1585,7 @@ void CServer::SendServerInfo(const NETADDR *pAddr, int Token, int Type, bool Sen
 	}
 
 	ADD_INT(p, PlayerCount); // num players
-	ADD_INT(p, maximum(MaxClients - g_Config.m_SvSpectatorSlots - g_Config.m_SvReservedSlots, PlayerCount)); // max players
+	ADD_INT(p, maximum(MaxClients - maximum(g_Config.m_SvSpectatorSlots, g_Config.m_SvReservedSlots), PlayerCount)); // max players
 	ADD_INT(p, ClientCount); // num clients
 	ADD_INT(p, maximum(MaxClients - g_Config.m_SvReservedSlots, ClientCount)); // max clients
 
