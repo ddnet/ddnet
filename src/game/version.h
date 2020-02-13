d89877603b83157c767322a74e3e2cf33da65c5c
def <dennis@felsin9.de>
2020-02-13 22:55:48 +0100

    Version 12.9.1

--- src/game/version.h
+++ src/game/version.h
@@ -2,9 +2,9 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-#define GAME_VERSION "0.6.4, 12.9"
+#define GAME_VERSION "0.6.4, 12.9.1"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
-#define GAME_RELEASE_VERSION "12.9"
-#define CLIENT_VERSIONNR 12090
+#define GAME_RELEASE_VERSION "12.9.1"
+#define CLIENT_VERSIONNR 12091
 extern const char *GIT_SHORTREV_HASH;
 #endif
