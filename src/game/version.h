1a87abe96b0f9c22f74b24f303416a533d9e9e19
def <dennis@felsin9.de>
2020-06-26 15:20:07 +0200

    Version 14.0.2

--- src/game/version.h
+++ src/game/version.h
@@ -2,10 +2,10 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-#define GAME_VERSION "0.6.4, 14.0.1"
+#define GAME_VERSION "0.6.4, 14.0.2"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 #define GAME_NAME "DDNet"
-#define GAME_RELEASE_VERSION "14.0.1"
-#define CLIENT_VERSIONNR 14001
+#define GAME_RELEASE_VERSION "14.0.2"
+#define CLIENT_VERSIONNR 14002
 extern const char *GIT_SHORTREV_HASH;
 #endif
