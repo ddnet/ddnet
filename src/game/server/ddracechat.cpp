4ceb4ddb9f52e45b9e641614d6cf951812149674
def <dennis@felsin9.de>
2020-08-08 14:09:36 +0200

    Version 14.3

--- src/game/server/ddracechat.cpp
+++ src/game/server/ddracechat.cpp
@@ -34,7 +34,9 @@ void CGameContext::ConCredits(IConsole::IResult *pResult, void *pUserData)
 	pSelf->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "credits",
 		"Bojidar, FallenKN, ardadem, archimede67, sirius1242, Aerll,");
 	pSelf->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "credits",
-		"trafilaw, Zwelf, Patiga, Konsti, ElXreno, MikiGamer & others.");
+		"trafilaw, Zwelf, Patiga, Konsti, ElXreno, MikiGamer,");
+	pSelf->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "credits",
+		"Fireball & others.");
 	pSelf->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "credits",
 		"Based on DDRace by the DDRace developers,");
 	pSelf->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "credits",
