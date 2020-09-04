3cc1e47ec4c06ab6bf077a94848e7505857bf3a7
def <dennis@felsin9.de>
2020-09-04 14:22:42 +0200

    Implement on-demand skin downloader (fixes #1166)
    Seems to work on first try, I'm somewhat surprised :D
    
    Future ideas (not for this change):
    
    - I haven't checked how the performance impact is of a new skin joining,
      might want to move the loading to a separate thread?
    - Use the skins from download directory instead of redownloading? Might
      want to compare modified time.
    - Make all skins load on demand only to save some memory?

--- src/game/client/components/skins.h
+++ src/game/client/components/skins.h
@@ -5,6 +5,7 @@
 #include <base/vmath.h>
 #include <base/color.h>
 #include <base/tl/sorted_array.h>
+#include <engine/client/http.h>
 #include <game/client/component.h>
 
 class CSkins : public CComponent
@@ -25,6 +26,12 @@ public:
 		bool operator==(const char *pOther) { return !str_comp(m_aName, pOther); }
 	};
 
+	struct CDownloadSkin : public CSkin
+	{
+		std::shared_ptr<CGetFile> m_pTask;
+		char m_aPath[MAX_PATH_LENGTH];
+	};
+
 	void OnInit();
 
 	int Num();
@@ -33,8 +40,10 @@ public:
 
 private:
 	sorted_array<CSkin> m_aSkins;
+	sorted_array<CDownloadSkin> m_aDownloadSkins;
 	char m_EventSkinPrefix[100];
 
+	int LoadSkin(const char *pName, const char *pPath, int DirType);
 	int FindImpl(const char *pName);
 	static int SkinScan(const char *pName, int IsDir, int DirType, void *pUser);
 };
