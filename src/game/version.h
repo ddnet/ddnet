eb725f800ea1c7328eb93e700577dac86bd08294
def <dennis@felsin9.de>
2020-09-24 12:19:48 +0200

    Allow specifying -DGAME_RELEASE_VERSION
    to be used for nightlies and release candidates to be able to tell them
    from regular releases more easily. For example:
    
    CPPFLAGS="-DGAME_RELEASE_VERSION=\\\"15.0.5-rc1\\\"" cmake .

--- src/game/version.h
+++ src/game/version.h
@@ -2,10 +2,12 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-#define GAME_VERSION "0.6.4, 15.0.5"
+#ifndef GAME_RELEASE_VERSION
+#define GAME_RELEASE_VERSION "15.0.5"
+#endif
+#define GAME_VERSION "0.6.4, " GAME_RELEASE_VERSION
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 #define GAME_NAME "DDNet"
-#define GAME_RELEASE_VERSION "15.0.5"
 #define CLIENT_VERSIONNR 15005
 extern const char *GIT_SHORTREV_HASH;
 #endif
