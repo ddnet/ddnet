db01e068d9b3d5555117fe9ff80c878ae46e9ad7
def <dennis@felsin9.de>
2020-09-24 10:46:19 +0200

    Add Mac OSX standalone client handling
    to find server executable

--- src/engine/shared/storage.cpp
+++ src/engine/shared/storage.cpp
@@ -259,7 +259,22 @@ public:
 					return;
 				}
 				else
+				{
+#if defined(CONF_PLATFORM_MACOSX)
+					str_append(m_aBinarydir, "/../../../DDNet-Server.app/Contents/MacOS", sizeof(m_aBinarydir));
+					str_format(aBuf, sizeof(aBuf), "%s/" PLAT_SERVER_EXEC, m_aBinarydir);
+					IOHANDLE File = io_open(aBuf, IOFLAG_READ);
+					if(File)
+					{
+						io_close(File);
+						return;
+					}
+					else
+						m_aBinarydir[0] = 0;
+#else
 					m_aBinarydir[0] = 0;
+#endif
+				}
 			}
 		}
 
