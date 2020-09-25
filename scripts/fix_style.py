678b2b15c39abd483cfe215cd749ea55e6206159
heinrich5991 <heinrich5991@gmail.com>
2020-09-26 00:58:58 +0200

    Change fix_style.py script to unconditionally format everything

--- scripts/fix_style.py
+++ scripts/fix_style.py
@@ -1,45 +1,39 @@
 #!/usr/bin/env python3
 
 from collections import defaultdict
+import os
 import re
 import subprocess
 import sys
 
-def git_get_changed_files(margin, base):
-	return subprocess.check_output([
-		"git",
-		"diff",
-		base,
-		"--name-only",
-	]).decode().splitlines()
+os.chdir(os.path.dirname(__file__) + "/..")
 
-def filter_cpp(changed_files):
-	return [filename for filename in changed_files
-		if any(filename.endswith(ext) for ext in ".c .cpp .h".split())]
-
-def reformat(changed_files):
-	for filename in changed_files:
-		subprocess.check_call(["clang-format", "-i", filename])
-
-def warn(changed_files):
-	result = 0
-	for filename in changed_files:
-		result = subprocess.call(["clang-format", "-Werror", "--dry-run", filename]) or result
+def recursive_file_list(path):
+	result = []
+	for dirpath, dirnames, filenames in os.walk(path):
+		result += [os.path.join(dirpath, filename) for filename in filenames]
 	return result
 
-def get_common_base(base):
-	return subprocess.check_output(["git", "merge-base", "HEAD", base]).decode().strip()
+def filter_cpp(filenames):
+	return [filename for filename in filenames
+		if any(filename.endswith(ext) for ext in ".c .cpp .h".split())]
+
+def reformat(filenames):
+	subprocess.check_call(["clang-format", "-i"] + filenames)
+
+def warn(filenames):
+	return subprocess.call(["clang-format", "-Werror", "--dry-run"] + filenames)
 
 def main():
 	import argparse
 	p = argparse.ArgumentParser(description="Check and fix style of changed files")
-	p.add_argument("--base", default="HEAD", help="Revision to compare to")
 	p.add_argument("-n", "--dry-run", action="store_true", help="Don't fix, only warn")
 	args = p.parse_args()
+	filenames = filter_cpp(recursive_file_list("src"))
 	if not args.dry_run:
-		reformat(filter_cpp(git_get_changed_files(1, base=args.base)))
+		reformat(filenames)
 	else:
-		sys.exit(warn(filter_cpp(git_get_changed_files(1, base=args.base))))
+		sys.exit(warn(filenames))
 
 if __name__ == "__main__":
 	main()
