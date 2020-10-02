ddnet/ddnet

commit 9b0081de57fe1fcca9955e8881441e46256470de

Fix ctrl+shift+d moving tee (fixes #2930)

src/game/client/components/binds.cpp
@@ -54,7 +54,7 @@ void CBinds::Bind(int KeyID, const char *pStr, bool FreeOnly, int Modifier)
 
 	// skip modifiers for +xxx binds
 	if(pStr[0] == '+')
-		Modifier = 0;
+		Modifier = 1 << MODIFIER_NONE;
 
 	char aBuf[256];
 	if(!pStr[0])
@@ -130,7 +130,6 @@ bool CBinds::OnInput(IInput::CEvent e)
 	if(!Mask)
 		Mask = 1 << MODIFIER_NONE;
 
-	bool ret = false;
 	for(int Mod = 1; Mod < MODIFIER_COMBINATION_COUNT; Mod++)
 	{
 		if(m_aapKeyBindings[Mod][e.m_Key] && (Mask == Mod))
@@ -139,20 +138,11 @@ bool CBinds::OnInput(IInput::CEvent e)
 				Console()->ExecuteLineStroked(1, m_aapKeyBindings[Mod][e.m_Key]);
 			if(e.m_Flags & IInput::FLAG_RELEASE)
 				Console()->ExecuteLineStroked(0, m_aapKeyBindings[Mod][e.m_Key]);
-			ret = true;
+			return true;
 		}
 	}
 
-	if(m_aapKeyBindings[0][e.m_Key] && (!ret || m_aapKeyBindings[0][e.m_Key][0] == '+')) // always trigger +xxx binds despite any modifier
-	{
-		if(e.m_Flags & IInput::FLAG_PRESS)
-			Console()->ExecuteLineStroked(1, m_aapKeyBindings[0][e.m_Key]);
-		if(e.m_Flags & IInput::FLAG_RELEASE)
-			Console()->ExecuteLineStroked(0, m_aapKeyBindings[0][e.m_Key]);
-		ret = true;
-	}
-
-	return ret;
+	return false;
 }
 
 void CBinds::UnbindAll()

commit e4ed027c7b803cef335931157fdefa6553be10d9

Fix binds
Caused by https://github.com/ddnet/ddnet/commit/9b0081de5

Thanks to Fireball for report

src/game/client/components/binds.cpp
@@ -142,6 +142,15 @@ bool CBinds::OnInput(IInput::CEvent e)
 		}
 	}
 
+	if(m_aapKeyBindings[0][e.m_Key])
+	{
+		if(e.m_Flags & IInput::FLAG_PRESS)
+			Console()->ExecuteLineStroked(1, m_aapKeyBindings[0][e.m_Key]);
+		if(e.m_Flags & IInput::FLAG_RELEASE)
+			Console()->ExecuteLineStroked(0, m_aapKeyBindings[0][e.m_Key]);
+		return true;
+	}
+
 	return false;
 }
 