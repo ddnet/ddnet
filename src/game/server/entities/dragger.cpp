2c3000ba81389c54d98deb491fea690de52d342e
def <dennis@felsin9.de>
2020-11-04 23:09:22 +0100

    Minor code cleanup in dragger.cpp

--- src/game/server/entities/dragger.cpp
+++ src/game/server/entities/dragger.cpp
@@ -57,10 +57,9 @@ void CDragger::Move()
 			continue;
 		}
 		int Res =
-			m_NW ? GameServer()->Collision()->IntersectNoLaserNW(m_Pos,
-				       Temp->m_Pos, 0, 0) :
-			       GameServer()->Collision()->IntersectNoLaser(m_Pos,
-				       Temp->m_Pos, 0, 0);
+			m_NW ?
+				GameServer()->Collision()->IntersectNoLaserNW(m_Pos, Temp->m_Pos, 0, 0) :
+				GameServer()->Collision()->IntersectNoLaser(m_Pos, Temp->m_Pos, 0, 0);
 
 		if(Res == 0)
 		{
@@ -81,9 +80,10 @@ void CDragger::Move()
 	}
 
 	if(!m_Target)
+	{
 		m_Target = Id != -1 ? TempEnts[Id] : 0;
-
-	if(m_Target)
+	}
+	else
 	{
 		for(auto &SoloEnt : m_SoloEnts)
 		{
@@ -95,38 +95,38 @@ void CDragger::Move()
 
 void CDragger::Drag()
 {
-	if(m_Target)
+	if(!m_Target)
+		return;
+
+	CCharacter *Target = m_Target;
+
+	for(int i = -1; i < MAX_CLIENTS; i++)
 	{
-		CCharacter *Target = m_Target;
+		if(i >= 0)
+			Target = m_SoloEnts[i];
 
-		for(int i = -1; i < MAX_CLIENTS; i++)
+		if(!Target)
+			continue;
+
+		int Res = 0;
+		if(!m_NW)
+			Res = GameServer()->Collision()->IntersectNoLaser(m_Pos,
+				Target->m_Pos, 0, 0);
+		else
+			Res = GameServer()->Collision()->IntersectNoLaserNW(m_Pos,
+				Target->m_Pos, 0, 0);
+		if(Res || length(m_Pos - Target->m_Pos) > g_Config.m_SvDraggerRange)
 		{
-			if(i >= 0)
-				Target = m_SoloEnts[i];
-
-			if(!Target)
-				continue;
-
-			int Res = 0;
-			if(!m_NW)
-				Res = GameServer()->Collision()->IntersectNoLaser(m_Pos,
-					Target->m_Pos, 0, 0);
+			Target = 0;
+			if(i == -1)
+				m_Target = 0;
 			else
-				Res = GameServer()->Collision()->IntersectNoLaserNW(m_Pos,
-					Target->m_Pos, 0, 0);
-			if(Res || length(m_Pos - Target->m_Pos) > g_Config.m_SvDraggerRange)
-			{
-				Target = 0;
-				if(i == -1)
-					m_Target = 0;
-				else
-					m_SoloEnts[i] = 0;
-			}
-			else if(length(m_Pos - Target->m_Pos) > 28)
-			{
-				vec2 Temp = Target->Core()->m_Vel + (normalize(m_Pos - Target->m_Pos) * m_Strength);
-				Target->Core()->m_Vel = ClampVel(Target->m_MoveRestrictions, Temp);
-			}
+				m_SoloEnts[i] = 0;
+		}
+		else if(length(m_Pos - Target->m_Pos) > 28)
+		{
+			vec2 Temp = Target->Core()->m_Vel + (normalize(m_Pos - Target->m_Pos) * m_Strength);
+			Target->Core()->m_Vel = ClampVel(Target->m_MoveRestrictions, Temp);
 		}
 	}
 }
