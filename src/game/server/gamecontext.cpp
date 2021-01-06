ef2dd39cf5e945b42ee9948da30875ceeced9904
hhh26459 <74860643+hhh26459@users.noreply.github.com>
2021-01-06 18:16:30 +0100

    typo in gamecontext.cpp
    specators...

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -2018,7 +2018,7 @@ void CGameContext::OnMessage(int MsgID, CUnpacker *pUnpacker, int ClientID)
 
 				if(!GetPlayerChar(ClientID) || !GetPlayerChar(SpectateID) || GetDDRaceTeam(ClientID) != GetDDRaceTeam(SpectateID))
 				{
-					SendChatTarget(ClientID, "You can only move your team member to specators");
+					SendChatTarget(ClientID, "You can only move your team member to spectators");
 					return;
 				}
 
