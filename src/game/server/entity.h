dc5b9340d57ff45aa1015d4fa8604dec30cc1e82
Alexander Akulich <akulichalexander@gmail.com>
2021-01-09 16:35:00 +0300

    Extract allocation macros to alloc.h (mimic the upstream)
    See the upstream commit 24bda2faa0a2b621aa24f3b1e2f6c6d6d9eabd9b

--- src/game/server/entity.h
+++ src/game/server/entity.h
@@ -5,59 +5,8 @@
 
 #include <base/vmath.h>
 #include <game/server/gameworld.h>
-#include <new>
 
-#define MACRO_ALLOC_HEAP() \
-public: \
-	void *operator new(size_t Size) \
-	{ \
-		void *p = malloc(Size); \
-		mem_zero(p, Size); \
-		return p; \
-	} \
-	void operator delete(void *pPtr) \
-	{ \
-		free(pPtr); \
-	} \
-\
-private:
-
-#define MACRO_ALLOC_POOL_ID() \
-public: \
-	void *operator new(size_t Size, int id); \
-	void operator delete(void *p, int id); \
-	void operator delete(void *p); /* NOLINT(misc-new-delete-overloads) */ \
-\
-private:
-
-#define MACRO_ALLOC_POOL_ID_IMPL(POOLTYPE, PoolSize) \
-	static char ms_PoolData##POOLTYPE[PoolSize][sizeof(POOLTYPE)] = {{0}}; \
-	static int ms_PoolUsed##POOLTYPE[PoolSize] = {0}; \
-	void *POOLTYPE::operator new(size_t Size, int id) \
-	{ \
-		dbg_assert(sizeof(POOLTYPE) == Size, "size error"); \
-		dbg_assert(!ms_PoolUsed##POOLTYPE[id], "already used"); \
-		/*dbg_msg("pool", "++ %s %d", #POOLTYPE, id);*/ \
-		ms_PoolUsed##POOLTYPE[id] = 1; \
-		mem_zero(ms_PoolData##POOLTYPE[id], Size); \
-		return ms_PoolData##POOLTYPE[id]; \
-	} \
-	void POOLTYPE::operator delete(void *p, int id) \
-	{ \
-		dbg_assert(ms_PoolUsed##POOLTYPE[id], "not used"); \
-		dbg_assert(id == (POOLTYPE *)p - (POOLTYPE *)ms_PoolData##POOLTYPE, "invalid id"); \
-		/*dbg_msg("pool", "-- %s %d", #POOLTYPE, id);*/ \
-		ms_PoolUsed##POOLTYPE[id] = 0; \
-		mem_zero(ms_PoolData##POOLTYPE[id], sizeof(POOLTYPE)); \
-	} \
-	void POOLTYPE::operator delete(void *p) /* NOLINT(misc-new-delete-overloads) */ \
-	{ \
-		int id = (POOLTYPE *)p - (POOLTYPE *)ms_PoolData##POOLTYPE; \
-		dbg_assert(ms_PoolUsed##POOLTYPE[id], "not used"); \
-		/*dbg_msg("pool", "-- %s %d", #POOLTYPE, id);*/ \
-		ms_PoolUsed##POOLTYPE[id] = 0; \
-		mem_zero(ms_PoolData##POOLTYPE[id], sizeof(POOLTYPE)); \
-	}
+#include "alloc.h"
 
 /*
 	Class: Entity
