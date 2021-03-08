271ab5f9de0b7ec7a8a8aaf08588d93079689b6e
def <dennis@felsin9.de>
2021-03-08 18:14:30 +0100

    Increase max ban duration from 1 month to 1 year
    to show ban message more accurately when player gets globally banned

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -227,7 +227,7 @@ void CServerBan::ConBanExt(IConsole::IResult *pResult, void *pUser)
 	CServerBan *pThis = static_cast<CServerBan *>(pUser);
 
 	const char *pStr = pResult->GetString(0);
-	int Minutes = pResult->NumArguments() > 1 ? clamp(pResult->GetInteger(1), 0, 44640) : 30;
+	int Minutes = pResult->NumArguments() > 1 ? clamp(pResult->GetInteger(1), 0, 525600) : 30;
 	const char *pReason = pResult->NumArguments() > 2 ? pResult->GetString(2) : "No reason given";
 
 	if(str_isallnum(pStr))
