7dcca9790098f8c0bdfa2e32f5f7b01b9aa4bd36
def <dennis@felsin9.de>
2021-08-04 16:30:25 +0200

    Revert "Fix client use-after-free reported by mmmds"
    Seems to cause problems:
    
    > Hussain wrote
    > :last update+ beta
    > my dummy stuck ( this second time)
    > disconnect and connect again useless still stuck
    > and disconnect server too
    > have to restart the game
    
    This reverts commit ce2f29b5f1e1925ac9292f29f7d498bd7da25aed.

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -655,12 +655,6 @@ void CClient::EnterGame()
 	if(State() == IClient::STATE_DEMOPLAYBACK)
 		return;
 
-	if(State() == IClient::STATE_ONLINE)
-	{
-		// Don't reset everything while already in game.
-		return;
-	}
-
 	// now we will wait for two snapshots
 	// to finish the connection
 	SendEnterGame();
