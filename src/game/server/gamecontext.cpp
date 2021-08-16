200af585d3d230c6e1aa4174af507f9ab2badf40
Edgar <git@edgarluque.com>
2021-08-16 18:29:24 +0200

    check if GIT_SHORTREV_HASH exists
    Co-authored-by: Emir Marincic <learath2@gmail.com>

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -3371,7 +3371,8 @@ void CGameContext::OnInit(/*class IKernel *pKernel*/)
 
 	//game.world.insert_entity(game.Controller);
 
-	Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "git-revision", GIT_SHORTREV_HASH);
+	if(GIT_SHORTREV_HASH)
+		Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "git-revision", GIT_SHORTREV_HASH);
 
 #ifdef CONF_DEBUG
 	if(g_Config.m_DbgDummies)
