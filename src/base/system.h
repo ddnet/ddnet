75662b6c1a30aad7c6ff7b30c2a48e17676b6a2a
Robert Müller <robert.mueller@uni-siegen.de>
2021-08-23 12:30:12 +0200

    remove unused and not implemented IOFLAG_RANDOM, add assert

--- src/base/system.h
+++ src/base/system.h
@@ -170,8 +170,7 @@ enum
 {
 	IOFLAG_READ = 1,
 	IOFLAG_WRITE = 2,
-	IOFLAG_RANDOM = 4,
-	IOFLAG_APPEND = 8,
+	IOFLAG_APPEND = 4,
 
 	IOSEEK_START = 0,
 	IOSEEK_CUR = 1,
@@ -186,7 +185,7 @@ typedef struct IOINTERNAL *IOHANDLE;
 
 	Parameters:
 		filename - File to open.
-		flags - A set of flags. IOFLAG_READ, IOFLAG_WRITE, IOFLAG_RANDOM, IOFLAG_APPEND.
+		flags - A set of flags. IOFLAG_READ, IOFLAG_WRITE, IOFLAG_APPEND.
 
 	Returns:
 		Returns a handle to the file on success and 0 on failure.
