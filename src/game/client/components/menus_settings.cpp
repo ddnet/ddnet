7552bfd05c96248ab52f31b8e2c196d653652794
Azorlogh <bott.alix@gmail.com>
2021-10-05 18:52:58 +0200

    set defaut highlight color in kill message to white

--- src/game/client/components/menus_settings.cpp
+++ src/game/client/components/menus_settings.cpp
@@ -2108,7 +2108,7 @@ void CMenus::RenderSettingsHUD(CUIRect MainView)
 		DoLine_ColorPicker(&KillMessageNormalColorID, 25.0f, 250.0f, 13.0f, 5.0f, &MainView, Localize("Kill Message Normal Color"), &g_Config.m_ClKillMessageNormalColor, ColorRGBA(1.0f, 1.0f, 1.0f), false);
 
 		static int KillMessageHighlightColorID;
-		DoLine_ColorPicker(&KillMessageHighlightColorID, 25.0f, 250.0f, 13.0f, 5.0f, &MainView, Localize("Kill Message Highlight Color"), &g_Config.m_ClKillMessageHighlightColor, ColorRGBA(1.0f, 0.2f, 0.2f), false);
+		DoLine_ColorPicker(&KillMessageHighlightColorID, 25.0f, 250.0f, 13.0f, 5.0f, &MainView, Localize("Kill Message Highlight Color"), &g_Config.m_ClKillMessageHighlightColor, ColorRGBA(1.0f, 1.0f, 1.0f), false);
 
 		// ***** Laser ***** //
 
