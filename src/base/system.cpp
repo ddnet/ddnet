e37ff4db6593a57743330112d940d50f78cef450
Robert Müller <robert.mueller@uni-siegen.de>
2021-11-21 10:46:31 +0100

    remove unused fs_getmtime in favor of fs_file_time

--- src/base/system.cpp
+++ src/base/system.cpp
@@ -2279,15 +2279,6 @@ int fs_is_dir(const char *path)
 #endif
 }
 
-time_t fs_getmtime(const char *path)
-{
-	struct stat sb;
-	if(stat(path, &sb) == -1)
-		return 0;
-
-	return sb.st_mtime;
-}
-
 int fs_chdir(const char *path)
 {
 	if(fs_is_dir(path))
