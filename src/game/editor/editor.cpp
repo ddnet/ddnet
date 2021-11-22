36a6b390317a0be7a01e4711b3edcdd464830c0d
def <dennis@felsin9.de>
2021-11-22 18:26:13 +0100

    Fix save typing when server settings are open in editor
    As reported by louis:
    
    > while saving a mapname while having the "Server Settings" tab open,
    > pressing "m" while typing the mapname will unfocus the cursor from the
    > mapname field onto the server settings field

--- src/game/editor/editor.cpp
+++ src/game/editor/editor.cpp
@@ -4248,6 +4248,9 @@ void CEditor::RenderFileDialog()
 					str_copy(&m_aFileDialogFileName[i], &m_aFileDialogFileName[i + 1], (int)(sizeof(m_aFileDialogFileName)) - i);
 			m_FilesSelectedIndex = -1;
 		}
+
+		if(m_FileDialogOpening)
+			UI()->SetActiveItem(&s_FileBoxID);
 	}
 	else
 	{
