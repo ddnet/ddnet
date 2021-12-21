6c42be9b8c428be43f268dc81c92894655045e54
def <dennis@felsin9.de>
2021-12-21 23:30:25 +0100

    Revert "Use temporary file when saving editor file (fixes #4476)"
    Doesn't work on Windows, not so easy to overwrite a file already open in
    editor. Not sure how our old approach works, but apparently it did
    better than the new one. As reported by texnonik on Discord
    
    This reverts commit e2084e1e9dff6f6e816ac63f02c49fb8ae9178b2.

--- src/game/editor/io.cpp
+++ src/game/editor/io.cpp
@@ -221,15 +221,13 @@ int CEditor::Save(const char *pFilename)
 
 int CEditorMap::Save(class IStorage *pStorage, const char *pFileName)
 {
-	char aFileNameTmp[IO_MAX_PATH_LENGTH];
-	str_format(aFileNameTmp, sizeof(aFileNameTmp), "%s.%d.tmp", pFileName, pid());
-	char aBuf[IO_MAX_PATH_LENGTH];
-	str_format(aBuf, sizeof(aBuf), "saving to '%s'...", aFileNameTmp);
+	char aBuf[256];
+	str_format(aBuf, sizeof(aBuf), "saving to '%s'...", pFileName);
 	m_pEditor->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "editor", aBuf);
 	CDataFileWriter df;
-	if(!df.Open(pStorage, aFileNameTmp))
+	if(!df.Open(pStorage, pFileName))
 	{
-		str_format(aBuf, sizeof(aBuf), "failed to open file '%s'...", aFileNameTmp);
+		str_format(aBuf, sizeof(aBuf), "failed to open file '%s'...", pFileName);
 		m_pEditor->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "editor", aBuf);
 		return 0;
 	}
@@ -554,13 +552,6 @@ int CEditorMap::Save(class IStorage *pStorage, const char *pFileName)
 	// finish the data file
 	df.Finish();
 	m_pEditor->Console()->Print(IConsole::OUTPUT_LEVEL_ADDINFO, "editor", "saving done");
-
-	str_format(aBuf, sizeof(aBuf), "moving '%s' to '%s'", aFileNameTmp, pFileName);
-	m_pEditor->Console()->Print(IConsole::OUTPUT_LEVEL_ADDINFO, "editor", aBuf);
-	if(!pStorage->RenameFile(aFileNameTmp, pFileName, IStorage::TYPE_SAVE))
-	{
-		return 0;
-	}
 
 	// send rcon.. if we can
 	if(m_pEditor->Client()->RconAuthed())
