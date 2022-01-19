e9293877caf568705055843dfd0f1805c0fd97e6
Robert Müller <robert.mueller@uni-siegen.de>
2022-01-19 22:30:29 +0100

    Add CLayers::NumLayers

--- src/game/layers.h
+++ src/game/layers.h
@@ -23,6 +23,7 @@ public:
 	void Init(class IKernel *pKernel);
 	void InitBackground(class IMap *pMap);
 	int NumGroups() const { return m_GroupsNum; };
+	int NumLayers() const { return m_LayersNum; }
 	class IMap *Map() const { return m_pMap; };
 	CMapItemGroup *GameGroup() const { return m_pGameGroup; };
 	CMapItemLayerTilemap *GameLayer() const { return m_pGameLayer; };
