6c558c21380bdb3c4dc3665b82b867cdccfac438
Robert Müller <robert.mueller@uni-siegen.de>
2022-01-19 22:27:59 +0100

    Improve code style of InitTilemapSkip

--- src/game/layers.cpp
+++ src/game/layers.cpp
@@ -167,29 +167,29 @@ void CLayers::InitTilemapSkip()
 {
 	for(int g = 0; g < NumGroups(); g++)
 	{
-		CMapItemGroup *pGroup = GetGroup(g);
+		const CMapItemGroup *pGroup = GetGroup(g);
 
 		for(int l = 0; l < pGroup->m_NumLayers; l++)
 		{
-			CMapItemLayer *pLayer = GetLayer(pGroup->m_StartLayer + l);
+			const CMapItemLayer *pLayer = GetLayer(pGroup->m_StartLayer + l);
 
 			if(pLayer->m_Type == LAYERTYPE_TILES)
 			{
-				CMapItemLayerTilemap *pTmap = (CMapItemLayerTilemap *)pLayer;
-				CTile *pTiles = (CTile *)m_pMap->GetData(pTmap->m_Data);
-				for(int y = 0; y < pTmap->m_Height; y++)
+				const CMapItemLayerTilemap *pTilemap = (CMapItemLayerTilemap *)pLayer;
+				CTile *pTiles = (CTile *)m_pMap->GetData(pTilemap->m_Data);
+				for(int y = 0; y < pTilemap->m_Height; y++)
 				{
-					for(int x = 1; x < pTmap->m_Width;)
+					for(int x = 1; x < pTilemap->m_Width;)
 					{
-						int sx;
-						for(sx = 1; x + sx < pTmap->m_Width && sx < 255; sx++)
+						int SkippedX;
+						for(SkippedX = 1; x + SkippedX < pTilemap->m_Width && SkippedX < 255; SkippedX++)
 						{
-							if(pTiles[y * pTmap->m_Width + x + sx].m_Index)
+							if(pTiles[y * pTilemap->m_Width + x + SkippedX].m_Index)
 								break;
 						}
 
-						pTiles[y * pTmap->m_Width + x].m_Skip = sx - 1;
-						x += sx;
+						pTiles[y * pTilemap->m_Width + x].m_Skip = SkippedX - 1;
+						x += SkippedX;
 					}
 				}
 			}
