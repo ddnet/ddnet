f634f66008c6496d9edb24f159a9ff02a18ceb46
Robert Müller <robytemueller@gmail.com>
2022-05-15 20:13:21 +0200

    Fix memory leaks in auto stat CSV output, refactoring
    And fix stringop-overflow warning:
    
    In function 'str_copy',
        inlined from 'FormatStats' at src\game\client\components\statboard.cpp:541:10:
    src\base\system.cpp:2524:16: warning: 'strncat' specified bound depends on the length of the source argument [-Wstringop-overflow=]
     2524 |         strncat(dst, src, dst_size - 1);
          |                ^
    src\game\client\components\statboard.cpp: In member function 'FormatStats':
    src\base\system.cpp:2557:27: note: length computed here
     2557 |         return (int)strlen(str);
          |                           ^

--- src/game/client/components/statboard.h
+++ src/game/client/components/statboard.h
@@ -2,6 +2,7 @@
 #define GAME_CLIENT_COMPONENTS_STATBOARD_H
 
 #include <game/client/component.h>
+#include <string>
 
 class CStatboard : public CComponent
 {
@@ -14,9 +15,8 @@ private:
 	void AutoStatScreenshot();
 	void AutoStatCSV();
 
-	char *m_pCSVstr;
-	char *ReplaceCommata(char *pStr);
-	void FormatStats();
+	std::string ReplaceCommata(char *pStr);
+	void FormatStats(char *pDest, size_t DestSize);
 
 public:
 	CStatboard();
