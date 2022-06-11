d04703fd286de7705f37fe6c054f16422cc889bc
ChillerDragon <ChillerDragon@gmail.com>
2022-06-11 14:43:17 +0200

    Start working on "where are you" reply
    related to issue #85

--- src/game/client/components/chillerbot/chathelper/replytoping.cpp
+++ src/game/client/components/chillerbot/chathelper/replytoping.cpp
@@ -537,6 +537,103 @@ bool CReplyToPing::Reply()
 		}
 	}
 
+	// where are you
+	if(str_find_nocase(m_pMessage, "where are you") || str_find_nocase(m_pMessage, "where r u") || (str_find_nocase(m_pMessage, "where r yo") && !str_find_nocase(m_pMessage, "wo bist")))
+	{
+		for(auto &Client : ChatHelper()->GameClient()->m_aClients)
+		{
+			if(!Client.m_Active)
+				continue;
+			if(str_comp(Client.m_aName, m_pMessageAuthor))
+				continue;
+
+			// TODO: dont get current dummy but the pinged dummy
+			CCharacter *pChar = ChatHelper()->GameClient()->m_GameWorld.GetCharacterByID(ChatHelper()->GameClient()->m_LocalIDs[g_Config.m_ClDummy]);
+			vec2 Self = pChar->m_Pos;
+			vec2 Other = Client.m_RenderPos;
+			float distY = abs(Self.y - Other.y);
+			float distX = abs(Self.x - Other.x);
+			if(distX < 5 * 32 && distY < 5 * 32)
+			{
+				if(distX > distY)
+				{
+					if(Self.x > Other.x)
+					{
+						str_format(m_pResponse, m_SizeOfResponse, "%s i am literally next to you (on your right)", m_pMessageAuthor);
+						return true;
+					}
+					else
+					{
+						str_format(m_pResponse, m_SizeOfResponse, "%s i am literally next to you (on your left)", m_pMessageAuthor);
+						return true;
+					}
+				}
+				else
+				{
+					if(Self.y > Other.y)
+					{
+						str_format(m_pResponse, m_SizeOfResponse, "%s i am literally next to you (below you)", m_pMessageAuthor);
+						return true;
+					}
+					else
+					{
+						str_format(m_pResponse, m_SizeOfResponse, "%s i am literally next to you (above you)", m_pMessageAuthor);
+						return true;
+					}
+				}
+			}
+			else if(distY > distX * 8)
+			{
+				if(Self.y > Other.y)
+				{
+					str_format(m_pResponse, m_SizeOfResponse, "%s below you (%d tiles)", m_pMessageAuthor, (int)(distY / 32));
+					return true;
+				}
+				else
+				{
+					str_format(m_pResponse, m_SizeOfResponse, "%s above you (%d tiles)", m_pMessageAuthor, (int)(distY / 32));
+					return true;
+				}
+			}
+			else if(Self.x > Other.x)
+			{
+				if(distY < 15 * 32)
+				{
+					str_format(m_pResponse, m_SizeOfResponse, "%s i am on your right (%d tiles away)", m_pMessageAuthor, (int)(distX / 32));
+					return true;
+				}
+				else if(Self.y > Other.y)
+				{
+					str_format(m_pResponse, m_SizeOfResponse, "%s i am on your right (%d tiles away) and %d tiles below you", m_pMessageAuthor, (int)(distX / 32), (int)(distY / 32));
+					return true;
+				}
+				else
+				{
+					str_format(m_pResponse, m_SizeOfResponse, "%s i am on your right (%d tiles away) and %d tiles above you", m_pMessageAuthor, (int)(distX / 32), (int)(distY / 32));
+					return true;
+				}
+			}
+			else
+			{
+				if(distY < 15 * 32)
+				{
+					str_format(m_pResponse, m_SizeOfResponse, "%s i am on your left (%d tiles away)", m_pMessageAuthor, (int)(distX / 32));
+					return true;
+				}
+				else if(Self.y > Other.y)
+				{
+					str_format(m_pResponse, m_SizeOfResponse, "%s i am on your left (%d tiles away) and %d tiles below you", m_pMessageAuthor, (int)(distX / 32), (int)(distY / 32));
+					return true;
+				}
+				else
+				{
+					str_format(m_pResponse, m_SizeOfResponse, "%s i am on your left (%d tiles away) and %d tiles above you", m_pMessageAuthor, (int)(distX / 32), (int)(distY / 32));
+					return true;
+				}
+			}
+		}
+	}
+
 	// spec me
 	if(str_find_nocase(m_pMessage, "spec") || str_find_nocase(m_pMessage, "watch") || (str_find_nocase(m_pMessage, "look") && !str_find_nocase(m_pMessage, "looks")) || str_find_nocase(m_pMessage, "schau"))
 	{
