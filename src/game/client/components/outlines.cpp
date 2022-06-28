28b34070d29cdb81a147613f5632358f89cb8dd6
Tater <Mr.Potatooh@gmail.com>
2022-06-28 17:49:16 -0500

    fix outline includes

--- src/game/client/components/outlines.cpp
+++ src/game/client/components/outlines.cpp
@@ -1,12 +1,13 @@
 #include <engine/graphics.h>
 #include <engine/shared/config.h>
 
+#include <game/client/gameclient.h>
 #include <game/client/animstate.h>
 #include <game/client/render.h>
 #include <game/generated/client_data.h>
 #include <game/generated/protocol.h>
+#include <game/mapitems.h>
 
-#include <game/client/gameclient.h>
 
 #include "outlines.h"
 
