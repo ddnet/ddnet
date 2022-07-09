ab4a4aeb7599bf0c88a87280d6d8128fc787bd4c
Robert Müller <robytemueller@gmail.com>
2022-07-09 17:08:19 +0200

    Use `DrawRect` instead of `DrawRoundRect` in `CMenus`
    To reduce duplicate code.

--- src/game/client/components/menus.cpp
+++ src/game/client/components/menus.cpp
@@ -940,13 +940,7 @@ void CMenus::RenderLoading(const char *pCaption, const char *pContent, int Incre
 	UI()->DoLabel(&Part, pContent, 20.0f, TEXTALIGN_CENTER);
 
 	if(RenderLoadingBar)
-	{
-		Graphics()->TextureClear();
-		Graphics()->QuadsBegin();
-		Graphics()->SetColor(1, 1, 1, 0.75f);
-		RenderTools()->DrawRoundRect(Box.x + 40, Box.y + Box.h - 75, (Box.w - 80) * Percent, 25, 5.0f);
-		Graphics()->QuadsEnd();
-	}
+		RenderTools()->DrawRect(Box.x + 40, Box.y + Box.h - 75, (Box.w - 80) * Percent, 25, ColorRGBA(1.0f, 1.0f, 1.0f, 0.75f), CUI::CORNER_ALL, 5.0f);
 
 	Client()->UpdateAndSwap();
 }
