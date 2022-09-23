17960e52b51fe10ad7bb8ac7042346e9df00c418
Dennis Felsing <dennis@felsin9.de>
2022-09-24 01:05:00 +0200

    Add luk51 in credits

--- src/game/server/ddracechat.cpp
+++ src/game/server/ddracechat.cpp
@@ -43,7 +43,7 @@ void CGameContext::ConCredits(IConsole::IResult *pResult, void *pUserData)
 	pSelf->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "chatresp",
 		"Zodiac, c0d3d3v, GiuCcc, Ravie, Robyt3, simpygirl,");
 	pSelf->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "chatresp",
-		"sjrc6, Cellegen, srdante, Nouaa, Voxel & others.");
+		"sjrc6, Cellegen, srdante, Nouaa, Voxel, luk51 & others.");
 	pSelf->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "chatresp",
 		"Based on DDRace by the DDRace developers,");
 	pSelf->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "chatresp",
