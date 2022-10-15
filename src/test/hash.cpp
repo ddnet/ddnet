0a6ad9cf9da3936bf1fc38a2d48424859182c190
Robert Müller <robytemueller@gmail.com>
2022-10-15 18:03:01 +0200

    Add tests for too small/large `sha256_str` and `md5_str` buffers
    Rename `Expect` to `ExpectSha256` and `Expect2` to `ExpectMd5` for better readability.
    
    Pass buffer size by template parameter to reduce duplicate source code.

--- src/test/hash.cpp
+++ src/test/hash.cpp
@@ -3,9 +3,10 @@
 #include <base/hash_ctxt.h>
 #include <base/system.h>
 
-static void Expect(SHA256_DIGEST Actual, const char *pWanted)
+template<size_t BufferSize = SHA256_MAXSTRSIZE>
+static void ExpectSha256(SHA256_DIGEST Actual, const char *pWanted)
 {
-	char aActual[SHA256_MAXSTRSIZE];
+	char aActual[BufferSize];
 	sha256_str(Actual, aActual, sizeof(aActual));
 	EXPECT_STREQ(aActual, pWanted);
 }
@@ -13,15 +14,15 @@ static void Expect(SHA256_DIGEST Actual, const char *pWanted)
 TEST(Hash, Sha256)
 {
 	// https://en.wikipedia.org/w/index.php?title=SHA-2&oldid=840187620#Test_vectors
-	Expect(sha256("", 0), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+	ExpectSha256(sha256("", 0), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
 	SHA256_CTX ctxt;
 
 	sha256_init(&ctxt);
-	Expect(sha256_finish(&ctxt), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+	ExpectSha256(sha256_finish(&ctxt), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
 
 	// printf 'The quick brown fox jumps over the lazy dog.' | sha256sum
 	char QUICK_BROWN_FOX[] = "The quick brown fox jumps over the lazy dog.";
-	Expect(sha256(QUICK_BROWN_FOX, str_length(QUICK_BROWN_FOX)), "ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c");
+	ExpectSha256(sha256(QUICK_BROWN_FOX, str_length(QUICK_BROWN_FOX)), "ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c");
 
 	sha256_init(&ctxt);
 	sha256_update(&ctxt, "The ", 4);
@@ -33,7 +34,20 @@ TEST(Hash, Sha256)
 	sha256_update(&ctxt, "the ", 4);
 	sha256_update(&ctxt, "lazy ", 5);
 	sha256_update(&ctxt, "dog.", 4);
-	Expect(sha256_finish(&ctxt), "ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c");
+	ExpectSha256(sha256_finish(&ctxt), "ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c");
+}
+
+TEST(Hash, Sha256ToStringSmallBuffer)
+{
+	char QUICK_BROWN_FOX[] = "The quick brown fox jumps over the lazy dog.";
+	ExpectSha256<1>(sha256(QUICK_BROWN_FOX, str_length(QUICK_BROWN_FOX)), "");
+	ExpectSha256<16 + 1>(sha256(QUICK_BROWN_FOX, str_length(QUICK_BROWN_FOX)), "ef537f25c895bfa7");
+}
+
+TEST(Hash, Sha256ToStringLargeBuffer)
+{
+	char QUICK_BROWN_FOX[] = "The quick brown fox jumps over the lazy dog.";
+	ExpectSha256<SHA256_MAXSTRSIZE + 64>(sha256(QUICK_BROWN_FOX, str_length(QUICK_BROWN_FOX)), "ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c");
 }
 
 TEST(Hash, Sha256Eq)
@@ -87,9 +101,10 @@ TEST(Hash, Sha256FromStr)
 	EXPECT_TRUE(sha256_from_str(&Sha256, "x123456789012345678901234567890123456789012345678901234567890123"));
 }
 
-static void Expect2(MD5_DIGEST Actual, const char *pWanted)
+template<size_t BufferSize = MD5_MAXSTRSIZE>
+static void ExpectMd5(MD5_DIGEST Actual, const char *pWanted)
 {
-	char aActual[MD5_MAXSTRSIZE];
+	char aActual[BufferSize];
 	md5_str(Actual, aActual, sizeof(aActual));
 	EXPECT_STREQ(aActual, pWanted);
 }
@@ -97,14 +112,14 @@ static void Expect2(MD5_DIGEST Actual, const char *pWanted)
 TEST(Hash, Md5)
 {
 	// https://en.wikipedia.org/w/index.php?title=MD5&oldid=889664074#MD5_hashes
-	Expect2(md5("", 0), "d41d8cd98f00b204e9800998ecf8427e");
+	ExpectMd5(md5("", 0), "d41d8cd98f00b204e9800998ecf8427e");
 	MD5_CTX ctxt;
 
 	md5_init(&ctxt);
-	Expect2(md5_finish(&ctxt), "d41d8cd98f00b204e9800998ecf8427e");
+	ExpectMd5(md5_finish(&ctxt), "d41d8cd98f00b204e9800998ecf8427e");
 
 	char QUICK_BROWN_FOX[] = "The quick brown fox jumps over the lazy dog.";
-	Expect2(md5(QUICK_BROWN_FOX, str_length(QUICK_BROWN_FOX)), "e4d909c290d0fb1ca068ffaddf22cbd0");
+	ExpectMd5(md5(QUICK_BROWN_FOX, str_length(QUICK_BROWN_FOX)), "e4d909c290d0fb1ca068ffaddf22cbd0");
 
 	md5_init(&ctxt);
 	md5_update(&ctxt, "The ", 4);
@@ -116,7 +131,20 @@ TEST(Hash, Md5)
 	md5_update(&ctxt, "the ", 4);
 	md5_update(&ctxt, "lazy ", 5);
 	md5_update(&ctxt, "dog.", 4);
-	Expect2(md5_finish(&ctxt), "e4d909c290d0fb1ca068ffaddf22cbd0");
+	ExpectMd5(md5_finish(&ctxt), "e4d909c290d0fb1ca068ffaddf22cbd0");
+}
+
+TEST(Hash, Md5ToStringSmallBuffer)
+{
+	char QUICK_BROWN_FOX[] = "The quick brown fox jumps over the lazy dog.";
+	ExpectMd5<1>(md5(QUICK_BROWN_FOX, str_length(QUICK_BROWN_FOX)), "");
+	ExpectMd5<16 + 1>(md5(QUICK_BROWN_FOX, str_length(QUICK_BROWN_FOX)), "e4d909c290d0fb1c");
+}
+
+TEST(Hash, Md5ToStringLargeBuffer)
+{
+	char QUICK_BROWN_FOX[] = "The quick brown fox jumps over the lazy dog.";
+	ExpectMd5<MD5_MAXSTRSIZE + 64>(md5(QUICK_BROWN_FOX, str_length(QUICK_BROWN_FOX)), "e4d909c290d0fb1ca068ffaddf22cbd0");
 }
 
 TEST(Hash, Md5Eq)
