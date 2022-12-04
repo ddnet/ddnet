b5eef1b64bf396a6ea1276c294b0effdd99b6882
louis <louisaltgeer@gmail.com>
2022-12-03 23:36:03 -0600

    added offset, fix my git issues
    added demomarker offset
    
    Revert "why is this here"
    
    This reverts commit a8c74b612300d6563b28a2bea733a0d3a7dd7f90.
    
    fix

--- src/game/client/components/menus_demo.cpp
+++ src/game/client/components/menus_demo.cpp
@@ -364,7 +364,7 @@ void CMenus::RenderDemoPlayer(CUIRect MainView)
 			Graphics()->TextureClear();
 			Graphics()->QuadsBegin();
 			Graphics()->SetColor(1.0f, 1.0f, 1.0f, 1.0f);
-			IGraphics::CQuadItem QuadItem(SeekBar.x + (SeekBar.w - 10.0f) * Ratio, SeekBar.y, UI()->PixelSize(), SeekBar.h);
+			IGraphics::CQuadItem QuadItem(8.0f + SeekBar.x + (SeekBar.w - 10.0f) * Ratio, SeekBar.y, UI()->PixelSize(), SeekBar.h);
 			Graphics()->QuadsDrawTL(&QuadItem, 1);
 			Graphics()->QuadsEnd();
 		}
