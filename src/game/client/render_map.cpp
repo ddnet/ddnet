2e5f37c61ba90c11659f6aafe6f0f1db9004bb26
Robert Müller <robytemueller@gmail.com>
2023-03-04 12:22:47 +0100

    Don't render switch number and delay for tiles where they are unused

--- src/game/client/render_map.cpp
+++ src/game/client/render_map.cpp
@@ -575,7 +575,7 @@ void CRenderTools::RenderSwitchOverlay(CSwitchTile *pSwitch, int w, int h, float
 			int c = mx + my * w;
 
 			unsigned char Index = pSwitch[c].m_Number;
-			if(Index)
+			if(Index && IsSwitchTileNumberUsed(pSwitch[c].m_Type))
 			{
 				char aBuf[16];
 				str_format(aBuf, sizeof(aBuf), "%d", Index);
@@ -585,7 +585,7 @@ void CRenderTools::RenderSwitchOverlay(CSwitchTile *pSwitch, int w, int h, float
 			}
 
 			unsigned char Delay = pSwitch[c].m_Delay;
-			if(Delay)
+			if(Delay && IsSwitchTileDelayUsed(pSwitch[c].m_Type))
 			{
 				char aBuf[16];
 				str_format(aBuf, sizeof(aBuf), "%d", Delay);
