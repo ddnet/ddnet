d69084ca0da633aedea15449b5c7b6943e38dfc5
Robert Müller <robytemueller@gmail.com>
2023-03-07 22:43:35 +0100

    Revert "Close editor popups and prevent popups from opening when GUI hidden"
    This reverts commit 5c4d5fb571434878c69048f497d393273fd32077.

--- src/game/editor/editor.cpp
+++ src/game/editor/editor.cpp
@@ -1331,7 +1331,7 @@ void CEditor::DoSoundSource(CSoundSource *pSource, int Index)
 		{
 			if(!UI()->MouseButton(1))
 			{
-				if(m_vSelectedLayers.size() == 1 && m_GuiActive)
+				if(m_vSelectedLayers.size() == 1)
 				{
 					static int s_SourcePopupID = 0;
 					UiInvokePopupMenu(&s_SourcePopupID, 0, UI()->MouseX(), UI()->MouseY(), 120, 200, PopupSource);
@@ -1480,7 +1480,7 @@ void CEditor::DoQuad(CQuad *pQuad, int Index)
 		{
 			if(!UI()->MouseButton(1))
 			{
-				if(m_vSelectedLayers.size() == 1 && m_GuiActive)
+				if(m_vSelectedLayers.size() == 1)
 				{
 					m_SelectedQuadIndex = FindSelectedQuadIndex(Index);
 
@@ -1692,7 +1692,7 @@ void CEditor::DoQuadPoint(CQuad *pQuad, int QuadIndex, int V)
 		{
 			if(!UI()->MouseButton(1))
 			{
-				if(m_vSelectedLayers.size() == 1 && m_GuiActive)
+				if(m_vSelectedLayers.size() == 1)
 				{
 					m_SelectedQuadPoint = V;
 					m_SelectedQuadIndex = FindSelectedQuadIndex(QuadIndex);
@@ -6496,10 +6496,7 @@ void CEditor::OnRender()
 	{
 		m_GuiActive = !m_GuiActive;
 		if(!m_GuiActive)
-		{
 			m_LockMouse = false;
-			UiClosePopupMenus();
-		}
 	}
 
 	if(Input()->KeyPress(KEY_F10))
