4d790e423d518155f55e4d237a598c0d5fde8e8c
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2023-03-08 15:04:21 +0100

    Fix crash

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -341,7 +341,7 @@ void CGameContext::CreateExplosion(vec2 Pos, int Owner, int Weapon, bool NoDamag
 			ForceDir = normalize(Diff);
 		l = 1 - clamp((l - InnerRadius) / (Radius - InnerRadius), 0.0f, 1.0f);
 
-		int TuneZone = m_apPlayers[Owner] ? m_apPlayers[Owner]->m_TuneZone : 0;
+		int TuneZone = (Owner == -1 || !m_apPlayers[Owner]) ? 0 : m_apPlayers[Owner]->m_TuneZone;
 		float Strength = Tuning(Owner, TuneZone)->m_ExplosionStrength;
 
 		float Dmg = Strength * l;
