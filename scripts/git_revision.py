c645c6fd7248fcc83425e0d97bf6bd201989dbf7
heinrich5991 <heinrich5991@gmail.com>
2023-03-19 17:02:49 +0100

    scripts/git_revision.py: Remove unused backcompat
    Compatibility with Python 3.2 and lower was already removed earlier.

--- scripts/git_revision.py
+++ scripts/git_revision.py
@@ -1,11 +1,8 @@
-import errno
 import subprocess
 try:
 	git_hash = subprocess.check_output(["git", "rev-parse", "--short=16", "HEAD"], stderr=subprocess.DEVNULL).decode().strip()
 	definition = f'"{git_hash}"'
-except FileNotFoundError as e:
-	if e.errno != errno.ENOENT:
-		raise
+except FileNotFoundError:
 	definition = "0"
 except subprocess.CalledProcessError:
 	definition = "0"
