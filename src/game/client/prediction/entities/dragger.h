8121abf877202b21c73326d1f1dc458f8f4e4d4d
trml <trml@users.noreply.github.com>
2023-06-03 18:51:38 +0200

    Add dragger prediction

---
+++ src/game/client/prediction/entities/dragger.h
@@ -0,0 +1,29 @@
+/* (c) Shereef Marzouk. See "licence DDRace.txt" and the readme.txt in the root of the distribution for more information. */
+#ifndef GAME_CLIENT_PREDICTION_ENTITIES_DRAGGER_H
+#define GAME_CLIENT_PREDICTION_ENTITIES_DRAGGER_H
+
+#include <game/client/prediction/entity.h>
+
+class CLaserData;
+
+class CDragger : public CEntity
+{
+	vec2 m_Core;
+	float m_Strength;
+	bool m_IgnoreWalls;
+	int m_TargetId;
+
+	void LookForPlayersToDrag();
+	void DraggerBeamTick();
+	void DraggerBeamReset();
+
+public:
+	CDragger(CGameWorld *pGameWorld, int ID, const CLaserData *pData);
+	bool Match(CDragger *pDragger);
+	void Read(const CLaserData *pData);
+	float GetStrength() { return m_Strength; }
+
+	void Tick() override;
+};
+
+#endif // GAME_CLIENT_PREDICTION_ENTITIES_DRAGGER_H
