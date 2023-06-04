708dbc5b5d6ca7c5807dde7d832bd0d617327ff5
Tater <Mr.Potatooh@gmail.com>
2023-06-03 21:16:02 -0500

    Update git_revision.py

--- scripts/git_revision.py
+++ scripts/git_revision.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 git_hash = os.environ.get("DDNET_GIT_SHORTREV_HASH")
 try:
-	git_hash = git_hash or subprocess.check_output(["git", "rev-parse", "--short=16", "HEAD"], stderr=subprocess.DEVNULL).decode().strip()
+	git_hash = git_hash or subprocess.check_output(["git", "rev-parse", "--short=40", "HEAD"], stderr=subprocess.DEVNULL).decode().strip()
 except (FileNotFoundError, subprocess.CalledProcessError):
 	pass
 if git_hash is not None:
