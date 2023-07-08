7802dc27605f4c85ce0fd5cf4b7beda13813246d
Alexander Akulich <akulichalexander@gmail.com>
2023-07-08 04:07:17 +0300

    Client: Omit '.demo' extension from the video file name

--- src/game/client/components/menus_demo.cpp
+++ src/game/client/components/menus_demo.cpp
@@ -1314,7 +1314,9 @@ void CMenus::RenderDemoList(CUIRect MainView)
 		if(DoButton_Menu(&s_RenderButton, Localize("Render"), 0, &RenderRect) || (Input()->KeyPress(KEY_R) && m_pClient->m_GameConsole.IsClosed()))
 		{
 			m_Popup = POPUP_RENDER_DEMO;
-			m_DemoRenderInput.Set(m_vDemos[m_DemolistSelectedIndex].m_aFilename);
+			char aNameWithoutExt[IO_MAX_PATH_LENGTH];
+			fs_split_file_extension(m_vDemos[m_DemolistSelectedIndex].m_aFilename, aNameWithoutExt, sizeof(aNameWithoutExt));
+			m_DemoRenderInput.Set(aNameWithoutExt);
 			UI()->SetActiveItem(&m_DemoRenderInput);
 			return;
 		}
