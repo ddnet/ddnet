da2da7b36f49b1978f70f09b3f119f19efa3fa7a
Robert Müller <robytemueller@gmail.com>
2023-07-09 17:23:43 +0200

    Fix editor hotkeys triggering while some editboxes are active
    Replace `CEditor::m_EditBoxActive` which only works with editboxes created from the editor with `CLineInput::GetActiveInput` which also works for editboxes which are created by generic UI functions, e.g. the value selector editboxes of color pickers.

--- src/game/editor/editor.h
+++ src/game/editor/editor.h
@@ -776,7 +776,6 @@ public:
 
 		m_Mode = MODE_LAYERS;
 		m_Dialog = 0;
-		m_EditBoxActive = 0;
 		m_pTooltip = nullptr;
 
 		m_GridActive = false;
@@ -932,7 +931,6 @@ public:
 
 	int m_Mode;
 	int m_Dialog;
-	int m_EditBoxActive;
 	const char *m_pTooltip;
 
 	bool m_GridActive;
