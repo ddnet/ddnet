11a16f553f471073e4426ed75a062a43d8678327
Alexander Akulich <akulichalexander@gmail.com>
2023-09-16 02:52:57 +0300

    CMake: Revert versioning (move it back to version.h)

--- src/game/version.h
+++ src/game/version.h
@@ -2,11 +2,12 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-
-#define GAME_RELEASE_VERSION "${PROJECT_VERSION}"
+#ifndef GAME_RELEASE_VERSION
+#define GAME_RELEASE_VERSION "17.2.1"
+#endif
 #define GAME_VERSION "0.6.4, " GAME_RELEASE_VERSION
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
-#define DDNET_VERSION_NUMBER ${DDNET_VERSION_NUMBER}
+#define DDNET_VERSION_NUMBER 17021
 extern const char *GIT_SHORTREV_HASH;
 #define GAME_NAME "DDNet"
 #endif
