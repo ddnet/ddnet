c64ec0f677d4c00a97d8ae2ac114ae49ac2188da
Robert Müller <robytemueller@gmail.com>
2023-09-28 17:29:09 +0200

    Limit effects of `dbg_stress` to features useful for debugging
    Using `dbg_stress 1` now only does the following (in debug build):
    
    - Randomly send inputs.
    - Randomly send chat messages.
    - Randomly connect/disconnect to server configured with `dbg_stress_server` (`localhost` by default).
    
    Previously it also did the following, which is not useful for this debugging feature and only complicates the code unnecessarily:
    
    - Cause images and sounds not to be loaded.
    - Render only every tenth frame.
    - Always use inactive graphics refresh rate.

--- src/engine/client/client.h
+++ src/engine/client/client.h
@@ -149,7 +149,6 @@ class CClient : public IClient, public CDemoPlayer::IListener
 	IGraphics::CTextureHandle m_DebugFont;
 
 	int64_t m_LastRenderTime;
-	int m_RenderFrames;
 
 	int m_SnapCrcErrors;
 	bool m_AutoScreenshotRecycle;
