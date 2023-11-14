1ca23d3babe235fe7935974403a3ed16fc5f297c
ChillerDragon <ChillerDragon@gmail.com>
2023-11-14 11:52:19 +0100

    Add seconds as arg to !restart (#16)

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -2053,7 +2053,7 @@ void CGameContext::OnSayNetMessage(const CNetMsg_Cl_Say *pMsg, int ClientID, con
 		Team = CHAT_ALL;
 
 	if(pMsg->m_pMessage[0] == '!')
-		OnBangCommand(pMsg->m_pMessage + 1, ClientID);
+		ParseChatCmd('!', ClientID, pMsg->m_pMessage + 1);
 	else if(pMsg->m_pMessage[0] == '/')
 	{
 		if(str_startswith_nocase(pMsg->m_pMessage + 1, "w "))
@@ -4794,19 +4794,108 @@ void CGameContext::ComCallShuffleVote(int ClientID)
 	BangCommandVote(ClientID, "shuffle_teams", "shuffle teams");
 }
 
-void CGameContext::OnBangCommand(const char *pLine, int ClientID)
+bool CGameContext::ParseChatCmd(char Prefix, int ClientID, const char *pCmdWithArgs)
+{
+	const int MAX_ARG_LEN = 256;
+	char aCmd[MAX_ARG_LEN];
+	int i;
+	for(i = 0; pCmdWithArgs[i] && i < MAX_ARG_LEN; i++)
+	{
+		if(pCmdWithArgs[i] == ' ')
+			break;
+		aCmd[i] = pCmdWithArgs[i];
+	}
+	aCmd[i] = '\0';
+	// int ROffset = m_pClient->m_ChatHelper.ChatCommandGetROffset(aCmd);
+	int ROffset = -1; // TODO: add params with typed args: s,r,i
+
+	// max 8 args of 128 len each
+	const int MAX_ARGS = 16;
+	char **ppArgs = new char *[MAX_ARGS];
+	for(int x = 0; x < MAX_ARGS; ++x)
+	{
+		ppArgs[x] = new char[MAX_ARG_LEN];
+		ppArgs[x][0] = '\0';
+	}
+	int NumArgs = 0;
+	int k = 0;
+	while(pCmdWithArgs[i])
+	{
+		if(k + 1 >= MAX_ARG_LEN)
+		{
+			dbg_msg("ddnet-insta", "ERROR: chat command has too long arg");
+			break;
+		}
+		if(NumArgs + 1 >= MAX_ARGS)
+		{
+			dbg_msg("ddnet-insta", "ERROR: chat command has too many args");
+			break;
+		}
+		if(pCmdWithArgs[i] == ' ')
+		{
+			// do not delimit on space
+			// if we reached the r parameter
+			if(NumArgs == ROffset)
+			{
+				// strip spaces from the beggining
+				// add spaces in the middle and end
+				if(ppArgs[NumArgs][0])
+				{
+					ppArgs[NumArgs][k] = pCmdWithArgs[i];
+					k++;
+					i++;
+					continue;
+				}
+			}
+			else if(ppArgs[NumArgs][0])
+			{
+				ppArgs[NumArgs][k] = '\0';
+				k = 0;
+				NumArgs++;
+			}
+			i++;
+			continue;
+		}
+		ppArgs[NumArgs][k] = pCmdWithArgs[i];
+		k++;
+		i++;
+	}
+	if(ppArgs[NumArgs][0])
+	{
+		ppArgs[NumArgs][k] = '\0';
+		NumArgs++;
+	}
+
+	char aArgsStr[128];
+	aArgsStr[0] = '\0';
+	for(i = 0; i < NumArgs; i++)
+	{
+		if(aArgsStr[0] != '\0')
+			str_append(aArgsStr, ", ", sizeof(aArgsStr));
+		str_append(aArgsStr, ppArgs[i], sizeof(aArgsStr));
+	}
+
+	char aBuf[512];
+	str_format(aBuf, sizeof(aBuf), "got cmd '%s' with %d args: %s", aCmd, NumArgs, aArgsStr);
+	bool match = OnBangCommand(ClientID, aCmd, NumArgs, (const char **)ppArgs);
+	for(int x = 0; x < 8; ++x)
+		delete[] ppArgs[x];
+	delete[] ppArgs;
+	return match;
+}
+
+bool CGameContext::OnBangCommand(int ClientID, const char *pCmd, int NumArgs, const char **ppArgs)
 {
 	if(ClientID < 0 || ClientID >= MAX_CLIENTS)
-		return;
+		return false;
 	CPlayer *pPlayer = m_apPlayers[ClientID];
 	if(!pPlayer)
-		return;
+		return false;
 	if(pPlayer->GetTeam() == TEAM_SPECTATORS && !g_Config.m_SvSpectatorVotes)
 	{
 		SendChatTarget(ClientID, "Spectators aren't allowed to vote.");
-		return;
+		return false;
 	}
-	const char *pCmd = pLine;
 
 	int SetSlots = -1;
 	const char aaVs[][16] = {"on", "n", "vs", "v"};
@@ -4840,7 +4929,7 @@ void CGameContext::OnBangCommand(const char *pLine, int ClientID)
 	}
 	else if(!str_comp_nocase(pCmd, "restart") || !str_comp_nocase(pCmd, "reload"))
 	{
-		int Seconds = 10;
+		int Seconds = NumArgs > 0 ? atoi(ppArgs[0]) : 10;
 		char aCmd[512];
 		str_format(aCmd, sizeof(aCmd), "restart %d", Seconds);
 		char aDesc[512];
@@ -4855,6 +4944,11 @@ void CGameContext::OnBangCommand(const char *pLine, int ClientID)
 	{
 		ComCallShuffleVote(ClientID);
 	}
+	else
+	{
+		return false;
+	}
+	return true;
 }
 
 void CGameContext::SendGameMsg(int GameMsgID, int ClientID)
--- src/game/server/gamecontext.h
+++ src/game/server/gamecontext.h
@@ -271,7 +271,8 @@ public:
 	void SendGameMsg(int GameMsgID, int ParaI1, int ClientID);
 	void SendGameMsg(int GameMsgID, int ParaI1, int ParaI2, int ParaI3, int ClientID); // gctf
 	int GetDDNetInstaWeapon();
-	void OnBangCommand(const char *pLine, int ClientID);
+	bool ParseChatCmd(char Prefix, int ClientID, const char *pCmdWithArgs);
+	bool OnBangCommand(int ClientID, const char *pCmd, int NumArgs, const char **ppArgs);
 	void BangCommandVote(int ClientID, const char *pCommand, const char *pDesc);
 	void ComCallShuffleVote(int ClientID);
 
