60e6025e7a04b3100a7383d2a6cae5a03ccfc37a
ChillerDragon <ChillerDragon@gmail.com>
2023-11-16 10:43:03 +0100

    Move m_TeleOuts from ddr controller to game controller
     #7127

--- src/game/server/gamecontroller.cpp
+++ src/game/server/gamecontroller.cpp
@@ -38,6 +38,8 @@ IGameController::IGameController(class CGameContext *pGameServer)
 	m_ForceBalanced = false;
 
 	m_CurrentRecord = 0;
+
+	InitTeleporter();
 }
 
 IGameController::~IGameController() = default;
@@ -484,6 +486,8 @@ void IGameController::OnCharacterSpawn(class CCharacter *pChr)
 	// give default weapons
 	pChr->GiveWeapon(WEAPON_HAMMER);
 	pChr->GiveWeapon(WEAPON_GUN);
+
+	pChr->SetTeleports(&m_TeleOuts, &m_TeleCheckOuts);
 }
 
 void IGameController::HandleCharacterTiles(CCharacter *pChr, int MapIndex)
@@ -725,6 +729,31 @@ CClientMask IGameController::GetMaskForPlayerWorldEvent(int Asker, int ExceptID)
 {
 	// Send all world events to everyone by default
 	return CClientMask().set().reset(ExceptID);
+}
+
+void IGameController::InitTeleporter()
+{
+	if(!GameServer()->Collision()->Layers()->TeleLayer())
+		return;
+	int Width = GameServer()->Collision()->Layers()->TeleLayer()->m_Width;
+	int Height = GameServer()->Collision()->Layers()->TeleLayer()->m_Height;
+
+	for(int i = 0; i < Width * Height; i++)
+	{
+		int Number = GameServer()->Collision()->TeleLayer()[i].m_Number;
+		int Type = GameServer()->Collision()->TeleLayer()[i].m_Type;
+		if(Number > 0)
+		{
+			if(Type == TILE_TELEOUT)
+			{
+				m_TeleOuts[Number - 1].emplace_back(i % Width * 32.0f + 16.0f, i / Width * 32.0f + 16.0f);
+			}
+			else if(Type == TILE_TELECHECKOUT)
+			{
+				m_TeleCheckOuts[Number - 1].emplace_back(i % Width * 32.0f + 16.0f, i / Width * 32.0f + 16.0f);
+			}
+		}
+	}
 }
 
 void IGameController::DoTeamChange(CPlayer *pPlayer, int Team, bool DoChatMsg)
