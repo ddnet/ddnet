5a1839023ea2545bb4889d9bbc3f7e00320dc080
ChillerDragon <ChillerDragon@gmail.com>
2023-12-13 18:39:22 +0100

    Fix style

--- src/game/server/instagib/strhelpers.h
+++ src/game/server/instagib/strhelpers.h
@@ -1,7 +1,5 @@
-// do we need some gctf system.h?
-
-#ifndef GAME_SERVER_INSTAGIB_H
-#define GAME_SERVER_INSTAGIB_H
+#ifndef GAME_SERVER_INSTAGIB_STRHELPERS_H
+#define GAME_SERVER_INSTAGIB_STRHELPERS_H
 
 #include "stdio.h"
 #include <cctype>
