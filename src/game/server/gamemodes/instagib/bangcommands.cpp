ba2bd1b15af7e396514272eb87cea08ce30b4403
ChillerDragon <chillerdragon@gmail.com>
2023-12-20 15:38:31 +0100

    Make bang vote code dryer

--- src/game/server/gamemodes/instagib/bangcommands.cpp
+++ src/game/server/gamemodes/instagib/bangcommands.cpp
@@ -7,6 +7,17 @@
 
 void CGameControllerInstagib::BangCommandVote(int ClientID, const char *pCommand, const char *pDesc)
 {
+	if(ClientID < 0 || ClientID >= MAX_CLIENTS)
+		return;
+	CPlayer *pPlayer = GameServer()->m_apPlayers[ClientID];
+	if(!pPlayer)
+		return;
+	// not needed for bang command but for slash command
+	if(pPlayer->GetTeam() == TEAM_SPECTATORS && !g_Config.m_SvSpectatorVotes)
+	{
+		SendChatTarget(ClientID, "Spectators aren't allowed to vote.");
+		return;
+	}
 	char aChatmsg[1024];
 	str_format(aChatmsg, sizeof(aChatmsg), "'%s' called vote to change server option '%s'", Server()->ClientName(ClientID), pDesc);
 	GameServer()->CallVote(ClientID, pDesc, pCommand, "chat cmd", aChatmsg);
@@ -14,48 +25,15 @@ void CGameControllerInstagib::BangCommandVote(int ClientID, const char *pCommand
 
 void CGameControllerInstagib::ComCallShuffleVote(int ClientID)
 {
-	if(ClientID < 0 || ClientID >= MAX_CLIENTS)
-		return;
-	CPlayer *pPlayer = GameServer()->m_apPlayers[ClientID];
-	if(!pPlayer)
-		return;
-	// not needed for bang command but for slash command
-	if(pPlayer->GetTeam() == TEAM_SPECTATORS && !g_Config.m_SvSpectatorVotes)
-	{
-		SendChatTarget(ClientID, "Spectators aren't allowed to vote.");
-		return;
-	}
 	BangCommandVote(ClientID, "shuffle_teams", "shuffle teams");
 }
 
 void CGameControllerInstagib::ComCallSwapTeamsVote(int ClientID)
 {
-	if(ClientID < 0 || ClientID >= MAX_CLIENTS)
-		return;
-	CPlayer *pPlayer = GameServer()->m_apPlayers[ClientID];
-	if(!pPlayer)
-		return;
-	// not needed for bang command but for slash command
-	if(pPlayer->GetTeam() == TEAM_SPECTATORS && !g_Config.m_SvSpectatorVotes)
-	{
-		SendChatTarget(ClientID, "Spectators aren't allowed to vote.");
-		return;
-	}
 	BangCommandVote(ClientID, "swap_teams", "swap teams");
 }
 
 void CGameControllerInstagib::ComCallSwapTeamsRandomVote(int ClientID)
 {
-	if(ClientID < 0 || ClientID >= MAX_CLIENTS)
-		return;
-	CPlayer *pPlayer = GameServer()->m_apPlayers[ClientID];
-	if(!pPlayer)
-		return;
-	// not needed for bang command but for slash command
-	if(pPlayer->GetTeam() == TEAM_SPECTATORS && !g_Config.m_SvSpectatorVotes)
-	{
-		SendChatTarget(ClientID, "Spectators aren't allowed to vote.");
-		return;
-	}
 	BangCommandVote(ClientID, "swap_teams_random", "swap teams (random)");
 }
