dc2c2699de5b4c3c2385643e5bc9323cb3ba8be9
Sefa Eyeoglu <contact@scrumplex.net>
2023-12-27 16:50:18 +0100

    Allow switching teams after finish
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- src/game/server/teams.cpp
+++ src/game/server/teams.cpp
@@ -377,7 +377,7 @@ const char *CGameTeams::SetCharacterTeam(int ClientID, int Team)
 		return "Your character is not valid";
 	if(Team == TEAM_SUPER && !Character(ClientID)->IsSuper())
 		return "You can't join super team if you don't have super rights";
-	if(Team != TEAM_SUPER && Character(ClientID)->m_DDRaceState != DDRACE_NONE)
+	if(Team != TEAM_SUPER && Character(ClientID)->m_DDRaceState != DDRACE_NONE && m_aTeamState[m_Core.Team(ClientID)] < TEAMSTATE_FINISHED)
 		return "You have started racing already";
 	// No cheating through noob filter with practice and then leaving team
 	if(m_aPractice[m_Core.Team(ClientID)])
