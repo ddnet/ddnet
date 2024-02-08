860a472d5cb2f9f866da60d306be97c8ecb24c56
ChillerDragon <ChillerDragon@gmail.com>
2024-02-08 15:59:26 +0800

    Cleanup editor object comments

--- src/game/editor/editor_object.h
+++ src/game/editor/editor_object.h
@@ -26,7 +26,7 @@ public:
 	/**
 	 * Initialise the component and interface pointers.
 	 * Needs to be the first function that is called.
-     * The default implentation also resets the component.
+	 * The default implentation also resets the component.
 	 */
 	virtual void Init(CEditor *pEditor);
 
@@ -45,7 +45,6 @@ public:
 
 	/**
 	 * Gets called after `OnRender` when the component is hot but not active.
-	 * I
 	 */
 	virtual void OnHot();
 
