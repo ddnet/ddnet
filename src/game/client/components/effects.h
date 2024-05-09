e732381db57685408bd4d9998605bf0eb6750eb6
ChillerDragon <ChillerDragon@gmail.com>
2024-05-09 16:45:45 +0800

    Use amout of snow flakes as unfreeze time indicator
    Use amount of snow flakes on a frozen tee to indicate the seconds till unfreeze.
    Only used for 2 and 1 seconds.
    For longer freeze times, freeze tiles, hearts, deep freeze the old
    random animation is used.
    
    closes #5527

--- src/game/client/components/effects.h
+++ src/game/client/components/effects.h
@@ -30,7 +30,7 @@ public:
 	void PlayerSpawn(vec2 Pos, float Alpha = 1.0f);
 	void PlayerDeath(vec2 Pos, int ClientId, float Alpha = 1.0f);
 	void PowerupShine(vec2 Pos, vec2 Size, float Alpha = 1.0f);
-	void FreezingFlakes(vec2 Pos, vec2 Size, float Alpha = 1.0f);
+	void FreezingFlakes(vec2 Pos, vec2 Size, float Alpha = 1.0f, bool Force = false);
 
 	void Update();
 };
