18b9e1e2a1464fc31d31f79860f41ff234f1d41e
ChillerDragon <ChillerDragon@gmail.com>
2024-05-28 09:53:49 +0800

    shellformat insta docs script

--- scripts/gendocs_instagib.sh
+++ scripts/gendocs_instagib.sh
@@ -2,8 +2,7 @@
 
 arg_is_dry=0
 
-if [ "$1" == "--dry-run" ]
-then
+if [ "$1" == "--dry-run" ]; then
 	arg_is_dry=1
 fi
 
@@ -18,15 +17,13 @@ gen_configs() {
 	local cmd
 	# shellcheck disable=SC2016
 	echo '+ `sv_gametype` Game type (gctf, ictf)'
-	while read -r cfg
-	do
+	while read -r cfg; do
 		desc="$(echo "$cfg" | cut -d',' -f7- | cut -d'"' -f2-)"
 		desc="${desc::-2}"
 		cmd="$(echo "$cfg" | cut -d',' -f2 | xargs)"
 		echo "+ \`$cmd\` $desc"
 	done < <(grep '^MACRO_CONFIG_INT' src/engine/shared/variables_insta.h)
-	while read -r cfg
-	do
+	while read -r cfg; do
 		desc="$(echo "$cfg" | cut -d',' -f6- | cut -d'"' -f2-)"
 		desc="${desc::-2}"
 		cmd="$(echo "$cfg" | cut -d',' -f2 | xargs)"
@@ -38,8 +35,7 @@ gen_rcon_cmds() {
 	local cfg
 	local desc
 	local cmd
-	while read -r cfg
-	do
+	while read -r cfg; do
 		desc="$(echo "$cfg" | cut -d',' -f3- | cut -d'"' -f2-)"
 		desc="${desc::-2}"
 		cmd="$(echo "$cfg" | cut -d',' -f1 | cut -d'"' -f2)"
@@ -55,25 +51,22 @@ insert_at() {
 	local filename="$4"
 	local from_ln
 	local to_ln
-	if ! grep -q "$from_pattern" "$filename"
-	then
+	if ! grep -q "$from_pattern" "$filename"; then
 		echo "Error: pattern '$from_pattern' not found in '$filename'"
 		exit 1
 	fi
 	from_ln="$(grep -n "$from_pattern" "$filename" | cut -d':' -f1 | head -n1)"
-	from_ln="$((from_ln+1))"
+	from_ln="$((from_ln + 1))"
 	to_ln="$(tail -n +"$from_ln" "$filename" | grep -n "$to_pattern" | cut -d':' -f1 | head -n1)"
-	to_ln="$((from_ln+to_ln-2))"
+	to_ln="$((from_ln + to_ln - 2))"
 
 	{
 		head -n "$from_ln" "$filename"
 		echo "$content"
 		tail -n +"$to_ln" "$filename"
 	} > "$(tmp)"
-	if [ "$arg_is_dry" == "1" ]
-	then
-		if [ "$(cat "$(tmp)")" != "$(cat "$filename")" ]
-		then
+	if [ "$arg_is_dry" == "1" ]; then
+		if [ "$(cat "$(tmp)")" != "$(cat "$filename")" ]; then
 			echo "Error: missing docs for $filename"
 			echo "       run ./scripts/gendocs_instagib.sh"
 			git diff --no-index --color "$(tmp)" "$filename"
