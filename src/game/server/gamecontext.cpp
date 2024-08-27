9fbbf5cde7c86f150f7c11dc75ac3a78b6ae18af
furo <furo@posteo.net>
2024-08-27 21:43:22 +0200

    Fix using `random_unfinished_map` from rcon

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -3140,6 +3140,8 @@ void CGameContext::ConRandomUnfinishedMap(IConsole::IResult *pResult, void *pUse
 	CGameContext *pSelf = (CGameContext *)pUserData;
 
 	int Stars = pResult->NumArguments() ? pResult->GetInteger(0) : -1;
+	if(pResult->m_ClientId != -1)
+		pSelf->m_VoteCreator = pResult->m_ClientId;
 
 	pSelf->m_pScore->RandomUnfinishedMap(pSelf->m_VoteCreator, Stars);
 }
