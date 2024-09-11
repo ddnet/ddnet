b69d0230d06236b127651f4a571798b0eefffc61
ChillerDragon <ChillerDragon@gmail.com>
2024-09-11 17:03:49 +0800

    stats: reorder

--- src/game/server/instagib/sql_stats_player.h
+++ src/game/server/instagib/sql_stats_player.h
@@ -4,19 +4,21 @@
 class CSqlStatsPlayer
 {
 public:
-	// Will also be set if spree chat messages are turned off
-	int m_Spree;
 	// kills, deaths and flag grabs/caps are tracked per round
 	int m_Kills;
 	int m_Deaths;
+
+	// Will also be set if spree chat messages are turned off
+	int m_Spree;
+
 	int m_FlagCaptures;
 	int m_FlagGrabs;
 
 	void Reset()
 	{
-		m_Spree = 0;
 		m_Kills = 0;
 		m_Deaths = 0;
+		m_Spree = 0;
 		m_FlagCaptures = 0;
 		m_FlagGrabs = 0;
 	}
