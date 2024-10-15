38da6cb76abaa51cc3c62b811e08065b4d6e76bb
qxdFox <92627325+qxdFox@users.noreply.github.com>
2024-10-15 15:44:41 +0200

    Nobo fixing crash

--- src/game/client/components/hud.cpp
+++ src/game/client/components/hud.cpp
@@ -600,25 +600,25 @@ void CHud::RenderCursor()
 	RenderTools()->MapScreenToInterface(m_pClient->m_Camera.m_Center.x, m_pClient->m_Camera.m_Center.y);
 
 	// render cursor
-	if(g_Config.m_ClDoCursorSpecOpacity)
+	if (m_pClient->m_Snap.m_SpecInfo.m_SpectatorId != SPEC_FREEVIEW)
 	{
-		if(m_pClient->m_Snap.m_SpecInfo.m_SpectatorId != SPEC_FREEVIEW)
-		{
-			CurWeapon = m_pClient->m_Snap.m_pLocalCharacter->m_Weapon % NUM_WEAPONS;
-			Graphics()->SetColor(1.f, 1.f, 1.f, 1.f);
-		}
-		else
-		{
-			Graphics()->SetColor(1.f, 1.f, 1.f, g_Config.m_ClRenderCursorSpecOpacity * 0.01);
-		}
-	}
-	else
+
 		CurWeapon = m_pClient->m_Snap.m_pLocalCharacter->m_Weapon % NUM_WEAPONS;
 		Graphics()->SetColor(1.f, 1.f, 1.f, 1.f);
-
-	Graphics()->TextureSet(m_pClient->m_GameSkin.m_aSpriteWeaponCursors[CurWeapon]);
-	Graphics()->RenderQuadContainerAsSprite(m_HudQuadContainerIndex, m_aCursorOffset[CurWeapon], m_pClient->m_Controls.m_aTargetPos[g_Config.m_ClDummy].x, m_pClient->m_Controls.m_aTargetPos[g_Config.m_ClDummy].y);
+		Graphics()->TextureSet(m_pClient->m_GameSkin.m_aSpriteWeaponCursors[CurWeapon]);
+		Graphics()->RenderQuadContainerAsSprite(m_HudQuadContainerIndex, m_aCursorOffset[CurWeapon], m_pClient->m_Controls.m_aTargetPos[g_Config.m_ClDummy].x, m_pClient->m_Controls.m_aTargetPos[g_Config.m_ClDummy].y);
+	}
+	else
+	{
+		if(g_Config.m_ClDoCursorSpecOpacity)
+			Graphics()->SetColor(1.f, 1.f, 1.f, g_Config.m_ClRenderCursorSpecOpacity * 0.01);
+		else
+			Graphics()->SetColor(1.f, 1.f, 1.f, 1.f);
+		Graphics()->TextureSet(m_pClient->m_GameSkin.m_aSpriteWeaponCursors[CurWeapon]);
+		Graphics()->RenderQuadContainerAsSprite(m_HudQuadContainerIndex, m_aCursorOffset[CurWeapon], m_pClient->m_Controls.m_aTargetPos[g_Config.m_ClDummy].x, m_pClient->m_Controls.m_aTargetPos[g_Config.m_ClDummy].y);
+	}
 }
+
 
 void CHud::PrepareAmmoHealthAndArmorQuads()
 {
