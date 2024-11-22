7b52d3d1acf5e7547e960f7f6722d871f007feef
Teero888 <teero484@gmail.com>
2024-11-22 19:20:01 +0100

    fix the rainbow component

--- src/game/client/components/rainbow.h
+++ src/game/client/components/rainbow.h
@@ -1,15 +1,16 @@
 
 #ifndef GAME_CLIENT_COMPONENTS_RAINBOW_H
 #define GAME_CLIENT_COMPONENTS_RAINBOW_H
+#include "game/client/render.h"
 #include <game/client/component.h>
 
 class CRainbow : public CComponent
 {
 public:
-	virtual int Sizeof() const override { return sizeof(*this); }
-	virtual void OnRender() override;
+	int Sizeof() const override { return sizeof(*this); }
+	void OnRender() override;
 
-	void TransformColor(unsigned char mode, int tick, CTeeRenderInfo *pinfo);
+	void TransformColor(unsigned char Mode, int Tick, CTeeRenderInfo *pInfo);
 	enum COLORMODE
 	{
 		COLORMODE_RAINBOW = 1,
