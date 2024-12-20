5068daca47cd70a2a1adf3ebdaa9fc4f91ea2730
ASKLL <askll520@outlook.com>
2024-12-21 01:01:39 +0800

    Style

--- src/engine/shared/console.cpp
+++ src/engine/shared/console.cpp
@@ -235,13 +235,13 @@ int CConsole::ParseArgs(CResult *pResult, const char *pFormat, bool IsColor)
 					if(!IsColor)
 					{
 						int Value;
-						if(str_comp_nocase(pResult->GetString(pResult->NumArguments() - 1), "all") == 0 || str_comp_nocase(pResult->GetString(pResult->NumArguments() - 1), "me") == 0)
-                    				{
-                        				Error = PARSEARGS_OK;
+						if(str_comp_nocase(pResult->GetString(pResult->NumArguments() - 1), "all") == 0 ||
+							str_comp_nocase(pResult->GetString(pResult->NumArguments() - 1), "me") == 0)
+						{
+							Error = PARSEARGS_OK;
 							break;
-                    				}
-						if(!str_toint(pResult->GetString(pResult->NumArguments() - 1), &Value) ||
-							Value == std::numeric_limits<int>::max() || Value == std::numeric_limits<int>::min())
+						}
+						if(!str_toint(pResult->GetString(pResult->NumArguments() - 1), &Value) || Value == std::numeric_limits<int>::max() || Value == std::numeric_limits<int>::min())
 						{
 							Error = PARSEARGS_INVALID_INTEGER;
 							break;
