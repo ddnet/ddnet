8f8ab8470e97cc41ecf9f64a1a1aad358b0f0547
Robert Müller <robytemueller@gmail.com>
2024-12-24 12:46:00 +0100

    Improve sound sample thread-safety and add assertions
    Lock the sound lock when accessing any of the sound samples, voices and channels.
    
    Add assertions to ensure that sound sample IDs are within range and that samples are loaded.
    
    Rename parameter `FromEditor` to `ForceLoad` to make usage clearer.
    
    Closes #8262.

--- src/engine/sound.h
+++ src/engine/sound.h
@@ -67,8 +67,8 @@ public:
 
 	virtual int LoadOpus(const char *pFilename, int StorageType = IStorage::TYPE_ALL) = 0;
 	virtual int LoadWV(const char *pFilename, int StorageType = IStorage::TYPE_ALL) = 0;
-	virtual int LoadOpusFromMem(const void *pData, unsigned DataSize, bool FromEditor = false) = 0;
-	virtual int LoadWVFromMem(const void *pData, unsigned DataSize, bool FromEditor = false) = 0;
+	virtual int LoadOpusFromMem(const void *pData, unsigned DataSize, bool ForceLoad = false) = 0;
+	virtual int LoadWVFromMem(const void *pData, unsigned DataSize, bool ForceLoad = false) = 0;
 	virtual void UnloadSample(int SampleId) = 0;
 
 	virtual float GetSampleTotalTime(int SampleId) = 0; // in s
