3445fdfc12b74365789ed2cb4bd728b8b6f50401
Tater <Mr.Potatooh@gmail.com>
2025-01-02 01:53:39 -0600

    trail rewrite progress

--- src/game/client/components/tclient/trails.cpp
+++ src/game/client/components/tclient/trails.cpp
@@ -64,7 +64,7 @@ void CTrails::ManageServerIntraTick()
 	m_RealIntraTick = TimeThisTick / TimeBetweenTicks;
 	m_RealIntraTick = std::clamp(m_RealIntraTick, 0.0f, 1.0f);
 	// char aBuf[32];
-	// str_format(aBuf, sizeof(aBuf), "%.2f", m_RealIntraTick);
+	// str_format(aBuf, sizeof(aBuf), "%.2f", Client()->IntraGameTick(g_Config.m_ClDummy));
 	// GameClient()->Echo(aBuf);
 }
 
@@ -123,6 +123,7 @@ void CTrails::OnRender()
 		{
 			StartTick = GameTick;
 			IntraTick = m_RealIntraTick;
+			IntraTick = Client()->IntraGameTick(g_Config.m_ClDummy);
 		}
 
 		vec2 CurServerPos = vec2(GameClient()->m_Snap.m_aCharacters[ClientId].m_Cur.m_X, GameClient()->m_Snap.m_aCharacters[ClientId].m_Cur.m_Y);
@@ -136,10 +137,6 @@ void CTrails::OnRender()
 
 		IGraphics::CLineItem LineItem;
 		bool LineMode = g_Config.m_ClTeeTrailWidth == 0;
-		if(!LineMode)
-			Graphics()->TrianglesBegin();
-		else
-			Graphics()->LinesBegin();
 
 		float Alpha = g_Config.m_ClTeeTrailAlpha / 100.0f;
 		ColorRGBA Col = color_cast<ColorRGBA>(ColorHSLA(g_Config.m_ClTeeTrailColor));
@@ -151,9 +148,11 @@ void CTrails::OnRender()
 		ColorRGBA FullCol = Col.WithAlpha(Alpha);
 		ColorRGBA PrevCol = FullCol;
 		Graphics()->SetColor(FullCol);
-		vec2 PrevPrevPos = vec2(0,0);
+		vec2 PrevPrevPos = vec2(0, 0);
+		int PrevPrevPosTick = 0;
 		bool PrevPrevInit = false;
 		vec2 PrevPos = vec2(0, 0);
+		int PrevPosTick = 0;
 		bool PrevInit = false;
 		vec2 Pos = vec2(0, 0);
 		bool Init = false;
@@ -168,147 +167,382 @@ void CTrails::OnRender()
 		vec2 PrevBot = vec2(0, 0);
 		bool WidthInit = false;
 		float Width = g_Config.m_ClTeeTrailWidth;
-		// vec2 PrevNormal = vec2(0, 0);
-
-		// This is a huge mess but it works fine, don't copy any of this
-		for(int i = 0; i < TrailLength; i++)
+		int DrawTick = -1;
+		if(false)
 		{
-			Progress = float(i) / (TrailLength - 1);
-			float InverseProgress = 1.0f - Progress;
-			int PosTick = StartTick - i;
-			bool LastDraw = false;
-			if(PredictPlayer)
-			{
-				if(GameClient()->m_aClients[ClientId].m_aPredTick[PosTick % 200] != PosTick)
-					continue;
-				Pos = GameClient()->m_aClients[ClientId].m_aPredPos[PosTick % 200];
-				Init = true;
-				LastDraw = i == TrailLength - 1;
-			}
+			if(!LineMode)
+				Graphics()->TrianglesBegin();
 			else
+				Graphics()->LinesBegin();
+
+			// vec2 PrevNormal = vec2(0, 0);
+			bool FirstDraw = true;
+			// This is a huge mess but it works fine, don't copy any of this
+			for(int i = 0; i < TrailLength; i++)
 			{
-				if(m_PositionTick[ClientId][PosTick % 200] != PosTick)
+				int PosTick = StartTick - i;
+				bool LastDraw = false;
+				if(PredictPlayer)
+				{
+					if(GameClient()->m_aClients[ClientId].m_aPredTick[PosTick % 200] != PosTick)
+						continue;
+					Pos = GameClient()->m_aClients[ClientId].m_aPredPos[PosTick % 200];
+					Init = true;
+					LastDraw = i == TrailLength - 1;
+				}
+				else
+				{
+					if(m_PositionTick[ClientId][PosTick % 200] != PosTick)
+						continue;
+					Pos = m_PositionHistory[ClientId][PosTick % 200];
+					Init = true;
+					int LastDrawPos = PosTick;
+					bool ValidFuture = (m_PositionTick[ClientId][(LastDrawPos - 2) % 200] == LastDrawPos - 2) ||
+							   (m_PositionTick[ClientId][(LastDrawPos - 1) % 200] == LastDrawPos - 1);
+					LastDraw = !ValidFuture || i >= TrailLength - 2;
+				}
+				if(Pos == PrevPos)
 					continue;
-				Pos = m_PositionHistory[ClientId][PosTick % 200];
-				Init = true;
-				LastDraw = (m_PositionTick[ClientId][(PosTick - 1) % 200] != PosTick - 1) && i > TrailLength - 3;
+
+				// int OffsetTick = 1;
+				// if(!PredictPlayer)
+				//	OffsetTick = 2;
+
+				Progress = ((double)(StartTick - PosTick) + IntraTick) / (TrailLength - 1);
+				float InverseProgress = 1.0f - Progress;
+
+				if(g_Config.m_ClTeeTrailRainbow)
+				{
+					float Cycle = (1.0f / TrailLength) * 0.5f;
+					float Hue = std::fmod(((PosTick + 6361 * ClientId) % 1000000) * Cycle, 1.0f);
+					Col = color_cast<ColorRGBA>(ColorHSLA(Hue, 1.0f, 0.5f));
+					FullCol = Col.WithAlpha(Alpha);
+					Graphics()->SetColor(FullCol);
+				}
+
+				if(g_Config.m_ClTeeTrailTaper)
+					Width = g_Config.m_ClTeeTrailWidth * InverseProgress;
+
+				if(g_Config.m_ClTeeTrailFade)
+				{
+					float FadeAlpha = Alpha * InverseProgress;
+					FullCol = Col.WithAlpha(FadeAlpha);
+					Graphics()->SetColor(FullCol);
+				}
+				if(PrevPrevInit && distance(PrevPos, Pos) < 600.0f)
+				{
+					DrawTick = PosTick;
+					if(LastDraw && LineMode)
+						Pos = mix(Pos, PrevPos, std::fmod(IntraTick / 2.0f, 1.0f));
+					else if(LastDraw && !PredictPlayer)
+					{
+						// PrevPos = mix(PrevPos, PrevPrevPos, IntraTick / 2.0f);
+					}
+					else if(LastDraw)
+						PrevPos = mix(PrevPos, PrevPrevPos, IntraTick);
+					if(LineMode)
+					{
+						LineItem = IGraphics::CLineItem(PrevPos.x, PrevPos.y, Pos.x, Pos.y);
+						Graphics()->LinesDraw(&LineItem, 1);
+					}
+					vec2 PrevDirection = normalize(PrevPos - PrevPrevPos);
+					vec2 Direction = normalize(Pos - PrevPos);
+					vec2 Normal = vec2(-PrevDirection.y, PrevDirection.x);
+
+					// if(!WidthInit)
+					//	PrevNormal = Normal;
+
+					vec2 Tanget = normalize(normalize(Pos - PrevPos) + normalize(PrevPos - PrevPrevPos));
+					if(Tanget == vec2(0, 0))
+						Tanget = Normal;
+					PerpVec = vec2(-Tanget.y, Tanget.x);
+					if(!PerpInit)
+					{
+						PrevPerpVec = Normal;
+					}
+
+					float ScaledWidth = Width /*/ dot(Normal, PerpVec)*/;
+					float TopScaled = ScaledWidth;
+					float BotScaled = ScaledWidth;
+					if(dot(PrevDirection, Tanget) > 0.0f)
+						TopScaled = std::min(Width * 3.0f, TopScaled);
+					else
+						BotScaled = std::min(Width * 3.0f, BotScaled);
+
+					vec2 Top = PrevPos + PerpVec * TopScaled;
+					vec2 Bot = PrevPos - PerpVec * BotScaled;
+					vec2 TempTop = Top;
+					vec2 TempBot = Bot;
+
+					if(dot(PrevDirection, Direction) < -0.25f)
+					{
+						Top = PrevPos - Tanget * Width;
+						Bot = PrevPos + Tanget * Width;
+						TempTop = Top;
+						TempBot = Bot;
+						float Det = PrevDirection.x * Direction.y - PrevDirection.y * Direction.x;
+						if(Det >= 0.0f)
+						{
+							vec2 TempPos = PrevTop;
+							PrevTop = PrevBot;
+							PrevBot = TempPos;
+							ScaledWidth = 0.0f;
+						}
+						else
+						{
+							TempTop = Bot;
+							TempBot = Top;
+						}
+					}
+					// float MinGap = length(Normal - PrevNormal) * ScaledWidth;
+					// if(distance(PrevPos, Pos) < MinGap)
+					//	continue;
+
+					if(!WidthInit)
+					{
+						Width = g_Config.m_ClTeeTrailWidth;
+						PrevTop = PrevPrevPos + PrevPerpVec * Width;
+						PrevBot = PrevPrevPos - PrevPerpVec * Width;
+					}
+					if(distance(PrevPos, PrevPrevPos) < 600.0f && !LineMode)
+					{
+						Graphics()->SetColor4(FullCol, FullCol, PrevCol, PrevCol);
+						IGraphics::CFreeformItem FreeformItem(Top.x, Top.y, Bot.x, Bot.y, PrevTop.x, PrevTop.y, PrevBot.x, PrevBot.y);
+						Graphics()->QuadsDrawFreeform(&FreeformItem, 1);
+					}
+					PrevTop = TempTop;
+					PrevBot = TempBot;
+					// PrevNormal = Normal;
+					WidthInit = true;
+
+					PrevPerpVec = PerpVec, PerpInit = true;
+				}
+				if(PrevInit)
+					PrevPrevPos = PrevPos, PrevPrevPosTick = PrevPosTick, PrevPrevInit = true;
+				if(Init)
+					PrevPos = Pos, PrevPosTick = PosTick, PrevInit = true;
+
+				PrevCol = FullCol;
+
+				// if(LastDraw)
+				//	break;
 			}
-			if(Pos == PrevPos)
+
+			if(!LineMode)
+				Graphics()->TrianglesEnd();
+			else
+				Graphics()->LinesEnd();
+		}
+
+		if(true)
+		{
+			// ATTEMPT 2
+			static std::vector<STrailPart> Trail;
+			Trail.clear();
+
+			if(!PredictPlayer)
+				TrailLength += 2;
+
+			// Fill trail list with initial positions
+			for(int i = 0; i < TrailLength; i++)
+			{
+				STrailPart Part;
+				int PosTick = StartTick - i;
+				if(PredictPlayer)
+				{
+					if(GameClient()->m_aClients[ClientId].m_aPredTick[PosTick % 200] != PosTick)
+						continue;
+					Part.Pos = GameClient()->m_aClients[ClientId].m_aPredPos[PosTick % 200];
+				}
+				else
+				{
+					if(m_PositionTick[ClientId][PosTick % 200] != PosTick)
+						continue;
+					Part.Pos = m_PositionHistory[ClientId][PosTick % 200];
+				}
+				Trail.push_back(Part);
+			}
+
+			// Trim the ends if intratick is too big
+			// this was not trivial to figure out
+			int TrimTicks = (int)IntraTick;
+			for(int i = 0; i < TrimTicks; i++)
+				Trail.pop_back();
+
+			// Stuff breaks if we have less than 3 points because we cannot calculate an angle between segments to preserve constant width
+			// TODO: Pad the list with generated entries in the same direction as before
+			if((int)Trail.size() < 3)
 				continue;
 
-			if(g_Config.m_ClTeeTrailRainbow)
-			{
-				float Cycle = (1.0f / TrailLength) * 0.5f;
-				float Hue = std::fmod(((PosTick + 6361 * ClientId) % 1000000) * Cycle, 1.0f);
-				Col = color_cast<ColorRGBA>(ColorHSLA(Hue, 1.0f, 0.5f));
-				FullCol = Col.WithAlpha(Alpha);
-				Graphics()->SetColor(FullCol);
-			}
+			float Trim = 0.0f;
+			if(!PredictPlayer)
+				Trim = 1.0f;
 
-			if(g_Config.m_ClTeeTrailTaper)
-				Width = g_Config.m_ClTeeTrailWidth * InverseProgress;
+			Trail.at(Trail.size() - 1).Pos = mix(Trail.at(Trail.size() - 1).Pos, Trail.at(Trail.size() - 2).Pos, std::fmod(IntraTick, 1.0f));
 
-			if(g_Config.m_ClTeeTrailFade)
+			// Set progress
+			for(int i = 0; i < (int)Trail.size(); i++)
 			{
-				float FadeAlpha = Alpha * InverseProgress;
-				FullCol = Col.WithAlpha(FadeAlpha);
-				Graphics()->SetColor(FullCol);
-			}
-			if(PrevPrevInit && distance(PrevPos, Pos) < 600.0f)
-			{
-				if(LastDraw && LineMode)
-					Pos = mix(Pos, PrevPos, IntraTick);
-				else if(LastDraw)
-					PrevPos = mix(PrevPos, PrevPrevPos, IntraTick);
+				float Size = float(Trail.size() - 1 + TrimTicks);
+				STrailPart &Part = Trail.at(i);
+				if(i == 0)
+					Part.Progress = 0.0f;
+				else if(i == (int)Trail.size() - 1)
+					Part.Progress = 1.0f;
+				else
+					Part.Progress = ((float)(i) + IntraTick - Trim) / (Size - Trim);
 
-				if(LineMode)
+				Part.Col = Col;
+				Part.Alpha = Alpha;
+				if(g_Config.m_ClTeeTrailRainbow)
 				{
-					LineItem = IGraphics::CLineItem(PrevPos.x, PrevPos.y, Pos.x, Pos.y);
-					Graphics()->LinesDraw(&LineItem, 1);
+					float Cycle = (1.0f / TrailLength) * 0.5f;
+					float Hue = std::fmod(((StartTick - i + 6361 * ClientId) % 1000000) * Cycle, 1.0f);
+					Part.Col = color_cast<ColorRGBA>(ColorHSLA(Hue, 1.0f, 0.5f));
 				}
-				vec2 PrevDirection = normalize(PrevPos - PrevPrevPos);
-				vec2 Direction = normalize(Pos - PrevPos);
+				if(g_Config.m_ClTeeTrailTaper)
+					Width = g_Config.m_ClTeeTrailWidth * (1.0 - Part.Progress);
+				if(g_Config.m_ClTeeTrailFade)
+					Part.Alpha = Alpha * (1.0 - Part.Progress);
+			}
+
+			auto NewEnd = std::unique(Trail.begin(), Trail.end());
+			Trail.erase(NewEnd, Trail.end());
+
+			if((int)Trail.size() < 3)
+				continue;
+
+			// Calculate the widths
+			for(int i = 0; i < (int)Trail.size(); i++)
+			{
+				STrailPart &Part = Trail.at(i);
+				vec2 PrevPos;
+				vec2 Pos = Trail.at(i).Pos;
+				vec2 NextPos;
+
+				if(i == 0)
+				{
+					vec2 Direction = normalize(Trail.at(i + 1).Pos - Pos);
+					PrevPos = Pos - Direction;
+				}
+				else
+					PrevPos = Trail.at(i - 1).Pos;
+
+				if(i == (int)Trail.size() - 1)
+				{
+					vec2 Direction = normalize(Pos - Trail.at(i - 1).Pos);
+					NextPos = Pos + Direction;
+				}
+				else
+					NextPos = Trail.at(i + 1).Pos;
+
+				vec2 NextDirection = normalize(NextPos - Pos);
+				vec2 PrevDirection = normalize(Pos - PrevPos);
+
 				vec2 Normal = vec2(-PrevDirection.y, PrevDirection.x);
-
-				// if(!WidthInit)
-				//	PrevNormal = Normal;
-
-				vec2 Tanget = normalize(normalize(Pos - PrevPos) + normalize(PrevPos - PrevPrevPos));
+				Part.Normal = Normal;
+				vec2 Tanget = normalize(NextDirection + PrevDirection);
 				if(Tanget == vec2(0, 0))
 					Tanget = Normal;
+
 				PerpVec = vec2(-Tanget.y, Tanget.x);
-				if(!PerpInit)
-				{
-					PrevPerpVec = Normal;
-				}
-
 				float ScaledWidth = Width / dot(Normal, PerpVec);
-				float TopScaled = ScaledWidth;
-				float BotScaled = ScaledWidth;
-				if(dot(PrevDirection, Tanget) > 0.0f)
-					TopScaled = std::min(Width * 3.0f, TopScaled);
-				else
-					BotScaled = std::min(Width * 3.0f, BotScaled);
+				// float TopScaled = ScaledWidth;
+				// float BotScaled = ScaledWidth;
+				// if(dot(PrevDirection, Tanget) > 0.0f)
+				//	TopScaled = std::min(Width * 3.0f, TopScaled);
+				// else
+				//	BotScaled = std::min(Width * 3.0f, BotScaled);
 
-				vec2 Top = PrevPos + PerpVec * TopScaled;
-				vec2 Bot = PrevPos - PerpVec * BotScaled;
+				vec2 Top = Pos + PerpVec * ScaledWidth;
+				vec2 Bot = Pos - PerpVec * ScaledWidth;
 				vec2 TempTop = Top;
 				vec2 TempBot = Bot;
 
-				if(dot(PrevDirection, Direction) < -0.25f)
+				// Bevel Cap
+				if(dot(PrevDirection, NextDirection) < -0.25f)
 				{
-					Top = PrevPos - Tanget * Width;
-					Bot = PrevPos + Tanget * Width;
+					Top = Pos + Tanget * Width;
+					Bot = Pos - Tanget * Width;
 					TempTop = Top;
 					TempBot = Bot;
-					float Det = PrevDirection.x * Direction.y - PrevDirection.y * Direction.x;
+					float Det = PrevDirection.x * NextDirection.y - PrevDirection.y * NextDirection.x;
 					if(Det >= 0.0f)
 					{
-						vec2 TempPos = PrevTop;
-						PrevTop = PrevBot;
-						PrevBot = TempPos;
-						ScaledWidth = 0.0f;
+						// vec2 TempPos = PrevTop;
+						// PrevTop = PrevBot;
+						// PrevBot = TempPos;
+						// if(i > 0)
+						//	Trail.at(i - 1).Flip = true;
+						if(i > 0)
+							Trail.at(i).Flip = true;
 					}
-					else
+					else // <-Left
 					{
 						TempTop = Bot;
 						TempBot = Top;
+						if(i > 0)
+							Trail.at(i).Flip = true;
 					}
 				}
-				// float MinGap = length(Normal - PrevNormal) * ScaledWidth;
-				// if(distance(PrevPos, Pos) < MinGap)
-				//	continue;
 
-				if(!WidthInit)
+				// float InverseProgress = 1.0 - Part.Progress;
+				Part.Top = TempTop;
+				Part.Bot = TempBot;
+			}
+			if(LineMode)
+				Graphics()->LinesBegin();
+			else
+				Graphics()->TrianglesBegin();
+
+			int SegmentCount = (int)Trail.size() - 1;
+			// Draw the trail list
+			for(int i = 0; i < SegmentCount; i++)
+			{
+				float InverseProgress = 1.0 - Trail.at(i).Progress;
+				if(g_Config.m_ClTeeTrailFade)
 				{
-					PrevTop = PrevPrevPos + PrevPerpVec * Width;
-					PrevBot = PrevPrevPos - PrevPerpVec * Width;
+					float FadeAlpha = Alpha * InverseProgress;
+					FullCol = Col.WithAlpha(FadeAlpha);
+					Graphics()->SetColor(FullCol);
 				}
-				if(distance(PrevPos, PrevPrevPos) < 600.0f && !LineMode)
+
+				vec2 Pos = Trail.at(i).Pos;
+				vec2 NextPos = Trail.at(i + 1).Pos;
+				Graphics()->SetColor(FullCol);
+				if(LineMode)
 				{
-					Graphics()->SetColor4(FullCol, FullCol, PrevCol, PrevCol);
-					IGraphics::CFreeformItem FreeformItem(Top.x, Top.y, Bot.x, Bot.y, PrevTop.x, PrevTop.y, PrevBot.x, PrevBot.y);
+					Graphics()->SetColor(Trail.at(i).Col);
+					LineItem = IGraphics::CLineItem(Pos.x, Pos.y, NextPos.x, NextPos.y);
+					Graphics()->LinesDraw(&LineItem, 1);
+				}
+				else
+				{
+					vec2 Top = Trail.at(i).Top;
+					vec2 Bot = Trail.at(i).Bot;
+
+					vec2 NextTop = Trail.at(i + 1).Top;
+					vec2 NextBot = Trail.at(i + 1).Bot;
+					if(Trail.at(i).Flip)
+					{
+						Top = Trail.at(i).Bot;
+						Bot = Trail.at(i).Top;
+					}
+
+					ColorRGBA ColNow = Trail.at(i).Col.WithAlpha(Trail.at(i).Alpha);
+					ColorRGBA ColNext = Trail.at(i + 1).Col.WithAlpha(Trail.at(i + 1).Alpha);
+					Graphics()->SetColor4(ColNext, ColNext, ColNow, ColNow);
+					// IGraphics::CFreeformItem FreeformItem(Top.x, Top.y, Bot.x, Bot.y, NextTop.x, NextTop.y, NextBot.x, NextBot.y);
+					IGraphics::CFreeformItem FreeformItem(NextTop.x, NextTop.y, NextBot.x, NextBot.y, Top.x, Top.y, Bot.x, Bot.y);
+
 					Graphics()->QuadsDrawFreeform(&FreeformItem, 1);
 				}
-				PrevTop = TempTop;
-				PrevBot = TempBot;
-				// PrevNormal = Normal;
-				WidthInit = true;
-
-				PrevPerpVec = PerpVec, PerpInit = true;
 			}
-			if(PrevInit)
-				PrevPrevPos = PrevPos, PrevPrevInit = true;
-			if(Init)
-				PrevPos = Pos, PrevInit = true;
-			PrevCol = FullCol;
+			if(LineMode)
+				Graphics()->LinesEnd();
+			else
+				Graphics()->TrianglesEnd();
 		}
-
-		if(!LineMode)
-			Graphics()->TrianglesEnd();
-		else
-			Graphics()->LinesEnd();
-
 		GameClient()->m_aClients[ClientId].m_aPredPos[PredTick % 200] = SavedTempPredPos;
 		m_PositionHistory[ClientId][GameTick % 200] = CurServerPos;
 	}
--- src/game/client/components/tclient/trails.h
+++ src/game/client/components/tclient/trails.h
@@ -2,20 +2,36 @@
 #define GAME_CLIENT_COMPONENTS_TRAILS_H
 #include <game/client/component.h>
 
+struct STrailPart
+{
+	vec2 Pos = vec2(0, 0);
+	ColorRGBA Col = {};
+	float Alpha = 1.0f;
+	float Width = 0.0f;
+	vec2 Normal = vec2(0, 0);
+	vec2 Top = vec2(0, 0);
+	vec2 Bot = vec2(0, 0);
+	bool Flip = false;
+	float Progress = 1.0f;
+
+
+	bool operator==(const STrailPart &Other) const
+	{
+		return Pos == Other.Pos;
+	}
+};
+
 class CTrails : public CComponent
 {
-
 public:
 	CTrails();
 	virtual int Sizeof() const override { return sizeof(*this); }
 	virtual void OnRender() override;
 	virtual void OnReset() override;
 
-
 private:
 	vec2 m_PositionHistory[MAX_CLIENTS][200];
 	int m_PositionTick[MAX_CLIENTS][200];
-
 	bool m_HistoryValid[MAX_CLIENTS] = {};
 
 	void ClearAllHistory();
@@ -25,6 +41,5 @@ private:
 	bool ShouldPredictPlayer(int ClientId);
 	float m_RealIntraTick = 0.0f;
 };
-
 
 #endif
