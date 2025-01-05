732dbced4ba33def5b79ab421017aa4073e47d93
heinrich5991 <heinrich5991@gmail.com>
2025-01-06 00:15:49 +0100

    Refactor `CServer::RedirectClient`
    Get rid of `Verbose` parameter, use `log_info` instead of
    `Console->Log`, etc.

--- src/engine/server/server.h
+++ src/engine/server/server.h
@@ -282,7 +282,7 @@ public:
 
 	void Kick(int ClientId, const char *pReason) override;
 	void Ban(int ClientId, int Seconds, const char *pReason, bool VerbatimReason) override;
-	void RedirectClient(int ClientId, int Port, bool Verbose = false) override;
+	void RedirectClient(int ClientId, int Port) override;
 
 	void DemoRecorder_HandleAutoStart() override;
 
