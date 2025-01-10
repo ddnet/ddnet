ceff31b23600ef432197fe83a3b914706b7bbde1
Louis Geer <louisjgeer@gmail.com>
2025-01-10 13:48:26 +0900

    add 45 snap angle setting

--- src/game/client/components/players.cpp
+++ src/game/client/components/players.cpp
@@ -189,7 +189,7 @@ void CPlayers::RenderHookCollLine(
 	if(Local && !m_pClient->m_Snap.m_SpecInfo.m_Active && Client()->State() != IClient::STATE_DEMOPLAYBACK)
 	{
 		// just use the direct input if it's the local player we are rendering
-		Angle = angle(m_pClient->m_Controls.m_aMousePos[g_Config.m_ClDummy]);
+		Angle = angle(m_pClient->m_Controls.m_aRenderedLocalTeeAngle[g_Config.m_ClDummy]);
 	}
 	else
 	{
@@ -232,8 +232,7 @@ void CPlayers::RenderHookCollLine(
 
 			if(Local && !m_pClient->m_Snap.m_SpecInfo.m_Active && Client()->State() != IClient::STATE_DEMOPLAYBACK)
 			{
-				ExDirection = normalize(vec2((int)m_pClient->m_Controls.m_aMousePos[g_Config.m_ClDummy].x, (int)m_pClient->m_Controls.m_aMousePos[g_Config.m_ClDummy].y));
-
+				ExDirection = normalize(m_pClient->m_Controls.m_aRenderedLocalTeeAngle[g_Config.m_ClDummy]);
 				// fix direction if mouse is exactly in the center
 				if(!(int)m_pClient->m_Controls.m_aMousePos[g_Config.m_ClDummy].x && !(int)m_pClient->m_Controls.m_aMousePos[g_Config.m_ClDummy].y)
 					ExDirection = vec2(1, 0);
@@ -485,7 +484,7 @@ void CPlayers::RenderPlayer(
 	if(Local && !m_pClient->m_Snap.m_SpecInfo.m_Active && Client()->State() != IClient::STATE_DEMOPLAYBACK)
 	{
 		// just use the direct input if it's the local player we are rendering
-		Angle = angle(m_pClient->m_Controls.m_aMousePos[g_Config.m_ClDummy]);
+		Angle = angle(m_pClient->m_Controls.m_aRenderedLocalTeeAngle[g_Config.m_ClDummy]);
 	}
 	else
 	{
