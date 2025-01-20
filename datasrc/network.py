8efab46e90ec084fa6a80349623091f41841e291
ChillerDragon <ChillerDragon@gmail.com>
2025-01-20 19:20:42 +0100

    Allow ammo count -1 over the network to inidcate infinite ammo

--- datasrc/network.py
+++ datasrc/network.py
@@ -198,7 +198,8 @@ Objects = [
 		NetIntRange("m_PlayerFlags", 0, 256),
 		NetIntRange("m_Health", 0, 10),
 		NetIntRange("m_Armor", 0, 10),
-		NetIntRange("m_AmmoCount", 0, 10),
+		# -1 is infinite ammo
+		NetIntRange("m_AmmoCount", -1, 10),
 		NetIntRange("m_Weapon", -1, 'NUM_WEAPONS-1'),
 		NetIntRange("m_Emote", 0, len(Emotes)),
 		NetIntRange("m_AttackTick", 0, 'max_int'),
