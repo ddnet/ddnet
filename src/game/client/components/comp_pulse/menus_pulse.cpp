0cf114c932e2aca21408e8ece79d2009d738b6a0
StormA <stormax0020@gmail.com>
2025-01-20 04:33:10 +0200

    Debug field

--- src/game/client/components/comp_pulse/menus_pulse.cpp
+++ src/game/client/components/comp_pulse/menus_pulse.cpp
@@ -38,7 +38,57 @@
 using namespace FontIcons;
 using namespace std::chrono_literals;
 
+const float FontSize = 14.0f;
+const float Margin = 10.0f;
+const float HeaderHeight = FontSize + 5.0f + Margin;
+
+const float LineSize = 20.0f;
+const float ColorPickerLineSize = 25.0f;
+const float HeadlineFontSize = 20.0f;
+const float HeadlineHeight = 30.0f;
+const float MarginSmall = 5.0f;
+const float MarginBetweenViews = 20.0f;
+
+const float ColorPickerLabelSize = 13.0f;
+const float ColorPickerLineSpacing = 5.0f;
+
 void CMenus::RenderSettingsPulse(CUIRect MainView)
 {
+static CScrollRegion s_ScrollRegion;
+	vec2 ScrollOffset(0.0f, 0.0f);
+	CScrollRegionParams ScrollParams;
+	ScrollParams.m_ScrollUnit = 120.0f;
+	s_ScrollRegion.Begin(&MainView, &ScrollOffset, &ScrollParams);
+	MainView.y += ScrollOffset.y;
 
+
+
+	CUIRect DebugGroup;
+
+	MainView.y -= 10.0f;
+
+	// Начало одной секции
+	static SFoldableSection s_InDebugGroup;
+	MainView.HSplitTop(Margin, nullptr, &DebugGroup);
+	DoFoldableSection(&s_InDebugGroup, Localize("General"), FontSize, &DebugGroup, &MainView, 5.0f, [&]() -> int {
+		DebugGroup.VMargin(Margin, &DebugGroup);
+		DebugGroup.HMargin(Margin, &DebugGroup);
+
+		// Начало любого говнокода в секции
+		char aBuf[64];
+		str_format(aBuf, sizeof(aBuf), "Key is pressed (ALT): %d", Input()->KeyIsPressed(KEY_LALT));
+		Ui()->DoLabel(&DebugGroup, aBuf, FontSize, TEXTALIGN_TL);
+
+		DebugGroup.HSplitTop(LineSize, nullptr, &DebugGroup);
+
+		char aBuf2[64];
+		str_format(aBuf2, sizeof(aBuf2), "Global time: %f", Client()->GlobalTime());
+		Ui()->DoLabel(&DebugGroup, aBuf2, FontSize, TEXTALIGN_TL);
+		// Конец любого говнокода в секции
+
+		int TotalHeight = 40.0f; // Длинна секции вниз
+		return TotalHeight + Margin;
+	});
+	s_ScrollRegion.AddRect(DebugGroup);
+	// Конец одной секции
 }
