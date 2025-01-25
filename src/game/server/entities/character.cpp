61fc3044d1962ae275ae4f23c99b2f5a6caa7219
Pioooooo <pioooooo.sun@gmail.com>
2025-01-25 21:09:55 +0800

    Fix border not killing when overlapped with death tile

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -1430,8 +1430,10 @@ void CCharacter::HandleSkippableTiles(int Index)
 			}
 		}
 		else
+		{
 			Die(m_pPlayer->GetCid(), WEAPON_WORLD);
-		return;
+			return;
+		}
 	}
 
 	if(GameLayerClipped(m_Pos))
