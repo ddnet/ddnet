8ce1353e336cd5edac2b97dcb4d002e39347844f
Paul <paul.turkovskiy@gmail.com>
2025-01-27 20:22:52 +0200

    dnsbl ban duration as variable

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -3168,7 +3168,7 @@ int CServer::Run()
 
 								if(Config()->m_SvDnsblBan)
 								{
-									m_NetServer.NetBan()->BanAddr(ClientAddr(ClientId), 60, Config()->m_SvDnsblBanReason, true);
+									m_NetServer.NetBan()->BanAddr(ClientAddr(ClientId), Config()->m_SvDnsblBanDuration, Config()->m_SvDnsblBanReason, true);
 								}
 							}
 						}
