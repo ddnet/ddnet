05b4170e6783d6bd30d503c5ec3202c5b801b1e7
SollyBunny <sollybunn@gmail.com>
2025-02-23 10:01:55 +0100

    Remove inaccurate copyright notices

--- src/engine/shared/snapshot.cpp
+++ src/engine/shared/snapshot.cpp
@@ -1,5 +1,3 @@
-/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
-/* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #include "snapshot.h"
 #include "compression.h"
 #include "uuid_manager.h"
