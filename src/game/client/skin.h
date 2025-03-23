b75bcb682a301c04b3e5a57d1e79103542cf2a64
Robert Müller <robytemueller@gmail.com>
2025-03-23 12:56:30 +0100

    Move skin function definitions to source file
    Avoid `system.h` include in `skin.h` header file.

--- src/game/client/skin.h
+++ src/game/client/skin.h
@@ -2,13 +2,9 @@
 #define GAME_CLIENT_SKIN_H
 
 #include <base/color.h>
-#include <base/system.h>
-#include <base/vmath.h>
 
 #include <engine/graphics.h>
 #include <engine/shared/protocol.h>
-
-#include <limits>
 
 // do this better and nicer
 class CSkin
@@ -30,105 +26,53 @@ public:
 
 		IGraphics::CTextureHandle m_aEyes[6];
 
-		void Reset()
-		{
-			m_Body = IGraphics::CTextureHandle();
-			m_BodyOutline = IGraphics::CTextureHandle();
-			m_Feet = IGraphics::CTextureHandle();
-			m_FeetOutline = IGraphics::CTextureHandle();
-			m_Hands = IGraphics::CTextureHandle();
-			m_HandsOutline = IGraphics::CTextureHandle();
-			for(auto &Eye : m_aEyes)
-				Eye = IGraphics::CTextureHandle();
-		}
-
-		void Unload(IGraphics *pGraphics)
-		{
-			pGraphics->UnloadTexture(&m_Body);
-			pGraphics->UnloadTexture(&m_BodyOutline);
-			pGraphics->UnloadTexture(&m_Feet);
-			pGraphics->UnloadTexture(&m_FeetOutline);
-			pGraphics->UnloadTexture(&m_Hands);
-			pGraphics->UnloadTexture(&m_HandsOutline);
-			for(auto &Eye : m_aEyes)
-				pGraphics->UnloadTexture(&Eye);
-		}
+		void Reset();
+		void Unload(IGraphics *pGraphics);
 	};
 
 	CSkinTextures m_OriginalSkin;
 	CSkinTextures m_ColorableSkin;
 	ColorRGBA m_BloodColor;
 
-	template<bool IsSizeType>
 	class CSkinMetricVariableInt
 	{
 	public:
 		int m_Value;
-		operator int() const { return m_Value; }
-		CSkinMetricVariableInt &operator=(int NewVal)
-		{
-			if(IsSizeType)
-				m_Value = maximum(m_Value, NewVal);
-			else
-				m_Value = minimum(m_Value, NewVal);
-			return *this;
-		}
 
-		CSkinMetricVariableInt()
-		{
-			Reset();
-		}
+		operator int() const;
+		CSkinMetricVariableInt &operator=(int NewVal);
+		CSkinMetricVariableInt();
+		void Reset();
+	};
 
-		void Reset()
-		{
-			if(IsSizeType)
-				m_Value = std::numeric_limits<int>::lowest();
-			else
-				m_Value = std::numeric_limits<int>::max();
-		}
+	class CSkinMetricVariableSize
+	{
+	public:
+		int m_Value;
+
+		operator int() const;
+		CSkinMetricVariableSize &operator=(int NewVal);
+		CSkinMetricVariableSize();
+		void Reset();
 	};
 
 	class CSkinMetricVariable
 	{
 	public:
-		CSkinMetricVariableInt<true> m_Width;
-		CSkinMetricVariableInt<true> m_Height;
-		CSkinMetricVariableInt<false> m_OffsetX;
-		CSkinMetricVariableInt<false> m_OffsetY;
+		CSkinMetricVariableSize m_Width;
+		CSkinMetricVariableSize m_Height;
+		CSkinMetricVariableInt m_OffsetX;
+		CSkinMetricVariableInt m_OffsetY;
 
 		// these can be used to normalize the metrics
-		CSkinMetricVariableInt<true> m_MaxWidth;
-		CSkinMetricVariableInt<true> m_MaxHeight;
+		CSkinMetricVariableSize m_MaxWidth;
+		CSkinMetricVariableSize m_MaxHeight;
 
-		float WidthNormalized() const
-		{
-			return (float)m_Width / (float)m_MaxWidth;
-		}
-
-		float HeightNormalized() const
-		{
-			return (float)m_Height / (float)m_MaxHeight;
-		}
-
-		float OffsetXNormalized() const
-		{
-			return (float)m_OffsetX / (float)m_MaxWidth;
-		}
-
-		float OffsetYNormalized() const
-		{
-			return (float)m_OffsetY / (float)m_MaxHeight;
-		}
-
-		void Reset()
-		{
-			m_Width.Reset();
-			m_Height.Reset();
-			m_OffsetX.Reset();
-			m_OffsetY.Reset();
-			m_MaxWidth.Reset();
-			m_MaxHeight.Reset();
-		}
+		float WidthNormalized() const;
+		float HeightNormalized() const;
+		float OffsetXNormalized() const;
+		float OffsetYNormalized() const;
+		void Reset();
 	};
 
 	class CSkinMetrics
@@ -137,49 +81,22 @@ public:
 		CSkinMetricVariable m_Body;
 		CSkinMetricVariable m_Feet;
 
-		void Reset()
-		{
-			m_Body.Reset();
-			m_Feet.Reset();
-		}
-
-		CSkinMetrics()
-		{
-			Reset();
-		}
+		CSkinMetrics();
+		void Reset();
 	};
 	CSkinMetrics m_Metrics;
 
-	bool operator<(const CSkin &Other) const { return str_comp(m_aName, Other.m_aName) < 0; }
-	bool operator==(const CSkin &Other) const { return !str_comp(m_aName, Other.m_aName); }
+	bool operator<(const CSkin &Other) const;
+	bool operator==(const CSkin &Other) const;
 
-	CSkin(const char *pName)
-	{
-		str_copy(m_aName, pName);
-	}
+	CSkin(const char *pName);
 	CSkin(CSkin &&) = default;
 	CSkin &operator=(CSkin &&) = default;
 
 	const char *GetName() const { return m_aName; }
 
-	// has to be kept in sync with m_aSkinNameRestrictions
-	static bool IsValidName(const char *pName)
-	{
-		if(pName[0] == '\0' || str_length(pName) >= (int)sizeof(CSkin("").m_aName))
-		{
-			return false;
-		}
-
-		for(int i = 0; pName[i] != '\0'; ++i)
-		{
-			if(pName[i] == '"' || pName[i] == '/' || pName[i] == '\\')
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-	static constexpr char m_aSkinNameRestrictions[] = "Skin names must be valid filenames shorter than 24 characters.";
+	static bool IsValidName(const char *pName);
+	static const char m_aSkinNameRestrictions[];
 };
 
 #endif
