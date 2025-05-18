badf5b250419e98d397a48e96e0b15e44f475eb8
Robert Müller <robytemueller@gmail.com>
2025-05-18 22:06:18 +0200

    Separate `CComponentInterfaces` from `CComponent`
    Add separate `CComponentInterfaces` class which provides convenient getters for all client interfaces, to allow client `CComponent`s to be split into separate sub-components more easily without having to add them to the list of gameclient components.

--- src/game/client/component.cpp
+++ src/game/client/component.cpp
@@ -4,32 +4,110 @@
 
 #include <base/system.h>
 
-class IKernel *CComponent::Kernel() const { return m_pClient->Kernel(); }
-class IEngine *CComponent::Engine() const { return m_pClient->Engine(); }
-class IGraphics *CComponent::Graphics() const { return m_pClient->Graphics(); }
-class ITextRender *CComponent::TextRender() const { return m_pClient->TextRender(); }
-class IInput *CComponent::Input() const { return m_pClient->Input(); }
-class IStorage *CComponent::Storage() const { return m_pClient->Storage(); }
-class CUi *CComponent::Ui() const { return m_pClient->Ui(); }
-class ISound *CComponent::Sound() const { return m_pClient->Sound(); }
-class CRenderTools *CComponent::RenderTools() const { return m_pClient->RenderTools(); }
-class IConfigManager *CComponent::ConfigManager() const { return m_pClient->ConfigManager(); }
-class CConfig *CComponent::Config() const { return m_pClient->Config(); }
-class IConsole *CComponent::Console() const { return m_pClient->Console(); }
-class IDemoPlayer *CComponent::DemoPlayer() const { return m_pClient->DemoPlayer(); }
-class IDemoRecorder *CComponent::DemoRecorder(int Recorder) const { return m_pClient->DemoRecorder(Recorder); }
-class IFavorites *CComponent::Favorites() const { return m_pClient->Favorites(); }
-class IServerBrowser *CComponent::ServerBrowser() const { return m_pClient->ServerBrowser(); }
-class CLayers *CComponent::Layers() const { return m_pClient->Layers(); }
-class CCollision *CComponent::Collision() const { return m_pClient->Collision(); }
+void CComponentInterfaces::OnInterfacesInit(CGameClient *pClient)
+{
+	dbg_assert(m_pClient == nullptr, "Component already initialized");
+	m_pClient = pClient;
+}
+
+class IKernel *CComponentInterfaces::Kernel() const
+{
+	return m_pClient->Kernel();
+}
+
+class IEngine *CComponentInterfaces::Engine() const
+{
+	return m_pClient->Engine();
+}
+
+class IGraphics *CComponentInterfaces::Graphics() const
+{
+	return m_pClient->Graphics();
+}
+
+class ITextRender *CComponentInterfaces::TextRender() const
+{
+	return m_pClient->TextRender();
+}
+
+class IInput *CComponentInterfaces::Input() const
+{
+	return m_pClient->Input();
+}
+
+class IStorage *CComponentInterfaces::Storage() const
+{
+	return m_pClient->Storage();
+}
+
+class CUi *CComponentInterfaces::Ui() const
+{
+	return m_pClient->Ui();
+}
+
+class ISound *CComponentInterfaces::Sound() const
+{
+	return m_pClient->Sound();
+}
+
+class CRenderTools *CComponentInterfaces::RenderTools() const
+{
+	return m_pClient->RenderTools();
+}
+
+class IConfigManager *CComponentInterfaces::ConfigManager() const
+{
+	return m_pClient->ConfigManager();
+}
+
+class CConfig *CComponentInterfaces::Config() const
+{
+	return m_pClient->Config();
+}
+
+class IConsole *CComponentInterfaces::Console() const
+{
+	return m_pClient->Console();
+}
+
+class IDemoPlayer *CComponentInterfaces::DemoPlayer() const
+{
+	return m_pClient->DemoPlayer();
+}
+
+class IDemoRecorder *CComponentInterfaces::DemoRecorder(int Recorder) const
+{
+	return m_pClient->DemoRecorder(Recorder);
+}
+
+class IFavorites *CComponentInterfaces::Favorites() const
+{
+	return m_pClient->Favorites();
+}
+
+class IServerBrowser *CComponentInterfaces::ServerBrowser() const
+{
+	return m_pClient->ServerBrowser();
+}
+
+class CLayers *CComponentInterfaces::Layers() const
+{
+	return m_pClient->Layers();
+}
+
+class CCollision *CComponentInterfaces::Collision() const
+{
+	return m_pClient->Collision();
+}
+
 #if defined(CONF_AUTOUPDATE)
-class IUpdater *CComponent::Updater() const
+class IUpdater *CComponentInterfaces::Updater() const
 {
 	return m_pClient->Updater();
 }
 #endif
 
-int64_t CComponent::time() const
+int64_t CComponentInterfaces::time() const
 {
 #if defined(CONF_VIDEORECORDER)
 	return IVideo::Current() ? IVideo::Time() : time_get();
@@ -38,7 +116,7 @@ int64_t CComponent::time() const
 #endif
 }
 
-float CComponent::LocalTime() const
+float CComponentInterfaces::LocalTime() const
 {
 #if defined(CONF_VIDEORECORDER)
 	return IVideo::Current() ? IVideo::LocalTime() : Client()->LocalTime();
@@ -47,9 +125,12 @@ float CComponent::LocalTime() const
 #endif
 }
 
-class IClient *CComponent::Client() const
+class IClient *CComponentInterfaces::Client() const
 {
 	return m_pClient->Client();
 }
 
-class IHttp *CComponent::Http() const { return m_pClient->Http(); }
+class IHttp *CComponentInterfaces::Http() const
+{
+	return m_pClient->Http();
+}
