68bdd48e0fa9ae450fe4d5c211e1af05301824cc
SollyBunny <sollybunn@gmail.com>
2025-06-02 18:15:43 +0000

    Consistent float notation in effects.cpp

--- src/game/client/components/effects.cpp
+++ src/game/client/components/effects.cpp
@@ -28,13 +28,13 @@ void CEffects::AirJump(vec2 Pos, float Alpha)
 	p.SetDefault();
 	p.m_Spr = SPRITE_PART_AIRJUMP;
 	p.m_Pos = Pos + vec2(-6.0f, 16.0f);
-	p.m_Vel = vec2(0, -200);
+	p.m_Vel = vec2(0.0f, -200.0f);
 	p.m_LifeSpan = 0.5f;
 	p.m_StartSize = 48.0f;
-	p.m_EndSize = 0;
+	p.m_EndSize = 0.0f;
 	p.m_Rot = random_angle();
-	p.m_Rotspeed = pi * 2;
-	p.m_Gravity = 500;
+	p.m_Rotspeed = pi * 2.0f;
+	p.m_Gravity = 500.0f;
 	p.m_Friction = 0.7f;
 	p.m_FlowAffected = 0.0f;
 	p.m_Color.a = Alpha;
@@ -62,13 +62,13 @@ void CEffects::PowerupShine(vec2 Pos, vec2 Size, float Alpha)
 	p.SetDefault();
 	p.m_Spr = SPRITE_PART_SLICE;
 	p.m_Pos = Pos + vec2(random_float(-0.5f, 0.5f), random_float(-0.5f, 0.5f)) * Size;
-	p.m_Vel = vec2(0, 0);
+	p.m_Vel = vec2(0.0f, 0.0f);
 	p.m_LifeSpan = 0.5f;
 	p.m_StartSize = 16.0f;
-	p.m_EndSize = 0;
+	p.m_EndSize = 0.0f;
 	p.m_Rot = random_angle();
-	p.m_Rotspeed = pi * 2;
-	p.m_Gravity = 500;
+	p.m_Rotspeed = pi * 2.0f;
+	p.m_Gravity = 500.0f;
 	p.m_Friction = 0.9f;
 	p.m_FlowAffected = 0.0f;
 	p.m_Color.a = Alpha;
@@ -85,7 +85,7 @@ void CEffects::FreezingFlakes(vec2 Pos, vec2 Size, float Alpha)
 	p.SetDefault();
 	p.m_Spr = SPRITE_PART_SNOWFLAKE;
 	p.m_Pos = Pos + vec2(random_float(-0.5f, 0.5f), random_float(-0.5f, 0.5f)) * Size;
-	p.m_Vel = vec2(0, 0);
+	p.m_Vel = vec2(0.0f, 0.0f);
 	p.m_LifeSpan = 1.5f;
 	p.m_StartSize = random_float(0.5f, 1.5f) * 16.0f;
 	p.m_EndSize = p.m_StartSize * 0.5f;
@@ -112,7 +112,7 @@ void CEffects::SparkleTrail(vec2 Pos, float Alpha)
 	p.SetDefault();
 	p.m_Spr = SPRITE_PART_SPARKLE;
 	p.m_Pos = Pos + random_direction() * random_float(40.0f);
-	p.m_Vel = vec2(0, 0);
+	p.m_Vel = vec2(0.0f, 0.0f);
 	p.m_LifeSpan = 0.5f;
 	p.m_StartSize = 0.0f;
 	p.m_EndSize = random_float(20.0f, 30.0f);
@@ -135,7 +135,7 @@ void CEffects::SmokeTrail(vec2 Pos, vec2 Vel, float Alpha, float TimePassed)
 	p.m_Vel = Vel + random_direction() * 50.0f;
 	p.m_LifeSpan = random_float(0.5f, 1.0f);
 	p.m_StartSize = random_float(12.0f, 20.0f);
-	p.m_EndSize = 0;
+	p.m_EndSize = 0.0f;
 	p.m_Friction = 0.7f;
 	p.m_Gravity = random_float(-500.0f);
 	p.m_Color.a = Alpha;
@@ -154,7 +154,7 @@ void CEffects::SkidTrail(vec2 Pos, vec2 Vel, int Direction, float Alpha)
 		p.m_Vel = vec2(-Direction * 100.0f * length(Vel), -50.0f) + random_direction() * 50.0f;
 		p.m_LifeSpan = random_float(0.5f, 1.0f);
 		p.m_StartSize = random_float(24.0f, 36.0f);
-		p.m_EndSize = 0;
+		p.m_EndSize = 0.0f;
 		p.m_Friction = 0.7f;
 		p.m_Gravity = random_float(-500.0f);
 		p.m_Color = ColorRGBA(0.75f, 0.75f, 0.75f, Alpha);
@@ -183,7 +183,7 @@ void CEffects::BulletTrail(vec2 Pos, float Alpha, float TimePassed)
 	p.m_Pos = Pos;
 	p.m_LifeSpan = random_float(0.25f, 0.5f);
 	p.m_StartSize = 8.0f;
-	p.m_EndSize = 0;
+	p.m_EndSize = 0.0f;
 	p.m_Friction = 0.7f;
 	p.m_Color.a *= Alpha;
 	p.m_StartAlpha = Alpha;
@@ -201,7 +201,7 @@ void CEffects::PlayerSpawn(vec2 Pos, float Alpha)
 		p.m_Vel = random_direction() * (std::pow(random_float(), 3) * 600.0f);
 		p.m_LifeSpan = random_float(0.3f, 0.6f);
 		p.m_StartSize = random_float(64.0f, 96.0f);
-		p.m_EndSize = 0;
+		p.m_EndSize = 0.0f;
 		p.m_Rot = random_angle();
 		p.m_Rotspeed = random_float();
 		p.m_Gravity = random_float(-400.0f);
@@ -259,7 +259,7 @@ void CEffects::PlayerDeath(vec2 Pos, int ClientId, float Alpha)
 		p.m_Vel = random_direction() * (random_float(0.1f, 1.1f) * 900.0f);
 		p.m_LifeSpan = random_float(0.3f, 0.6f);
 		p.m_StartSize = random_float(24.0f, 40.0f);
-		p.m_EndSize = 0;
+		p.m_EndSize = 0.0f;
 		p.m_Rot = random_angle();
 		p.m_Rotspeed = random_float(-0.5f, 0.5f) * pi;
 		p.m_Gravity = 800.0f;
@@ -292,7 +292,7 @@ void CEffects::Confetti(vec2 Pos, float Alpha)
 		p.m_Vel = direction(-0.5f * pi + random_float(-0.2f, 0.2f)) * random_float(0.01f, 1.0f) * 2000.0f;
 		p.m_LifeSpan = random_float(1.0f, 1.2f);
 		p.m_StartSize = random_float(12.0f, 24.0f);
-		p.m_EndSize = 0;
+		p.m_EndSize = 0.0f;
 		p.m_Rot = random_angle();
 		p.m_Rotspeed = random_float(-0.5f, 0.5f) * pi;
 		p.m_Gravity = -700.0f;
@@ -313,7 +313,7 @@ void CEffects::Confetti(vec2 Pos, float Alpha)
 		p.m_Vel = direction(-0.5f * pi + random_float(-0.8f, 0.8f)) * random_float(0.01f, 1.0f) * 1500.0f;
 		p.m_LifeSpan = random_float(0.8f, 1.0f);
 		p.m_StartSize = random_float(12.0f, 24.0f);
-		p.m_EndSize = 0;
+		p.m_EndSize = 0.0f;
 		p.m_Rot = random_angle();
 		p.m_Rotspeed = random_float(-0.5f, 0.5f) * pi;
 		p.m_Gravity = -700.0f;
@@ -334,8 +334,8 @@ void CEffects::Explosion(vec2 Pos, float Alpha)
 			if(x == 0 && y == 0)
 				continue;
 
-			float a = 1 - (length(vec2(x, y)) / length(vec2(8, 8)));
-			m_pClient->m_Flow.Add(Pos + vec2(x, y) * 16, normalize(vec2(x, y)) * 5000.0f * a, 10.0f);
+			float a = 1 - (length(vec2(x, y)) / length(vec2(8.0f, 8.0f)));
+			m_pClient->m_Flow.Add(Pos + vec2(x, y) * 16.0f, normalize(vec2(x, y)) * 5000.0f * a, 10.0f);
 		}
 
 	// add the explosion
@@ -345,7 +345,7 @@ void CEffects::Explosion(vec2 Pos, float Alpha)
 	p.m_Pos = Pos;
 	p.m_LifeSpan = 0.4f;
 	p.m_StartSize = 150.0f;
-	p.m_EndSize = 0;
+	p.m_EndSize = 0.0f;
 	p.m_Rot = random_angle();
 	p.m_Color.a = Alpha;
 	p.m_StartAlpha = Alpha;
@@ -358,8 +358,8 @@ void CEffects::Explosion(vec2 Pos, float Alpha)
 		const vec2 DistanceToTopLeft = Pos - vec2(round_truncate(Pos.x / 32), round_truncate(Pos.y / 32)) * 32;
 
 		vec2 CheckOffset;
-		CheckOffset.x = (DistanceToTopLeft.x > 16 ? 32 : -1);
-		CheckOffset.y = (DistanceToTopLeft.y > 16 ? 32 : -1);
+		CheckOffset.x = (DistanceToTopLeft.x > 16.0f ? 32.0f : -1.0f);
+		CheckOffset.y = (DistanceToTopLeft.y > 16.0f ? 32.0f : -1.0f);
 		CheckOffset -= DistanceToTopLeft;
 
 		for(vec2 Mask : {vec2(1.0f, 0.0f), vec2(0.0f, 1.0f), vec2(1.0f, 1.0f)})
@@ -382,7 +382,7 @@ void CEffects::Explosion(vec2 Pos, float Alpha)
 		p.m_Vel = random_direction() * (random_float(1.0f, 1.2f) * 1000.0f);
 		p.m_LifeSpan = random_float(0.5f, 0.9f);
 		p.m_StartSize = random_float(32.0f, 40.0f);
-		p.m_EndSize = 0;
+		p.m_EndSize = 0.0f;
 		p.m_Gravity = random_float(-800.0f);
 		p.m_Friction = 0.4f;
 		p.m_Color = mix(vec4(0.75f, 0.75f, 0.75f, 1.0f), vec4(0.5f, 0.5f, 0.5f, 1.0f), random_float());
@@ -401,7 +401,7 @@ void CEffects::HammerHit(vec2 Pos, float Alpha)
 	p.m_Pos = Pos;
 	p.m_LifeSpan = 0.3f;
 	p.m_StartSize = 120.0f;
-	p.m_EndSize = 0;
+	p.m_EndSize = 0.0f;
 	p.m_Rot = random_angle();
 	p.m_Color.a = Alpha;
 	p.m_StartAlpha = Alpha;
