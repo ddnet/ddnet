203428dee4cddf78825e06b8470c1d664f68b729
K1nop1c0 <f1234567891991@outlook.com>
2025-06-16 16:40:05 +0800

    Touch controls editor
    Supports editing touch controls ingame. General features:
    1. Edit everything that can be changed by editing touch_controls.json
    2. Move & zoom buttons on screen directly
    3. Automatically avoid selected button being overlapped
    4. Preview button layout under different visibilities
    5. Browse & search for buttons by label, command or position
    
    Other edits:
    Use `m_JoystickCount` to support multi joysticks on screen so `swap-action` can act correctly

--- src/game/client/components/menus.h
+++ src/game/client/components/menus.h
@@ -20,6 +20,7 @@
 
 #include <game/client/component.h>
 #include <game/client/components/mapimages.h>
+#include <game/client/components/menus_ingame_touch_controls.h>
 #include <game/client/lineinput.h>
 #include <game/client/render.h>
 #include <game/client/ui.h>
@@ -499,6 +500,12 @@ protected:
 	void PopupConfirmDiscardTouchControlsChanges();
 	void PopupConfirmResetTouchControls();
 	void PopupConfirmImportTouchControlsClipboard();
+	void PopupConfirmDeleteButton();
+	void PopupCancelDeselectButton();
+	void PopupConfirmSelectedNotVisible();
+	void PopupConfirmChangeSelectedButton();
+	void PopupCancelChangeSelectedButton();
+	void PopupConfirmTurnOffEditor();
 	void RenderPlayers(CUIRect MainView);
 	void RenderServerInfo(CUIRect MainView);
 	void RenderServerInfoMotd(CUIRect Motd);
@@ -792,6 +799,8 @@ public:
 private:
 	CCommunityIcons m_CommunityIcons;
 	CMenusStart m_MenusStart;
+	CMenusIngameTouchControls m_MenusIngameTouchControls;
+	friend CMenusIngameTouchControls;
 
 	static int GhostlistFetchCallback(const CFsFileInfo *pInfo, int IsDir, int StorageType, void *pUser);
 
