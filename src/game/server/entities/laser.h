1e797728b36ce8182861e389159c8f1a56aab44a
ChillerDragon <ChillerDragon@gmail.com>
2025-06-21 12:32:38 +0200

    Add interact state to fix invisible lasers
    Closed #10417, #9229

--- src/game/server/entities/laser.h
+++ src/game/server/entities/laser.h
@@ -4,6 +4,7 @@
 #define GAME_SERVER_ENTITIES_LASER_H
 
 #include <game/server/entity.h>
+#include <game/server/interact_state.h>
 
 class CLaser : public CEntity
 {
@@ -31,8 +32,8 @@ private:
 	int m_Bounces;
 	int m_EvalTick;
 	int m_Owner;
-	CClientMask m_TeamMask;
 	bool m_ZeroEnergyBounceInLastTick;
+	CInteractState m_InteractState;
 
 	// DDRace
 
@@ -42,6 +43,7 @@ private:
 	bool m_TeleportCancelled;
 	bool m_IsBlueTeleport;
 	bool m_BelongsToPracticeTeam;
+	void SyncInteractState();
 };
 
 #endif
