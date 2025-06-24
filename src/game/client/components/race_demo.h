73a31f91828b4efd9e995d7af692d123289f97bd
SollyBunny <sollybunn@gmail.com>
2025-06-24 07:51:38 +0000

    Fix modernize-use-override

--- src/game/client/components/race_demo.h
+++ src/game/client/components/race_demo.h
@@ -39,13 +39,13 @@ public:
 	bool m_AllowRestart;
 
 	CRaceDemo();
-	virtual int Sizeof() const override { return sizeof(*this); }
+	int Sizeof() const override { return sizeof(*this); }
 
-	virtual void OnReset() override;
-	virtual void OnStateChange(int NewState, int OldState) override;
-	virtual void OnMessage(int MsgType, void *pRawMsg) override;
-	virtual void OnMapLoad() override;
-	virtual void OnShutdown() override;
-	virtual void OnNewSnapshot() override;
+	void OnReset() override;
+	void OnStateChange(int NewState, int OldState) override;
+	void OnMessage(int MsgType, void *pRawMsg) override;
+	void OnMapLoad() override;
+	void OnShutdown() override;
+	void OnNewSnapshot() override;
 };
 #endif
