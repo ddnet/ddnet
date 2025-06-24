fd59ff61c345082ae9bfcb8544eeb932ee2a9cb3
KebsCS <git@kebs.dev>
2025-06-24 18:20:37 +0200

    Add toast notifications for windows

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -4585,7 +4585,7 @@ int main(int argc, const char **argv)
 	}
 	gs_AndroidStarted = true;
 #elif defined(CONF_FAMILY_WINDOWS)
-	CWindowsComLifecycle WindowsComLifecycle(true);
+	CWindowsComRtLifecycle WindowsComRtLifecycle(true);
 #endif
 	CCmdlineFix CmdlineFix(&argc, &argv);
 
@@ -4798,7 +4798,7 @@ int main(int argc, const char **argv)
 	pEngine->Init();
 	pConsole->Init();
 	pConfigManager->Init();
-	pNotifications->Init(GAME_NAME " Client");
+	pNotifications->Init(GAME_NAME);
 
 	// register all console commands
 	pClient->RegisterCommands();
@@ -5212,6 +5212,8 @@ void CClient::ShellRegister()
 		return;
 	}
 
+	windows_create_shortcut(GAME_NAME, aFullPath);
+
 	bool Updated = false;
 	if(!shell_register_protocol("ddnet", aFullPath, &Updated))
 		log_error("client", "Failed to register ddnet protocol");
@@ -5234,6 +5236,8 @@ void CClient::ShellUnregister()
 		log_error("client", "Failed to unregister protocol and file extensions: could not determine absolute path");
 		return;
 	}
+
+	windows_remove_shortcut(GAME_NAME);
 
 	bool Updated = false;
 	if(!shell_unregister_class("ddnet", &Updated))
