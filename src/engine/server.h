c470b763cdafe4ba9b1a6e63b35f9531ecd92e93
ChillerDragon <ChillerDragon@gmail.com>
2025-07-16 20:12:45 +0200

    Add ``CGameServer::OnSetTimedOut`` in preperation for 128 slots
    This will come in handy for the planned 128 player support and reduces
    the diff of the currently pending pr #9274
    
    This commit alone without 128 player support is a bit weird and empty.
    But it is also a rather complicated change. The gameserver,
    gamecontroller and antibot get the client drop with a different state
    now. This is why reviewing and merging this independently has its
    advantages. Reviewing and testing this independently reduces the risk.
    And makes reasoning about the 128 player pr easier and merging it safer.

--- src/engine/server.h
+++ src/engine/server.h
@@ -356,6 +356,7 @@ public:
 	virtual void OnPreTickTeehistorian() = 0;
 
 	virtual void OnSetAuthed(int ClientId, int Level) = 0;
+	virtual void OnSetTimedOut(int ClientId) = 0;
 	virtual bool PlayerExists(int ClientId) const = 0;
 
 	virtual void TeehistorianRecordAntibot(const void *pData, int DataSize) = 0;
