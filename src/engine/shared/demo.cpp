a61e3280ae0c4590e1e20bf520c702ea4753569e
SollyBunny <sollybunn@gmail.com>
2025-08-02 17:39:35 +0000

    Don't seek back to a keyframe if seeking forward into the same keyframe

--- src/engine/shared/demo.cpp
+++ src/engine/shared/demo.cpp
@@ -1001,6 +1001,19 @@ bool CDemoPlayer::SetPos(int WantedTick)
 	if(!m_File)
 		return false;
 
+	// Do nothing
+	if(WantedTick == m_Info.m_NextTick)
+		return true;
+
+	// Just the next tick
+	if(WantedTick == m_Info.m_NextTick + 1)
+	{
+		// This does handle looping correctly
+		DoTick();
+		Play();
+		return true;
+	}
+
 	WantedTick = std::clamp(WantedTick, m_Info.m_Info.m_FirstTick, m_Info.m_Info.m_LastTick);
 	const int KeyFrameWantedTick = WantedTick - 5; // -5 because we have to have a current tick and previous tick when we do the playback
 	const float Percent = (KeyFrameWantedTick - m_Info.m_Info.m_FirstTick) / (float)(m_Info.m_Info.m_LastTick - m_Info.m_Info.m_FirstTick);
@@ -1012,16 +1025,19 @@ bool CDemoPlayer::SetPos(int WantedTick)
 	while(KeyFrame > 0 && m_vKeyFrames[KeyFrame].m_Tick > KeyFrameWantedTick)
 		KeyFrame--;
 
-	// seek to the correct key frame
-	if(io_seek(m_File, m_vKeyFrames[KeyFrame].m_Filepos, IOSEEK_START) != 0)
+	// If seeking backwards or the current tick is not in this keyframe, seek to the keyframe
+	if(WantedTick < m_Info.m_Info.m_CurrentTick || m_Info.m_Info.m_CurrentTick < m_vKeyFrames[KeyFrame].m_Tick)
 	{
-		Stop("Error seeking keyframe position");
-		return false;
+		// seek to the correct key frame
+		if(io_seek(m_File, m_vKeyFrames[KeyFrame].m_Filepos, IOSEEK_START) != 0)
+		{
+			Stop("Error seeking keyframe position");
+			return false;
+		}
+		m_Info.m_NextTick = -1;
+		m_Info.m_Info.m_CurrentTick = -1;
+		m_Info.m_PreviousTick = -1;
 	}
-
-	m_Info.m_NextTick = -1;
-	m_Info.m_Info.m_CurrentTick = -1;
-	m_Info.m_PreviousTick = -1;
 
 	// playback everything until we hit our tick
 	while(m_Info.m_NextTick < WantedTick && IsPlaying())
