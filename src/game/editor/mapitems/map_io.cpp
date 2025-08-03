6683512b31e532c31be673a5db49ca5fc68aeb66
Robert Müller <robytemueller@gmail.com>
2025-08-03 23:57:28 +0200

    Fix editor crashes on maps with invalid tile layer images

--- src/game/editor/mapitems/map_io.cpp
+++ src/game/editor/mapitems/map_io.cpp
@@ -756,6 +756,12 @@ bool CEditorMap::Load(const char *pFileName, int StorageType, const std::functio
 					pTiles->m_Image = pTilemapItem->m_Image;
 					pTiles->m_HasGame = pTilemapItem->m_Flags & TILESLAYERFLAG_GAME;
 
+					// validate image index
+					if(pTiles->m_Image < -1 || pTiles->m_Image >= (int)m_vpImages.size())
+					{
+						pTiles->m_Image = -1;
+					}
+
 					// load layer name
 					if(pTilemapItem->m_Version >= 3)
 						IntsToStr(pTilemapItem->m_aName, std::size(pTilemapItem->m_aName), pTiles->m_aName, std::size(pTiles->m_aName));
@@ -870,8 +876,12 @@ bool CEditorMap::Load(const char *pFileName, int StorageType, const std::functio
 					std::shared_ptr<CLayerQuads> pQuads = std::make_shared<CLayerQuads>(m_pEditor);
 					pQuads->m_Flags = pLayerItem->m_Flags;
 					pQuads->m_Image = pQuadsItem->m_Image;
+
+					// validate image index
 					if(pQuads->m_Image < -1 || pQuads->m_Image >= (int)m_vpImages.size())
+					{
 						pQuads->m_Image = -1;
+					}
 
 					// load layer name
 					if(pQuadsItem->m_Version >= 2)
@@ -897,9 +907,11 @@ bool CEditorMap::Load(const char *pFileName, int StorageType, const std::functio
 					pSounds->m_Flags = pLayerItem->m_Flags;
 					pSounds->m_Sound = pSoundsItem->m_Sound;
 
-					// validate m_Sound
+					// validate sound index
 					if(pSounds->m_Sound < -1 || pSounds->m_Sound >= (int)m_vpSounds.size())
+					{
 						pSounds->m_Sound = -1;
+					}
 
 					// load layer name
 					IntsToStr(pSoundsItem->m_aName, std::size(pSoundsItem->m_aName), pSounds->m_aName, std::size(pSounds->m_aName));
@@ -926,9 +938,11 @@ bool CEditorMap::Load(const char *pFileName, int StorageType, const std::functio
 					pSounds->m_Flags = pLayerItem->m_Flags;
 					pSounds->m_Sound = pSoundsItem->m_Sound;
 
-					// validate m_Sound
+					// validate sound index
 					if(pSounds->m_Sound < -1 || pSounds->m_Sound >= (int)m_vpSounds.size())
+					{
 						pSounds->m_Sound = -1;
+					}
 
 					// load layer name
 					IntsToStr(pSoundsItem->m_aName, std::size(pSoundsItem->m_aName), pSounds->m_aName, std::size(pSounds->m_aName));
