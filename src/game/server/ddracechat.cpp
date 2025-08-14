3fa793307d21d0e2a2f54384bf2312f22d7ff604
Pioooooo <pioooooo.sun@gmail.com>
2025-08-14 17:25:10 +0800

    Add rcon command set_switch, practice command setswitch

--- src/game/server/ddracechat.cpp
+++ src/game/server/ddracechat.cpp
@@ -2280,6 +2280,13 @@ void CGameContext::ConPracticeUnEndlessHook(IConsole::IResult *pResult, void *pU
 		ConUnEndlessHook(pResult, pUserData);
 }
 
+void CGameContext::ConPracticeSetSwitch(IConsole::IResult *pResult, void *pUserData)
+{
+	CGameContext *pSelf = (CGameContext *)pUserData;
+	if(pSelf->GetPracticeCharacter(pResult))
+		ConSetSwitch(pResult, pUserData);
+}
+
 void CGameContext::ConPracticeToggleInvincible(IConsole::IResult *pResult, void *pUserData)
 {
 	CGameContext *pSelf = (CGameContext *)pUserData;
