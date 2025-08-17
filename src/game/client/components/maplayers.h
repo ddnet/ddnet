357adfc5edd273f214c98eb88f62d0e96de8460b
Marvin Winkens <m.winkens@fz-juelich.de>
2025-08-17 22:37:57 +0200

    Add a map renderer

--- src/game/client/components/maplayers.h
+++ src/game/client/components/maplayers.h
@@ -4,21 +4,15 @@
 #define GAME_CLIENT_COMPONENTS_MAPLAYERS_H
 
 #include <game/client/component.h>
-#include <game/map/render_layer.h>
+#include <game/map/map_renderer.h>
 
 #include <cstdint>
 #include <memory>
-#include <vector>
 
 class CCamera;
 class CLayers;
 class CMapImages;
 class ColorRGBA;
-
-class CMapItemGroup;
-class CMapItemLayer;
-class CMapItemLayerTilemap;
-class CMapItemLayerQuads;
 
 class CMapLayers : public CComponent, public IEnvelopeEval
 {
@@ -29,14 +23,14 @@ class CMapLayers : public CComponent, public IEnvelopeEval
 	CMapImages *m_pImages;
 	std::shared_ptr<CMapBasedEnvelopePointAccess> m_pEnvelopePoints;
 
-	int m_Type;
+	ERenderType m_Type;
 	bool m_OnlineOnly;
 
 public:
 	static void EnvelopeEval(int TimeOffsetMillis, int Env, ColorRGBA &Result, size_t Channels, IMap *pMap, CMapBasedEnvelopePointAccess *pEnvelopePoints, IClient *pClient, CGameClient *pGameClient, bool OnlineOnly);
 	void EnvelopeEval(int TimeOffsetMillis, int Env, ColorRGBA &Result, size_t Channels) override;
 
-	CMapLayers(int Type, bool OnlineOnly = true);
+	CMapLayers(ERenderType Type, bool OnlineOnly = true);
 	int Sizeof() const override { return sizeof(*this); }
 	void OnInit() override;
 	void OnRender() override;
@@ -46,9 +40,8 @@ public:
 	virtual CCamera *GetCurCamera();
 
 private:
-	std::vector<std::unique_ptr<CRenderLayer>> m_vpRenderLayers;
-	int GetLayerType(const CMapItemLayer *pLayer) const;
 	CRenderLayerParams m_Params;
+	CMapRenderer m_MapRenderer;
 };
 
 #endif
