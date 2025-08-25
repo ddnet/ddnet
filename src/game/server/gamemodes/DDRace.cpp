959da5d445f14fd912ee445a89be386f8200cb9a
qxdFox <jusi.p@icloud.com>
2025-08-25 06:15:44 +0200

    Add FoxNet Version and send that to chat on join

--- src/game/server/gamemodes/DDRace.cpp
+++ src/game/server/gamemodes/DDRace.cpp
@@ -137,8 +137,7 @@ void CGameControllerDDRace::OnPlayerConnect(CPlayer *pPlayer)
 		str_format(aBuf, sizeof(aBuf), "'%s' entered and joined the %s", Server()->ClientName(ClientId), GetTeamName(pPlayer->GetTeam()));
 		GameServer()->SendChat(-1, TEAM_ALL, aBuf, -1, CGameContext::FLAG_SIX);
 
-		GameServer()->SendChatTarget(ClientId, "DDraceNetwork Mod. Version: " GAME_VERSION);
-		GameServer()->SendChatTarget(ClientId, "please visit DDNet.org or say /info and make sure to read our /rules");
+		GameServer()->SendChatTarget(ClientId, "FoxNetwork Mod " GAME_VERSION);
 	}
 }
 
