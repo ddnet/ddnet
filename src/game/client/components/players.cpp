0e030887ee4cd2d8a0358f43c9b638f6828a4f11
SollyBunny <sollybunn@gmail.com>
2025-09-03 18:35:32 +0000

    Add falling woosh sfx

--- src/game/client/components/players.cpp
+++ src/game/client/components/players.cpp
@@ -518,6 +518,44 @@ void CPlayers::RenderPlayer(
 	if(!InAir && WantOtherDir && length(Vel * 50) > 500.0f)
 		GameClient()->m_Effects.SkidTrail(Position, Vel, Player.m_Direction, Alpha);
 
+
+	// Update woosh sound effect
+	// TODO: vfx?
+	for(auto &Client : GameClient()->m_aClients)
+	{
+		const auto &Char = GameClient()->m_Snap.m_aCharacters[Client.ClientId()];
+		const bool IsPlaying = Client.m_SfxFallingWoosh.IsValid();
+		if(!Char.m_Active && IsPlaying)
+		{
+			Sound()->StopVoice(Client.m_SfxFallingWoosh);
+			Client.m_SfxFallingWoosh = ISound::CVoiceHandle();
+			continue;
+		}
+
+		static constexpr float FALLING_WOOSH_SPEED = 14.0f;
+		const float MaxVel = std::max(std::abs(Vel.x), std::abs(Vel.y));
+		const float Volume = MaxVel / (FALLING_WOOSH_SPEED * 2.0f);
+
+		const bool ShouldPlaying = MaxVel >= FALLING_WOOSH_SPEED;
+		if(ShouldPlaying)
+		{
+			if(IsPlaying)
+			{
+				Sound()->SetVoicePosition(Client.m_SfxFallingWoosh, Client.m_RenderPos);
+				Sound()->SetVoiceVolume(Client.m_SfxFallingWoosh, Volume);
+			}
+			else
+			{
+				Client.m_SfxFallingWoosh = Sound()->PlayAt(CSounds::CHN_WORLD, GameClient()->m_Sounds.GetSampleId(SOUND_FALLING_WOOSH), ISound::FLAG_LOOP, Volume, Client.m_RenderPos);
+			}
+		}
+		else
+		{
+			Sound()->StopVoice(Client.m_SfxFallingWoosh);
+			Client.m_SfxFallingWoosh = ISound::CVoiceHandle();
+		}
+	}
+
 	// draw gun
 	if(Player.m_Weapon >= 0)
 	{
