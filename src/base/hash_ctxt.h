068644716bcbd3e400ab1e47a1959d13647e4b75
Robert Müller <robytemueller@gmail.com>
2025-10-06 20:27:00 +0200

    Ignore `readability-redundant-declaration` when using external MD5
    ```
    src/base/hash_ctxt.h:32:6: warning: redundant 'md5_init' declaration [readability-redundant-declaration]
       32 | void md5_init(MD5_CTX *ctxt);
          |      ^
    src/engine/external/md5/md5.h:79:6: note: previously declared here
       79 | void md5_init(md5_state_t *pms);
          |      ^
    src/engine/external/md5/md5.h:79:6: warning: function 'md5_init' has 1 other declaration with different parameter names [readability-inconsistent-declaration-parameter-name]
       79 | void md5_init(md5_state_t *pms);
          |      ^
    src/base/hash_ctxt.h:32:6: note: the 1st inconsistent declaration seen here
       32 | void md5_init(MD5_CTX *ctxt);
          |      ^
    ```

--- src/base/hash_ctxt.h
+++ src/base/hash_ctxt.h
@@ -29,7 +29,7 @@ void sha256_init(SHA256_CTX *ctxt);
 void sha256_update(SHA256_CTX *ctxt, const void *data, size_t data_len);
 SHA256_DIGEST sha256_finish(SHA256_CTX *ctxt);
 
-void md5_init(MD5_CTX *ctxt);
+void md5_init(MD5_CTX *ctxt); // NOLINT(readability-redundant-declaration, readability-inconsistent-declaration-parameter-name)
 void md5_update(MD5_CTX *ctxt, const void *data, size_t data_len);
 MD5_DIGEST md5_finish(MD5_CTX *ctxt);
 
