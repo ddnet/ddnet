wxj881027/TaterClient-ddnet-Qimeng

Make nameplate client id a tag

src/game/client/components/nameplates.cpp
@@ -300,7 +300,7 @@ private:
 protected:
 	bool UpdateNeeded(CGameClient &This, const CNamePlateData &Data) override
 	{
-		m_Visible = Data.m_ShowClientId && (Data.m_ClientIdSeperateLine == m_ClientIdSeperateLine);
+		m_Visible = Data.m_ShowClientId && Data.m_ClientIdSeperateLine == m_ClientIdSeperateLine;
 		if(!m_Visible)
 			return false;
 		m_Color = Data.m_Color;
@@ -310,10 +310,7 @@ protected:
 	{
 		m_FontSize = Data.m_FontSizeClientId;
 		m_ClientId = Data.m_ClientId;
-		if(m_ClientIdSeperateLine)
-			str_format(m_aText, sizeof(m_aText), "%d", m_ClientId);
-		else
-			str_format(m_aText, sizeof(m_aText), "%d:", m_ClientId);
+		str_format(m_aText, sizeof(m_aText), "%d", m_ClientId);
 		CTextCursor Cursor;
 		This.TextRender()->SetCursor(&Cursor, 0.0f, 0.0f, m_FontSize, TEXTFLAG_RENDER);
 		This.TextRender()->CreateOrAppendTextContainer(m_TextContainerIndex, &Cursor, m_aText);
@@ -324,6 +321,7 @@ public:
 		CNamePlatePartText(This)
 	{
 		m_ClientIdSeperateLine = ClientIdSeperateLine;
+		m_IsTag = !ClientIdSeperateLine;
 	}
 };
 

ddnet/ddnet

src/game/client/components/nameplates.cpp
@@ -379,7 +379,7 @@ private:
 protected:
 	bool UpdateNeeded(CGameClient &This, const CNamePlateData &Data) override
 	{
-		m_Visible = Data.m_ShowClientId && (Data.m_ClientIdSeparateLine == m_ClientIdSeparateLine);
+		m_Visible = Data.m_ShowClientId && Data.m_ClientIdSeparateLine == m_ClientIdSeparateLine;
 		if(!m_Visible)
 			return false;
 		m_Color = Data.m_Color;
@@ -389,10 +389,7 @@ protected:
 	{
 		m_FontSize = Data.m_FontSizeClientId;
 		m_ClientId = Data.m_ClientId;
-		if(m_ClientIdSeparateLine)
-			str_format(m_aText, sizeof(m_aText), "%d", m_ClientId);
-		else
-			str_format(m_aText, sizeof(m_aText), "%d:", m_ClientId);
+		str_format(m_aText, sizeof(m_aText), "%d", m_ClientId);
 		CTextCursor Cursor;
 		Cursor.m_FontSize = m_FontSize;
 		This.TextRender()->CreateOrAppendTextContainer(m_TextContainerIndex, &Cursor, m_aText);
@@ -403,6 +400,7 @@ public:
 		CNamePlatePartText(This)
 	{
 		m_ClientIdSeparateLine = ClientIdSeparateLine;
+		m_IsTag = !ClientIdSeparateLine;
 	}
 };
 