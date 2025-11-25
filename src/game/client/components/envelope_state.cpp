aed6660c5257a7f225d338ed496053b1b5fc73b4
Marvin Winkens <m.winkens@fz-juelich.de>
2025-11-25 11:24:20 +0100

    refactor CEnvelopeState class
    Move static variables into class members
    use namespace std::chrono more often

--- src/game/client/components/envelope_state.cpp
+++ src/game/client/components/envelope_state.cpp
@@ -35,38 +35,32 @@ void CEnvelopeState::EnvelopeEval(int TimeOffsetMillis, int Env, ColorRGBA &Resu
 	if(m_pEnvelopePoints->NumPoints() == 0)
 		return;
 
-	// online rendering
-	if(m_OnlineOnly)
-	{
-		// we are doing time integration for smoother animations
-		static nanoseconds s_OnlineTime{0};
-		static const nanoseconds s_NanosPerTick = nanoseconds(1s) / static_cast<int64_t>(Client()->GameTickSpeed());
+	nanoseconds Time;
 
+	// offline rendering (like menu background) relies on local time
+	if(!m_OnlineOnly)
+	{
+		Time = time_get_nanoseconds();
+	}
+	else
+	{
+		// online rendering
 		if(GameClient()->m_Snap.m_pGameInfoObj)
 		{
+			static const nanoseconds s_NanosPerTick = nanoseconds(1s) / static_cast<int64_t>(Client()->GameTickSpeed());
+
 			// get the lerp of the current tick and prev
 			const int MinTick = Client()->PrevGameTick(g_Config.m_ClDummy) - GameClient()->m_Snap.m_pGameInfoObj->m_RoundStartTick;
 			const int CurTick = Client()->GameTick(g_Config.m_ClDummy) - GameClient()->m_Snap.m_pGameInfoObj->m_RoundStartTick;
-			s_OnlineTime = std::chrono::nanoseconds((int64_t)(mix<double>(
-										  0,
-										  (CurTick - MinTick),
-										  (double)Client()->IntraGameTick(g_Config.m_ClDummy)) *
-									  s_NanosPerTick.count())) +
-				       MinTick * s_NanosPerTick;
+
+			double TickRatio = mix<double>(0, CurTick - MinTick, (double)Client()->IntraGameTick(g_Config.m_ClDummy));
+			Time = duration_cast<nanoseconds>(TickRatio * s_NanosPerTick) + MinTick * s_NanosPerTick;
 		}
-		CRenderMap::RenderEvalEnvelope(m_pEnvelopePoints.get(), s_OnlineTime + milliseconds(TimeOffsetMillis), Result, Channels);
+		else
+		{
+			Time = nanoseconds::zero();
+		}
 	}
-	else // offline rendering (like menu background) relies on local time
-	{
-		// integrate over time deltas for smoother animations
-		static auto s_LastLocalTime = time_get_nanoseconds();
-		nanoseconds CurTime = time_get_nanoseconds();
-		static nanoseconds s_Time{0};
-		s_Time += CurTime - s_LastLocalTime;
 
-		CRenderMap::RenderEvalEnvelope(m_pEnvelopePoints.get(), s_Time + milliseconds(TimeOffsetMillis), Result, Channels);
-
-		// update local timer
-		s_LastLocalTime = CurTime;
-	}
+	CRenderMap::RenderEvalEnvelope(m_pEnvelopePoints.get(), Time + milliseconds(TimeOffsetMillis), Result, Channels);
 }
