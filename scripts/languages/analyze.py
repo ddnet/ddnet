6fe5225a83af853ab41409a0e77f4fc547ab976d
byfox <byfox213@gmail.com>
2025-12-13 12:33:16 +0500

    refactor(scripts/languages)

--- scripts/languages/analyze.py
+++ scripts/languages/analyze.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python3
+# TODO: Please remove this when we switch completely to ruff.
+# pylint: disable=bad-indentation
 import os
 import sys
+from pathlib import Path
+
 import twlang
 
-os.chdir(os.path.dirname(__file__) + "/../..")
+os.chdir(Path(__file__).resolve().parent.parent.parent)
 
-if len(sys.argv) > 1:
-	langs = sys.argv[1:]
-else:
-	langs = twlang.languages()
+langs = sys.argv[1:] if len(sys.argv) > 1 else twlang.languages()
 local = twlang.localizes()
 table = []
 for lang in langs:
-	trans = twlang.translations(lang)
-	empty = 0
-	supported = 0
-	unused = 0
-	for tran, (_, expr, _) in trans.items():
-		if not expr:
-			empty += 1
-		else:
-			if tran in local:
-				supported += 1
-			else:
-				unused += 1
-	table.append([lang, len(trans), empty, len(local)-supported, unused])
+    trans = twlang.translations(lang)
+    empty = 0
+    supported = 0
+    unused = 0
+    for tran, (_, expr, _) in trans.items():
+        if not expr:
+            empty += 1
+        elif tran in local:
+            supported += 1
+        else:
+            unused += 1
+    table.append([lang, len(trans), empty, len(local) - supported, unused])
 
-table.sort(key=lambda l: l[3])
-table = [["filename", "total", "empty", "missing", "unused"]] + table
-s = [[str(e) for e in row] for row in table]
-lens = [max(map(len, col)) for col in zip(*s)]
+table.sort(key=lambda row: row[3])
+new_table = [["filename", "total", "empty", "missing", "unused"], *table]
+s = [[str(e) for e in row] for row in new_table]
+lens = [max(map(len, col)) for col in zip(*s, strict=True)]
 fmt = "    ".join(f"{{:{x}}}" for x in lens)
 t = [fmt.format(*row) for row in s]
 print("\n".join(t))
--- scripts/languages/twlang.py
+++ scripts/languages/twlang.py
@@ -1,93 +1,103 @@
+# TODO: Please remove this when we switch completely to ruff.
+# pylint: disable=bad-indentation
 import functools
 import os
 import re
 from collections import OrderedDict
+from pathlib import Path
+from typing import IO, Any
+
+TranslationKey = tuple[str, str]
+TranslationData = list[Any]
+DecodedData = dict[TranslationKey, TranslationData]
+
 
 class LanguageDecodeError(Exception):
-	def __init__(self, message, filename, line):
-		error = f"File \"{filename}\", line {line+1}: {message}"
-		super().__init__(error)
-
-def decode(fileobj, elements_per_key, allow_context=True):
-	data = {}
-	current_context = ""
-	current_key = None
-	index = -1
-	for index, line in enumerate(fileobj):
-		line = line.encode("utf-8").decode("utf-8-sig").rstrip("\r\n")
-		if not line or line[:1] == "#":
-			current_context = ""
-			continue
-
-		if line[0] == "[":
-			if line[-1] != "]":
-				raise LanguageDecodeError("Invalid context string", fileobj.name, index)
-			if not allow_context:
-				raise LanguageDecodeError("Context not allowed in this file", fileobj.name, index)
-			current_context = line[1:-1]
-		elif line[:3] == "== ":
-			if len(data[current_key]) >= 1+elements_per_key:
-				raise LanguageDecodeError("Wrong number of elements per key", fileobj.name, index)
-			if current_key:
-				translation = line[3:]
-				data[current_key].extend([translation])
-			else:
-				raise LanguageDecodeError("Element before key given", fileobj.name, index)
-		else:
-			if current_key:
-				if len(data[current_key]) != 1+elements_per_key:
-					raise LanguageDecodeError("Wrong number of elements per key", fileobj.name, index)
-				data[current_key].append(index - 1 if current_context else index)
-			if line in data:
-				raise LanguageDecodeError("Key defined multiple times: " + line, fileobj.name, index)
-			data[(line, current_context)] = [index - 1 if current_context else index]
-			current_key = (line, current_context)
-	if len(data[current_key]) != 1+elements_per_key:
-		raise LanguageDecodeError("Wrong number of elements per key", fileobj.name, index)
-	data[current_key].append(index+1)
-	new_data = {}
-	for key, value in data.items():
-		if key[0]:
-			new_data[key] = value
-	return new_data
+    def __init__(self, message: str, filename: str, line: int) -> None:
+        error = f'File "{filename}", line {line + 1}: {message}'
+        super().__init__(error)
 
 
-def check_file(path):
-	with open(path, encoding="utf-8") as fileobj:
-		matches = re.findall(r"(Localize|Localizable)\s*\(\s*\"((?:(?:\\\")|[^\"])+)\"(?:\s*,\s*\"((?:(?:\\\")|[^\"])+)\")?\s*\)", fileobj.read())
-	return matches
+def decode(fileobj: IO[str], elements_per_key: int, *, allow_context: bool = True) -> DecodedData:  # noqa: PLR0912, C901
+    data = {}
+    current_context = ""
+    current_key = None
+    index = -1
+    for index, raw_line in enumerate(fileobj):
+        line = raw_line.encode("utf-8").decode("utf-8-sig").rstrip("\r\n")
+        if not line or line[:1] == "#":
+            current_context = ""
+            continue
+
+        if line[0] == "[":
+            if line[-1] != "]":
+                raise LanguageDecodeError("Invalid context string", fileobj.name, index)
+            if not allow_context:
+                raise LanguageDecodeError("Context not allowed in this file", fileobj.name, index)
+            current_context = line[1:-1]
+        elif line[:3] == "== ":
+            if len(data[current_key]) >= 1 + elements_per_key:
+                raise LanguageDecodeError("Wrong number of elements per key", fileobj.name, index)
+            if current_key:
+                translation = line[3:]
+                data[current_key].extend([translation])
+            else:
+                raise LanguageDecodeError("Element before key given", fileobj.name, index)
+        else:
+            if current_key:
+                if len(data[current_key]) != 1 + elements_per_key:
+                    raise LanguageDecodeError("Wrong number of elements per key", fileobj.name, index)
+                data[current_key].append(index - 1 if current_context else index)
+            if line in data:
+                raise LanguageDecodeError("Key defined multiple times: " + line, fileobj.name, index)
+            data[(line, current_context)] = [index - 1 if current_context else index]
+            current_key = (line, current_context)
+    if len(data[current_key]) != 1 + elements_per_key:
+        raise LanguageDecodeError("Wrong number of elements per key", fileobj.name, index)
+    data[current_key].append(index + 1)
+    return {key: value for key, value in data.items() if key[0]}
+
+
+def check_file(path: Path) -> list[tuple]:
+    with path.open(encoding="utf-8") as file:
+        return re.findall(
+            r"(Localize|Localizable)\s*\(\s*\"((?:(?:\\\")|[^\"])+)\"(?:\s*,\s*\"((?:(?:\\\")|[^\"])+)\")?\s*\)",
+            file.read(),
+        )
 
 
 @functools.lru_cache(None)
-def check_folder(path):
-	englishlist = OrderedDict()
-	for path2, dirs, files in os.walk(path):
-		dirs.sort()
-		for f in sorted(files):
-			if not any(f.endswith(x) for x in [".cpp", ".c", ".h"]):
-				continue
-			for sentence in check_file(os.path.join(path2, f)):
-				key = (sentence[1:][0].replace("\\\"", "\""), sentence[1:][1].replace("\\\"", "\""))
-				englishlist[key] = None
-	return englishlist
+def check_folder(path: str) -> OrderedDict[TranslationKey, None]:
+    englishlist: OrderedDict[TranslationKey, None] = OrderedDict()
+
+    for path2, dirs, files in os.walk(path):
+        dirs.sort()
+        for f in sorted(files):
+            if not any(f.endswith(x) for x in [".cpp", ".c", ".h"]):
+                continue
+            for sentence in check_file(Path(path2) / f):
+                key = (
+                    sentence[1:][0].replace('\\"', '"'),
+                    sentence[1:][1].replace('\\"', '"'),
+                )
+                englishlist[key] = None
+    return englishlist
 
 
-def language_index():
-	with open("data/languages/index.txt", encoding="utf-8") as f:
-		return {key: value for (key, _), value in decode(f, 3, allow_context=False).items()}
+def language_index() -> dict[str, TranslationData]:
+    with Path("data/languages/index.txt").open(encoding="utf-8") as f:
+        return {key: value for (key, _), value in decode(f, 3, allow_context=False).items()}
 
 
-def languages():
-	index = language_index()
-	langs = {"data/languages/"+key+".txt": [key]+elements for key, elements in index.items()}
-	return langs
+def languages() -> dict[str, list[Any]]:
+    index = language_index()
+    return {"data/languages/" + key + ".txt": [key, *elements] for key, elements in index.items()}
 
 
-def translations(filename):
-	with open(filename, encoding="utf-8") as f:
-		return decode(f, 1)
+def translations(filename: str) -> DecodedData:
+    with Path(filename).open(encoding="utf-8") as f:
+        return decode(f, 1)
 
 
-def localizes():
-	englishlist = list(check_folder("src"))
-	return englishlist
+def localizes() -> list[TranslationKey]:
+    return list(check_folder("src"))
