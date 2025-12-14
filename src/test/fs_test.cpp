5ed62f07dc0b068f2c4cf24620608155da9d2c33
CherryEx <57710259+CherryEx@users.noreply.github.com>
2025-12-14 19:42:01 +0330

    Rework FindBinaryDirectory for accurate executable location
    Adds fs_executable_path and prioritizes it over argv[0] inside FindBinaryDirectory

--- src/test/fs_test.cpp
+++ src/test/fs_test.cpp
@@ -91,6 +91,15 @@ TEST(Filesystem, StoragePath)
 	EXPECT_TRUE(str_endswith_nocase(aStoragePath, "/TestAppName"));
 }
 
+TEST(Filesystem, ExecutablePath)
+{
+	char aExecutablePath[IO_MAX_PATH_LENGTH];
+	ASSERT_FALSE(fs_executable_path(aExecutablePath, sizeof(aExecutablePath)));
+	EXPECT_TRUE(fs_is_file(aExecutablePath));
+	fs_parent_dir(aExecutablePath);
+	EXPECT_FALSE(fs_is_relative_path(aExecutablePath));
+}
+
 TEST(Filesystem, CreateCloseDelete)
 {
 	CTestInfo Info;
