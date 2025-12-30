24b7d1f2d83fbfb18b69450fbe296769c9bf86af
Robert Müller <robytemueller@gmail.com>
2025-12-30 15:56:59 +0100

    Fix headless Emscripten client not yielding control to browser
    This is normally handled by SDL, but in the headless Emscripten client we need to call `emscripten_sleep` manually on graphics swap-commands to return control to the browser's main thread periodically.

--- src/engine/client/backend/null/backend_null.cpp
+++ src/engine/client/backend/null/backend_null.cpp
@@ -2,6 +2,10 @@
 
 #include <engine/client/backend_sdl.h>
 
+#if defined(CONF_PLATFORM_EMSCRIPTEN)
+#include <emscripten/emscripten.h>
+#endif
+
 ERunCommandReturnTypes CCommandProcessorFragment_Null::RunCommand(const CCommandBuffer::SCommand *pBaseCommand)
 {
 	switch(pBaseCommand->m_Cmd)
@@ -17,6 +21,9 @@ ERunCommandReturnTypes CCommandProcessorFragment_Null::RunCommand(const CCommand
 		break;
 	case CCommandBuffer::CMD_TEXT_TEXTURE_UPDATE:
 		Cmd_TextTexture_Update(static_cast<const CCommandBuffer::SCommand_TextTexture_Update *>(pBaseCommand));
+		break;
+	case CCommandBuffer::CMD_SWAP:
+		Cmd_Swap(static_cast<const CCommandBuffer::SCommand_Swap *>(pBaseCommand));
 		break;
 	}
 	return ERunCommandReturnTypes::RUN_COMMAND_COMMAND_HANDLED;
@@ -58,4 +65,13 @@ void CCommandProcessorFragment_Null::Cmd_TextTextures_Create(const CCommandBuffe
 void CCommandProcessorFragment_Null::Cmd_TextTexture_Update(const CCommandBuffer::SCommand_TextTexture_Update *pCommand)
 {
 	free(pCommand->m_pData);
+}
+
+void CCommandProcessorFragment_Null::Cmd_Swap(const CCommandBuffer::SCommand_Swap *pCommand)
+{
+#if defined(CONF_PLATFORM_EMSCRIPTEN)
+	// Return control to the browser's main thread. This is normally done in SDL_GL_SwapWindow,
+	// but with headless graphics we do not have a GL context to call this function.
+	emscripten_sleep(0);
+#endif
 }
--- src/engine/client/backend/null/backend_null.h
+++ src/engine/client/backend/null/backend_null.h
@@ -8,9 +8,10 @@ class CCommandProcessorFragment_Null : public CCommandProcessorFragment_GLBase
 	bool GetPresentedImageData(uint32_t &Width, uint32_t &Height, CImageInfo::EImageFormat &Format, std::vector<uint8_t> &vDstData) override { return false; }
 	ERunCommandReturnTypes RunCommand(const CCommandBuffer::SCommand *pBaseCommand) override;
 	bool Cmd_Init(const SCommand_Init *pCommand);
-	virtual void Cmd_Texture_Create(const CCommandBuffer::SCommand_Texture_Create *pCommand);
-	virtual void Cmd_TextTextures_Create(const CCommandBuffer::SCommand_TextTextures_Create *pCommand);
-	virtual void Cmd_TextTexture_Update(const CCommandBuffer::SCommand_TextTexture_Update *pCommand);
+	void Cmd_Texture_Create(const CCommandBuffer::SCommand_Texture_Create *pCommand);
+	void Cmd_TextTextures_Create(const CCommandBuffer::SCommand_TextTextures_Create *pCommand);
+	void Cmd_TextTexture_Update(const CCommandBuffer::SCommand_TextTexture_Update *pCommand);
+	void Cmd_Swap(const CCommandBuffer::SCommand_Swap *pCommand);
 };
 
 #endif
