31f8efe0f308b54b7289a3f599de61caa878cf2f
Dennis Felsing <dennis@felsing.org>
2026-01-10 18:38:58 +0100

    Revert "Fix macOS client always changing current working directory"
    This reverts commit 00f16103db8d30e81834cfbaed537fc1d50d5638.

--- src/macos/client.mm
+++ src/macos/client.mm
@@ -5,16 +5,18 @@ extern "C" int TWMain(int argc, const char **argv);
 int main(int argc, const char **argv)
 {
 	BOOL FinderLaunch = argc >= 2 && !strncmp(argv[1], "-psn", 4);
+
+	NSString *pResourcePath = [[NSBundle mainBundle] resourcePath];
+	if(!pResourcePath)
+		return -1;
+
+	[[NSFileManager defaultManager] changeCurrentDirectoryPath:pResourcePath];
+
 	if(FinderLaunch)
 	{
-		NSString *pResourcePath = [[NSBundle mainBundle] resourcePath];
-		if(!pResourcePath)
-			return -1;
-
-		[[NSFileManager defaultManager] changeCurrentDirectoryPath:pResourcePath];
-
 		const char *apArgv[2] = { argv[0], NULL };
 		return TWMain(1, apArgv);
 	}
-	return TWMain(argc, argv);
+	else
+		return TWMain(argc, argv);
 }
