ed4910db3158c6ebb2e38940d56e857faa355097
heinrich5991 <heinrich5991@gmail.com>
2026-03-09 12:33:29 +0100

    Downgrade cxxbridge command to 1.0.71
    Pin `cxx` dependency. It needs to be kept in sync with our generated
    files in src/rust-bridge.

--- src/rust-bridge/engine/shared/rust_version.cpp
+++ src/rust-bridge/engine/shared/rust_version.cpp
@@ -2,12 +2,12 @@
 #include "engine/console.h"
 
 extern "C" {
-void cxxbridge1$RustVersionPrint(::IConsole const &console) noexcept;
+void cxxbridge1$RustVersionPrint(const ::IConsole &console) noexcept;
 
 void cxxbridge1$RustVersionRegister(::IConsole &console) noexcept;
 } // extern "C"
 
-void RustVersionPrint(::IConsole const &console) noexcept {
+void RustVersionPrint(const ::IConsole &console) noexcept {
   cxxbridge1$RustVersionPrint(console);
 }
 
