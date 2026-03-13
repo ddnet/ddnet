113037ca7402da92cd5fa189eacfe5a47fec4f97
Robert Müller <robytemueller@gmail.com>
2026-03-13 20:51:05 +0100

    Remove unused `CMenus::OnReset` function
    The `CMenus::OnReset` function is empty which is identical to the default `CComponent::OnReset` implementation.

--- src/game/client/components/menus.h
+++ src/game/client/components/menus.h
@@ -645,7 +645,6 @@ public:
 
 	void OnStateChange(int NewState, int OldState) override;
 	void OnWindowResize() override;
-	void OnReset() override;
 	void OnRender() override;
 	bool OnInput(const IInput::CEvent &Event) override;
 	bool OnCursorMove(float x, float y, IInput::ECursorType CursorType) override;
