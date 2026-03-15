f3d71a3ad949c3de836093edbea66262bff1196f
Marvin Winkens <m.winkens@fz-juelich.de>
2026-03-15 13:08:41 +0100

    optimize particle rendering

--- src/game/collision.cpp
+++ src/game/collision.cpp
@@ -471,8 +471,8 @@ void CCollision::MovePoint(vec2 *pInoutPos, vec2 *pInoutVel, float Elasticity, i
 	if(pBounces)
 		*pBounces = 0;
 
-	vec2 Pos = *pInoutPos;
-	vec2 Vel = *pInoutVel;
+	const vec2 &Pos = *pInoutPos;
+	const vec2 &Vel = *pInoutVel;
 	if(CheckPoint(Pos + Vel))
 	{
 		int Affected = 0;
@@ -500,7 +500,7 @@ void CCollision::MovePoint(vec2 *pInoutPos, vec2 *pInoutVel, float Elasticity, i
 	}
 	else
 	{
-		*pInoutPos = Pos + Vel;
+		*pInoutPos += Vel;
 	}
 }
 
