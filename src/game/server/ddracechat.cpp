80af8498a69959b5fbdd3686e16bac94d2442a74
swarfeya <135431730+swarfeya@users.noreply.github.com>
2026-04-09 19:54:56 +0200

    fix clang-tidy style

--- src/game/server/ddracechat.cpp
+++ src/game/server/ddracechat.cpp
@@ -1158,7 +1158,7 @@ void CGameContext::AttemptJoinTeam(int ClientId, int Team)
 		if(!m_pController->Teams().IsAllowLeaderCommands(pPlayer->GetCid(), Team))
 		{
 			str_copy(aBuf, "", sizeof(aBuf));
-			int leaders = 0;
+			int Leaders = 0;
 			for(int i = 0; i < MAX_CLIENTS; i++)
 			{
 				if(m_pController->Teams().IsAllowLeaderCommands(i, Team))
@@ -1166,10 +1166,10 @@ void CGameContext::AttemptJoinTeam(int ClientId, int Team)
 					if(str_comp(aBuf, "") != 0)
 						str_append(aBuf, ", ");
 					str_append(aBuf, Server()->ClientName(i));
-					leaders++;
+					Leaders++;
 				}
 			}
-			if(leaders > 1)
+			if(Leaders > 1)
 				SendChatTarget(pPlayer->GetCid(), "Team leader mode is enabled for your team. These are your team leaders:");
 			else
 				SendChatTarget(pPlayer->GetCid(), "Team leader mode is enabled for your team. This is your team leader:");
