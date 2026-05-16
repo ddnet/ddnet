ddnet/ddnet

commit dda83d9627199727b61c53b0f3ec9806a2fdab82

Also fix the ordering for AuthInit

src/engine/server/server.cpp
@@ -3185,13 +3185,7 @@ int CServer::Run()
 					m_ServerInfoFirstRequest = 0;
 					Kernel()->ReregisterInterface(GameServer());
 					Console()->StoreCommands(true);
-					GameServer()->OnInit(m_pPersistentData);
-					Console()->StoreCommands(false);
-					if(ErrorShutdown())
-					{
-						break;
-					}
-					UpdateServerInfo(true);
+
 					for(int ClientId = 0; ClientId < MAX_CLIENTS; ClientId++)
 					{
 						if(m_aClients[ClientId].m_State < CClient::STATE_PREAUTH)
@@ -3202,6 +3196,14 @@ int CServer::Run()
 						// Record PlayerJoin events here to record the Sixup version and player join event.
 						GameServer()->TeehistorianRecordPlayerJoin(ClientId, m_aClients[ClientId].m_Sixup);
 					}
+
+					GameServer()->OnInit(m_pPersistentData);
+					Console()->StoreCommands(false);
+					if(ErrorShutdown())
+					{
+						break;
+					}
+					UpdateServerInfo(true);
 				}
 				else
 				{

commit 368ab203b361a07de86061ccb94ad50eb5e30b82

Fix Join events not written to Teehistorian file on map change
Currently the Teehistorian file is initialized after the Join/Auth
events are recorded. The Join/Auth chunks are therefore not written to
the new Teehistorian file. They also don't end up in the old one,
because it is already closed.

Regressed by commit dda83d9627199727b61c53b0f3ec9806a2fdab82
Related issue: https://gitlab.com/ddnet-rs/twgame/-/issues/23

src/engine/server/server.cpp
@@ -3252,6 +3252,8 @@ int CServer::Run()
 					m_ServerInfoFirstRequest = 0;
 					Kernel()->ReregisterInterface(GameServer());
 					Console()->StoreCommands(true);
+					GameServer()->OnInit(m_pPersistentData);
+					Console()->StoreCommands(false);
 
 					for(int ClientId = 0; ClientId < MAX_CLIENTS; ClientId++)
 					{
@@ -3272,8 +3274,6 @@ int CServer::Run()
 						}
 					}
 
-					GameServer()->OnInit(m_pPersistentData);
-					Console()->StoreCommands(false);
 					if(ErrorShutdown())
 					{
 						break;

src/game/server/teehistorian.cpp
@@ -27,7 +27,7 @@ private:
 static const char TEEHISTORIAN_NAME[] = "teehistorian@ddnet.tw";
 static const CUuid TEEHISTORIAN_UUID = CalculateUuid(TEEHISTORIAN_NAME);
 static const char TEEHISTORIAN_VERSION[] = "2";
-static const char TEEHISTORIAN_VERSION_MINOR[] = "21";
+static const char TEEHISTORIAN_VERSION_MINOR[] = "22";
 
 #define UUID(id, name) static const CUuid UUID_##id = CalculateUuid(name);
 #include <engine/shared/teehistorian_ex_chunks.h>

src/test/teehistorian_test.cpp
@@ -110,7 +110,7 @@ protected:
 	void Expect(const unsigned char *pOutput, size_t OutputSize)
 	{
 		static CUuid TEEHISTORIAN_UUID = CalculateUuid("teehistorian@ddnet.tw");
-		static const char PREFIX1[] = "{\"comment\":\"teehistorian@ddnet.tw\",\"version\":\"2\",\"version_minor\":\"21\",\"game_uuid\":\"a1eb7182-796e-3b3e-941d-38ca71b2a4a8\",\"server_version\":\"DDNet test\",\"start_time\":\"";
+		static const char PREFIX1[] = "{\"comment\":\"teehistorian@ddnet.tw\",\"version\":\"2\",\"version_minor\":\"22\",\"game_uuid\":\"a1eb7182-796e-3b3e-941d-38ca71b2a4a8\",\"server_version\":\"DDNet test\",\"start_time\":\"";
 		static const char PREFIX2[] = "\",\"server_name\":\"server name\",\"server_port\":\"8303\",\"game_type\":\"game type\",\"map_name\":\"Kobra 3 Solo\",\"map_size\":\"903514\",\"map_sha256\":\"0123456789012345678901234567890123456789012345678901234567890123\",\"map_crc\":\"eceaf25c\",\"prng_description\":\"test-prng:02468ace\",\"config\":{},\"tuning\":{},\"uuids\":[";
 		static const char PREFIX3[] = "]}";
 