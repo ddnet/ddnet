4b54ae7e898b36cf22299bed00387159d709c52a
ChillerDragon <ChillerDragon@gmail.com>
2026-06-28 16:53:04 +0200

    draft

--- src/engine/shared/ssh_server.h
+++ src/engine/shared/ssh_server.h
@@ -4,11 +4,24 @@
 #include <engine/shared/config.h>
 #include <engine/shared/network.h>
 
+#include <libssh/libssh.h>
+#include <libssh/server.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PORT "2222"
+#define HOSTKEY_FILE "ssh_host_rsa_key"
+#define USERNAME "demo"
+#define PASSWORD "secret"
+
 class CSshServer
 {
 	CConfig *m_pConfig;
 	IConsole *m_pConsole;
 	CNetConsole m_NetConsole;
+
+	ssh_bind m_Bind;
 
 public:
 	IConsole *Console() { return m_pConsole; }
