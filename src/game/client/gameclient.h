08be317ef50d944bd515b7396a7c4ccafb1e728f
heinrich5991 <heinrich5991@gmail.com>
2026-07-02 14:35:03 +0200

    Document `CGameClient::m_Predicted{,Prev}Char`

--- src/game/client/gameclient.h
+++ src/game/client/gameclient.h
@@ -332,8 +332,15 @@ public:
 
 	vec2 m_LocalCharacterPos;
 
-	// predicted players
+	/**
+	 * Our prediction for the local character at tick
+	 * `IClient::PredGameTick() - 1`.
+	 */
 	CCharacterCore m_PredictedPrevChar;
+	/**
+	 * Our prediction for the local character at tick
+	 * `IClient::PredGameTick()`.
+	 */
 	CCharacterCore m_PredictedChar;
 
 	// snap pointers
