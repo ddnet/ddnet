df9c34944c09de422159221118456e62b03ca251
ChillerDragon <chillerdragon@gmail.com>
2026-07-16 13:21:55 +0200

    Looks kinda good

--- src/engine/shared/ssh_server.cpp
+++ src/engine/shared/ssh_server.cpp
@@ -538,6 +538,8 @@ void CSshClient::AddSingleUtf8CodePointToInput(const char *pUtf8, size_t Utf8Siz
 
 void CSshClient::InsertToInputAtCursor(const char *pText)
 {
+	// TODO: bound check if we exceed any width or size
+
 	ssh_channel Channel = m_Channel;
 	bool CursorAtEnd = m_aInput[m_InputIdx] == '\0';
 	const int TextSize = str_length(pText);
@@ -554,27 +556,21 @@ void CSshClient::InsertToInputAtCursor(const char *pText)
 		// if we are in the middle of the string
 		// we need to shift all the input
 
-		// clear everything from the cursor till the end
-		ssh_channel_write(Channel, "\33[0K", str_length("\33[0K"));
-		ssh_channel_write(Channel, pText, TextSize);
-
 		char aRight[2048];
 		// TODO: this for sure can go OOB pls add some checks
 		str_copy(aRight, m_aInput + m_InputIdx);
 
+		// clear everything from the cursor till the end
+		ssh_channel_write(Channel, "\33[0K", str_length("\33[0K"));
+		ssh_channel_write(Channel, pText, TextSize);
 		ssh_channel_write(Channel, aRight, str_length(aRight));
 
-		str_append(m_aInput, pText);
-		m_InputIdx += TextSize;
 		m_aInput[m_InputIdx] = '\0';
+		str_append(m_aInput, pText);
 		str_append(m_aInput, aRight);
 
+		m_InputIdx += TextSize;
 		m_CursorPos.x += TextWidth;
-
-		// FIXME: i am currently editing this branch
-		//        i messed a bit with the null term
-		//        and the input used to be duplicated on paste
-		//        reason about this a bit
 	}
 }
 
