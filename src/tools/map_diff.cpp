86e20d44f799ea1883e7bacd3012489c82e7f0a7
Dennis Felsing <dennis@felsing.org>
2026-07-21 14:40:38 +0200

    map-diff: Try doing it correctly for physics layers

--- src/tools/map_diff.cpp
+++ src/tools/map_diff.cpp
@@ -10,6 +10,31 @@
 #include <game/gamecore.h>
 #include <game/mapitems.h>
 
+template<typename T, typename FCompare>
+static bool DiffTileLayer(CDataFileReader aMaps[2], const char **pMapNames, const int aData[2], int Width, int Height, FCompare pfnCompare)
+{
+	const T *apTile[2];
+	for(int i = 0; i < 2; ++i)
+	{
+		apTile[i] = (const T *)aMaps[i].GetData(aData[i]);
+		if(apTile[i] == nullptr || (size_t)aMaps[i].GetDataSize(aData[i]) < (size_t)Width * Height * sizeof(T))
+		{
+			dbg_msg("map_diff", "invalid tile layer data in \"%s\"", pMapNames[i]);
+			return false;
+		}
+	}
+
+	for(int y = 0; y < Height; y++)
+	{
+		for(int x = 0; x < Width; x++)
+		{
+			const int Pos = y * Width + x;
+			pfnCompare(apTile[0][Pos], apTile[1][Pos], x, y);
+		}
+	}
+	return true;
+}
+
 static bool Process(IStorage *pStorage, const char **pMapNames)
 {
 	CDataFileReader aMaps[2];
@@ -73,35 +98,65 @@ static bool Process(IStorage *pStorage, const char **pMapNames)
 			IntsToStr(apTilemap[i]->m_aName, std::size(apTilemap[i]->m_aName), aaName[i], std::size(aaName[i]));
 		}
 
-		if(str_comp(aaName[0], aaName[1]) != 0 || apTilemap[0]->m_Width != apTilemap[1]->m_Width || apTilemap[0]->m_Height != apTilemap[1]->m_Height)
+		const int PhysicsLayerFlags = TILESLAYERFLAG_TELE | TILESLAYERFLAG_SPEEDUP | TILESLAYERFLAG_FRONT | TILESLAYERFLAG_SWITCH | TILESLAYERFLAG_TUNE;
+		if(str_comp(aaName[0], aaName[1]) != 0 || apTilemap[0]->m_Width != apTilemap[1]->m_Width || apTilemap[0]->m_Height != apTilemap[1]->m_Height || (apTilemap[0]->m_Flags & PhysicsLayerFlags) != (apTilemap[1]->m_Flags & PhysicsLayerFlags))
 		{
 			dbg_msg("map_diff", "different tile layers:");
 			for(int i = 0; i < 2; ++i)
-				dbg_msg("map_diff", "  \"%s\" (%dx%d)", aaName[i], apTilemap[i]->m_Width, apTilemap[i]->m_Height);
+				dbg_msg("map_diff", "  \"%s\" (%dx%d, flags: %d)", aaName[i], apTilemap[i]->m_Width, apTilemap[i]->m_Height, apTilemap[i]->m_Flags);
 			return false;
 		}
-		CTile *apTile[2];
-		for(int i = 0; i < 2; ++i)
+
+		const int Width = apTilemap[0]->m_Width;
+		const int Height = apTilemap[0]->m_Height;
+		const int Flags = apTilemap[0]->m_Flags & PhysicsLayerFlags;
+		bool Ok;
+		if(Flags & TILESLAYERFLAG_TELE)
 		{
-			apTile[i] = (CTile *)aMaps[i].GetData(apTilemap[i]->m_Data);
-			if(apTile[i] == nullptr || (size_t)aMaps[i].GetDataSize(apTilemap[i]->m_Data) < (size_t)apTilemap[i]->m_Width * apTilemap[i]->m_Height * sizeof(CTile))
-			{
-				dbg_msg("map_diff", "invalid tile layer data in \"%s\"", pMapNames[i]);
-				return false;
-			}
+			const int aData[2] = {apTilemap[0]->m_Tele, apTilemap[1]->m_Tele};
+			Ok = DiffTileLayer<CTeleTile>(aMaps, pMapNames, aData, Width, Height, [&](const CTeleTile &Tile0, const CTeleTile &Tile1, int x, int y) {
+				if(Tile0.m_Number != Tile1.m_Number || Tile0.m_Type != Tile1.m_Type)
+					dbg_msg("map_diff", "[%d:%s] %dx%d: (number: %d, type: %d) != (number: %d, type: %d)", aLayersNum[0], aaName[0], x, y, Tile0.m_Number, Tile0.m_Type, Tile1.m_Number, Tile1.m_Type);
+			});
+		}
+		else if(Flags & TILESLAYERFLAG_SPEEDUP)
+		{
+			const int aData[2] = {apTilemap[0]->m_Speedup, apTilemap[1]->m_Speedup};
+			Ok = DiffTileLayer<CSpeedupTile>(aMaps, pMapNames, aData, Width, Height, [&](const CSpeedupTile &Tile0, const CSpeedupTile &Tile1, int x, int y) {
+				if(Tile0.m_Force != Tile1.m_Force || Tile0.m_MaxSpeed != Tile1.m_MaxSpeed || Tile0.m_Type != Tile1.m_Type || Tile0.m_Angle != Tile1.m_Angle)
+					dbg_msg("map_diff", "[%d:%s] %dx%d: (force: %d, maxspeed: %d, angle: %d, type: %d) != (force: %d, maxspeed: %d, angle: %d, type: %d)", aLayersNum[0], aaName[0], x, y, Tile0.m_Force, Tile0.m_MaxSpeed, Tile0.m_Angle, Tile0.m_Type, Tile1.m_Force, Tile1.m_MaxSpeed, Tile1.m_Angle, Tile1.m_Type);
+			});
+		}
+		else if(Flags & TILESLAYERFLAG_SWITCH)
+		{
+			const int aData[2] = {apTilemap[0]->m_Switch, apTilemap[1]->m_Switch};
+			Ok = DiffTileLayer<CSwitchTile>(aMaps, pMapNames, aData, Width, Height, [&](const CSwitchTile &Tile0, const CSwitchTile &Tile1, int x, int y) {
+				if(Tile0.m_Number != Tile1.m_Number || Tile0.m_Type != Tile1.m_Type || Tile0.m_Flags != Tile1.m_Flags || Tile0.m_Delay != Tile1.m_Delay)
+					dbg_msg("map_diff", "[%d:%s] %dx%d: (number: %d, type: %d, flags: %d, delay: %d) != (number: %d, type: %d, flags: %d, delay: %d)", aLayersNum[0], aaName[0], x, y, Tile0.m_Number, Tile0.m_Type, Tile0.m_Flags, Tile0.m_Delay, Tile1.m_Number, Tile1.m_Type, Tile1.m_Flags, Tile1.m_Delay);
+			});
+		}
+		else if(Flags & TILESLAYERFLAG_TUNE)
+		{
+			const int aData[2] = {apTilemap[0]->m_Tune, apTilemap[1]->m_Tune};
+			Ok = DiffTileLayer<CTuneTile>(aMaps, pMapNames, aData, Width, Height, [&](const CTuneTile &Tile0, const CTuneTile &Tile1, int x, int y) {
+				if(Tile0.m_Number != Tile1.m_Number || Tile0.m_Type != Tile1.m_Type)
+					dbg_msg("map_diff", "[%d:%s] %dx%d: (number: %d, type: %d) != (number: %d, type: %d)", aLayersNum[0], aaName[0], x, y, Tile0.m_Number, Tile0.m_Type, Tile1.m_Number, Tile1.m_Type);
+			});
+		}
+		else
+		{
+			// Regular/game layers use m_Data, the front layer uses m_Front, both as CTile.
+			const int aData[2] = {
+				(Flags & TILESLAYERFLAG_FRONT) ? apTilemap[0]->m_Front : apTilemap[0]->m_Data,
+				(Flags & TILESLAYERFLAG_FRONT) ? apTilemap[1]->m_Front : apTilemap[1]->m_Data};
+			Ok = DiffTileLayer<CTile>(aMaps, pMapNames, aData, Width, Height, [&](const CTile &Tile0, const CTile &Tile1, int x, int y) {
+				if(Tile0.m_Index != Tile1.m_Index || Tile0.m_Flags != Tile1.m_Flags)
+					dbg_msg("map_diff", "[%d:%s] %dx%d: (index: %d, flags: %d) != (index: %d, flags: %d)", aLayersNum[0], aaName[0], x, y, Tile0.m_Index, Tile0.m_Flags, Tile1.m_Index, Tile1.m_Flags);
+			});
 		}
 
-		for(int y = 0; y < apTilemap[0]->m_Height; y++)
-		{
-			for(int x = 0; x < apTilemap[0]->m_Width; x++)
-			{
-				int Pos = y * apTilemap[0]->m_Width + x;
-				if(apTile[0][Pos].m_Index != apTile[1][Pos].m_Index || apTile[0][Pos].m_Flags != apTile[1][Pos].m_Flags)
-				{
-					dbg_msg("map_diff", "[%d:%s] %dx%d: (index: %d, flags: %d) != (index: %d, flags: %d)", aLayersNum[0], aaName[0], x, y, apTile[0][Pos].m_Index, apTile[0][Pos].m_Flags, apTile[1][Pos].m_Index, apTile[1][Pos].m_Flags);
-				}
-			}
-		}
+		if(!Ok)
+			return false;
 	}
 
 	return true;
