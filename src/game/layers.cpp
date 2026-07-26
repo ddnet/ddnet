cbc93d0be669cecd5640c7457fcd711c64f04343
Dennis Felsing <dennis@felsing.org>
2026-07-26 20:54:17 +0200

    client: Fix tile data access

--- src/game/layers.cpp
+++ src/game/layers.cpp
@@ -155,6 +155,12 @@ void CLayers::InitTilemapSkip(bool GameOnly)
 				continue;
 
 			CTile *pTiles = static_cast<CTile *>(m_pMap->GetData(pTilemap->m_Data));
+			if(pTiles == nullptr || pTilemap->m_Width < 0 || pTilemap->m_Height < 0 ||
+				(int64_t)pTilemap->m_Width * pTilemap->m_Height > m_pMap->GetDataSize(pTilemap->m_Data) / (int)sizeof(CTile))
+			{
+				continue;
+			}
+
 			for(int y = 0; y < pTilemap->m_Height; y++)
 			{
 				for(int x = 1; x < pTilemap->m_Width;)
