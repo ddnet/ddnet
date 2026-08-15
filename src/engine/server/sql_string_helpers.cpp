9bc8a7d1d1e71cd1d22de86e0fc580d939ff9a7e
Dennis Felsing <dennis@felsing.org>
2026-08-15 16:25:50 +0200

    sql: don't read 1 byte past buffer in FuzzyString

--- src/engine/server/sql_string_helpers.cpp
+++ src/engine/server/sql_string_helpers.cpp
@@ -15,7 +15,7 @@ void sqlstr::FuzzyString(char *pString, int Size)
 			break;
 
 		pNewString[OutPos++] = pString[i];
-		if(pString[i] != '\\' && str_utf8_isstart(pString[i + 1]))
+		if(pString[i] != '\\' && str_utf8_isstart(i + 1 < Size ? pString[i + 1] : '\0'))
 			pNewString[OutPos++] = '%';
 	}
 
