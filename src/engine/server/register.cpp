d98e1e4ea3294b3bd10adb3b77cd9dfeebb73b02
Robert Müller <robytemueller@gmail.com>
2026-08-17 23:22:21 +0200

    Increase server info size to handle 128 players with 0.7 skins

--- src/engine/server/register.cpp
+++ src/engine/server/register.cpp
@@ -143,7 +143,7 @@ class CRegister : public IRegister
 	CUuid m_Secret = RandomUuid();
 	CUuid m_ChallengeSecret = RandomUuid();
 	bool m_GotServerInfo = false;
-	char m_aServerInfo[32768];
+	char m_aServerInfo[100 * 1024];
 
 public:
 	CRegister(CConfig *pConfig, IConsole *pConsole, IEngine *pEngine, IHttp *pHttp, int ServerPort, unsigned SixupSecurityToken);
