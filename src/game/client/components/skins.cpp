03407db9e6cd1c1269b8f7cc2e30ce68d2562f46
Robert Müller <robytemueller@gmail.com>
2026-08-30 21:36:05 +0200

    Fix skin metrics potentially not being reset before loading
    In the `CSkins::CSkinDownloadJob::Run` function, the `LoadSkinData` function may be called twice using the same output variable without the skin metrics being reset. This could have caused the skin metrics to be incorrect if the metrics for the newly downloaded skin file are different from the metrics for the local skin file.

--- src/game/client/components/skins.cpp
+++ src/game/client/components/skins.cpp
@@ -357,6 +357,7 @@ bool CSkins::LoadSkinData(const char *pName, CSkinLoadData &Data) const
 		Data.m_BloodColor = ColorRGBA(NormalizedColor.x, NormalizedColor.y, NormalizedColor.z);
 	}
 
+	Data.m_Metrics.Reset();
 	CheckMetrics(Data.m_Metrics.m_Body, Data.m_Info.m_pData, Pitch, 0, 0, BodyWidth, BodyHeight);
 	CheckMetrics(Data.m_Metrics.m_Body, Data.m_Info.m_pData, Pitch, BodyOutlineOffsetX, BodyOutlineOffsetY, BodyOutlineWidth, BodyOutlineHeight);
 	CheckMetrics(Data.m_Metrics.m_Feet, Data.m_Info.m_pData, Pitch, FeetOffsetX, FeetOffsetY, FeetWidth, FeetHeight);
