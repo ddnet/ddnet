a2d15798cf7c67642a019e3ce422b9e95d9eae21
heinrich5991 <heinrich5991@gmail.com>
2026-09-15 12:10:24 +0200

    comment

--- src/engine/asdf.h
+++ src/engine/asdf.h
@@ -13,7 +13,18 @@ public:
 	virtual ~IAsdf() = default;
 
 	virtual void Init() = 0;
+	/**
+	 * Checks whether the bans accessible through `IsBanned` have changed
+	 * since the last time `BansHaveChanged` has been called.
+	 */
 	virtual bool BansHaveChanged() = 0;
+	/**
+	 * Checks whether a given `pAddr` is banned.
+	 *
+	 * @param pBuf Buffer for the ban reason, only populated if the function returns true.
+	 * @param BufferSize Size of the buffer `ppBuf`.
+	 * @return `true` if the given `pAddr` is banned.
+	 */
 	virtual bool IsBanned(const NETADDR *pAddr, char *pBuf, unsigned BufferSize) const = 0;
 };
 
